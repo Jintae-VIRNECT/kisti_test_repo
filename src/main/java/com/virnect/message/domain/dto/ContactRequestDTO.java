f65d40c5d0e915d16ba33a5464c543a1723449ff
jukyoung lee <ljk@virnect.com>
2020-02-12 17:18:53 +0900

    fix : modify requst dto

--- src/main/java/com/virnect/message/domain/dto/ContactRequestDTO.java
+++ src/main/java/com/virnect/message/domain/dto/ContactRequestDTO.java
@@ -17,22 +17,17 @@ import java.util.Map;
 @Getter
 @Setter
 public class ContactRequestDTO {
-    //문의하기 -> message서비스 -> 버넥트 관계자
-    private List<String> category;
-    private String name;//회사명 or 이름
-    private String phone;//연락처
+    @NotBlank
+    private String sender;
 
     @NotBlank
-    private String sender;//Email
+    private List<String> receiver;
 
     @NotBlank
-    private List<String> receiver; //버넥트
+    private String subject;
+
+    private Map<String, Object> context;
 
     @NotBlank
-    private String subject;//제목
-
-    private Map<String, Object> context; //이름, Email, 연락처, 제목, 내용
-
-    @NotBlank
-    private String template; //none 이면 (필수값 : none)
+    private String template;
 }
