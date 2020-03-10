637b18446f6cd4cc36fb84c592559359ddfd9ddb
wooka <kakrwoo@naver.com>
2020-03-10 18:06:35 +0900

    Feat:user device setting test page

--- src/components/service/call/mainview/MainVideo.vue
+++ src/components/service/call/mainview/MainVideo.vue
@@ -1,8 +1,7 @@
 <template>
-<div class="main-video">
-  <div class="main-video__box">
-    
-    <video 
+  <div class="main-video">
+    <div class="main-video__box">
+      <video
         ref="mainVideo"
         id="main-video"
         :srcObject.prop="session.stream"
@@ -10,26 +9,23 @@
         @loadeddata="optimizeVideoSize"
         :muted="!speaker"
         autoplay
-        playsinline 
+        playsinline
         loop
-    ></video>
+      ></video>
 
-    <pointing
-        v-if="true"
-        :scale="1"
-        class="main-video__pointing"
-    ></pointing>
+      <pointing v-if="true" :scale="1" class="main-video__pointing"></pointing>
 
-    <div class="main-video__info">
-      <img class="profile" src="~assets/image/call/chat_img_user.svg"/>
-      <span class="name">{{ session.nickName }}</span>
-      <span class="status" :class="status">연결상태</span>
+      <div class="main-video__info">
+        <img class="profile" src="~assets/image/call/chat_img_user.svg" />
+        <span class="name">{{ session.nickName }}</span>
+        <span class="status" :class="status">연결상태</span>
+      </div>
+
+      <button v-if="session.nodeId === 'main'" class="main-video__setting">
+        화면 설정
+      </button>
     </div>
-
-    <button v-if="session.nodeId === 'main'" class="main-video__setting">화면 설정</button>
-
   </div>
-</div>
 </template>
 
 <script>
@@ -37,41 +33,40 @@ import { mapActions, mapGetters } from 'vuex'
 
 import Pointing from './partials/Pointing'
 export default {
-	name: "MainVideo",
-	components: {
-    Pointing
+  name: 'MainVideo',
+  components: {
+    Pointing,
   },
-	data() {
-		return {
-      status: 'good'  // good, normal, bad
+  data() {
+    return {
+      status: 'good', // good, normal, bad
     }
-	},
-	computed: {
+  },
+  computed: {
     ...mapGetters({
       session: 'mainSession',
-      speaker: 'speaker'
-    })
+      speaker: 'speaker',
+    }),
   },
-	watch: {
+  watch: {
     speaker(val) {
-      this.$refs['mainVideo'].muted = ( val ? false : true )
+      this.$refs['mainVideo'].muted = val ? false : true
       console.log(this.$refs['mainVideo'].muted)
-    }
+    },
   },
-	methods: {
+  methods: {
     ...mapActions(['updateAccount']),
     optimizeVideoSize() {
       const mainWrapper = this.$el
       const videoBox = this.$el.querySelector('.main-video__box')
       const videoEl = this.$el.querySelector('#main-video')
 
-      let 
-        videoWidth = videoEl.offsetWidth,
+      let videoWidth = videoEl.offsetWidth,
         videoHeight = videoEl.offsetHeight,
         wrapperWidth = mainWrapper.offsetWidth,
         wrapperHeight = mainWrapper.offsetHeight
 
-      if(videoHeight / videoWidth > wrapperHeight / wrapperWidth) {
+      if (videoHeight / videoWidth > wrapperHeight / wrapperWidth) {
         videoBox.style.width = 'auto'
         videoEl.style.width = 'auto'
         videoEl.style.height = '100%'
@@ -87,30 +82,30 @@ export default {
     },
     capture() {
       const videoEl = this.$el.querySelector('#main-video')
-      
+
       const width = videoEl.offsetWidth
-      const height = videoEl.offsetHeight;
+      const height = videoEl.offsetHeight
 
-      const tmpCanvas = document.createElement('canvas');
-      tmpCanvas.width = width;
-      tmpCanvas.height = height;
+      const tmpCanvas = document.createElement('canvas')
+      tmpCanvas.width = width
+      tmpCanvas.height = height
 
-      const tmpCtx = tmpCanvas.getContext('2d');
-      tmpCtx.drawImage(videoEl, 0, 0, width, height);
-      tmpCanvas.toBlob((blob)=>{
-        const a = document.createElement('a');
-        document.body.appendChild(a);
-        const url = window.URL.createObjectURL(blob);
-        a.href = url;
-        a.download = '캡쳐.png';
-        a.click();
+      const tmpCtx = tmpCanvas.getContext('2d')
+      tmpCtx.drawImage(videoEl, 0, 0, width, height)
+      tmpCanvas.toBlob(blob => {
+        const a = document.createElement('a')
+        document.body.appendChild(a)
+        const url = window.URL.createObjectURL(blob)
+        a.href = url
+        a.download = '캡쳐.png'
+        a.click()
         setTimeout(() => {
-          window.URL.revokeObjectURL(url);
-          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url)
+          document.body.removeChild(a)
         }, 0)
-          // this.imgBlob = blob
+        // this.imgBlob = blob
       }, 'image/png')
-    }
+    },
   },
 
   /* Lifecycles */
@@ -123,20 +118,19 @@ export default {
     this.$eventBus.$on('capture', this.capture)
     window.addEventListener('resize', this.optimizeVideoSize)
   },
-	mounted() {
+  mounted() {
     // console.log(this.optimizeVideoSize())
     // let sessionName = "Session " + Math.floor(Math.random() * 10)
-    let sessionName = "virnect"
-    let nickName = "Participant " + Math.floor(Math.random() * 100)
+    let sessionName = 'virnect'
+    let nickName = 'Participant ' + Math.floor(Math.random() * 100)
     this.updateAccount({
       sessionName: sessionName,
-      nickName: nickName
+      nickName: nickName,
     })
-    
-    this.$openvidu.join(sessionName, nickName)
-      .then(session => {
-        console.log(session.connection)
-      })
-  }
+
+    this.$openvidu.join(sessionName, nickName).then(session => {
+      console.log(session.connection)
+    })
+  },
 }
 </script>
---
+++ src/components/test/TestRemoteSetting.vue
@@ -0,0 +1,240 @@
+<template>
+  <div class="setting">
+    <h1 class="setting-title">Remote Setting Test</h1>
+
+    <!-- Video Device 설정 -->
+    <section class="setting-section">
+      <h2 class="setting-subtitle">Select Video</h2>
+      <div>
+        <select v-model="selectVideo" @change="getVideoStream">
+          <option
+            v-for="device in videoDevices"
+            :key="device.deviceId"
+            :value="device.deviceId"
+            >{{ device.label }}
+          </option>
+        </select>
+      </div>
+      <video :srcObject.prop="videoStream" autoplay></video>
+    </section>
+
+    <!-- Audio Device 설정 -->
+    <section class="setting-section">
+      <h2 class="setting-subtitle">Select Audio</h2>
+      <div>
+        <select v-model="selectAudio" @click="getAudioStream">
+          <option
+            v-for="device in audioDevices"
+            :key="device.deviceId"
+            :value="device.deviceId"
+            >{{ device.label }}
+          </option>
+        </select>
+        <label>볼륨</label>
+        <select v-model="audioVolume" @change="changeAudioVolume">
+          <option v-for="(volume, idx) in volumeList" :key="idx" :value="volume"
+            >{{ volume }}
+          </option>
+        </select>
+        <button @click="$refs['audioComponent'].play()">재생</button>
+        <button @click="$refs['audioComponent'].pause()">정지</button>
+      </div>
+      <audio
+        ref="audioComponent"
+        :srcObject.prop="audioStream"
+        autoplay
+      ></audio>
+      <meter ref="audioMeter" min="0" max="100" :value="soundWidth"></meter>
+      <div class="sound">
+        <div class="sound-back"></div>
+        <div class="sound-front" :style="{ width: soundWidth + '%' }"></div>
+      </div>
+    </section>
+
+    <!-- Output Device 설정 -->
+    <section class="setting-section">
+      <h2 class="setting-subtitle">Select Output</h2>
+      <div>
+        <select v-model="selectOutput" @change="setAudioOutput">
+          <option
+            v-for="device in outputDevices"
+            :key="device.deviceId"
+            :value="device.deviceId"
+            >{{ device.label }}
+          </option>
+        </select>
+        <label>볼륨</label>
+        <select v-model="outputVolume" @change="changeOutputVolume">
+          <option v-for="(volume, idx) in volumeList" :key="idx" :value="volume"
+            >{{ volume }}
+          </option>
+        </select>
+        <button @click="$refs['outputComponent'].play()">재생</button>
+        <button @click="$refs['outputComponent'].pause()">정지</button>
+      </div>
+      <audio ref="outputComponent" :src="mp3" autoplay></audio>
+      <meter ref="outputMeter" min="0" max="100" :value="outputWidth"></meter>
+      <div class="sound">
+        <div class="sound-back"></div>
+        <div class="sound-front" :style="{ width: outputWidth + '%' }"></div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+import SoundMeter from 'plugins/service/soundmeter'
+
+export default {
+  data() {
+    return {
+      volumeList: [0.2, 0.4, 0.6, 0.8, 1],
+      /* video */
+      videoDevices: [],
+      selectVideo: null,
+      videoStream: null,
+      /* audio */
+      audioDevices: [],
+      selectAudio: null,
+      audioStream: null,
+      audioVolume: 1,
+      audioSoundVolume: 0,
+      audioContext: null,
+      audioSoundMeter: null,
+      /* output */
+      mp3: require('assets/media/queen.mp3'),
+      outputDevices: [],
+      selectOutput: null,
+      outputStream: null,
+      outputVolume: 1,
+      outputSoundVolume: 0,
+      outputSoundMeter: null,
+    }
+  },
+  computed: {
+    soundWidth() {
+      return parseInt(this.audioSoundVolume * 100)
+    },
+    outputWidth() {
+      return parseInt(this.outputSoundVolume * 100)
+    },
+  },
+  methods: {
+    init() {
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then(devices => {
+          console.log(devices)
+          devices.forEach(device => {
+            if (device.kind === 'videoinput') {
+              this.videoDevices.push(device)
+            } else if (device.kind === 'audioinput') {
+              this.audioDevices.push(device)
+            } else if (device.kind === 'audiooutput') {
+              this.outputDevices.push(device)
+            }
+          })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    getVideoStream() {
+      console.log(this.selectVideo)
+      const constraints = {
+        video: {
+          deviceId: this.selectVideo,
+        },
+        audio: false,
+      }
+      navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+        console.log(stream)
+        this.videoStream = stream
+      })
+    },
+    getAudioStream() {
+      console.log(this.selectAudio)
+      const constraints = {
+        video: false,
+        audio: {
+          deviceId: this.selectAudio,
+        },
+      }
+      navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+        // Put variables in global scope to make them available to the
+        // browser console.
+        this.audioStream = stream
+        const soundMeter = new SoundMeter(this.audioContext)
+        soundMeter.connectToSource(stream, e => {
+          if (e) {
+            alert(e)
+            return
+          }
+          setInterval(() => {
+            this.audioSoundVolume = soundMeter.instant.toFixed(2)
+          }, 200)
+        })
+      })
+    },
+    setOutputSoundMeter() {
+      this.outputSoundMeter = new SoundMeter(this.audioContext)
+      console.log(this.outputSoundMeter)
+      this.outputSoundMeter.connectToMedia(this.$refs['outputComponent'], e => {
+        if (e) {
+          alert(e)
+          return
+        }
+        setInterval(() => {
+          this.outputSoundVolume = this.outputSoundMeter.instant.toFixed(2)
+        }, 200)
+      })
+    },
+    changeAudioVolume() {
+      this.$refs['audioComponent'].volume = this.audioVolume
+    },
+    changeOutputVolume() {
+      this.$refs['outputComponent'].volume = this.outputVolume
+    },
+    setAudioOutput() {
+      this.$refs['outputComponent'].pause()
+      this.$nextTick(() => {
+        this.$refs['outputComponent'].setSinkId(this.selectOutput).then(() => {
+          this.$refs['outputComponent'].play()
+        })
+      })
+    },
+  },
+  mounted() {
+    this.init()
+    window.onload = () => {
+      this.audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)()
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.setting-section {
+  margin: 20px;
+  background-color: #dedede;
+  border-radius: 10px;
+}
+
+.sound {
+  position: relative;
+  width: 200px;
+  height: 30px;
+  .sound-back {
+    width: 100%;
+    height: 100%;
+    background-color: #999;
+  }
+  .sound-front {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    background-color: #9cdcf0;
+  }
+}
+</style>
