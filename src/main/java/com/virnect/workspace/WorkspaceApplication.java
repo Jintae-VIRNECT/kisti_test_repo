365710baa4baf013f5e94897cfd41518f10e3b0b
jukyoung lee <ljk@virnect.com>
2020-03-17 17:20:42 +0900

    fix: error

--- src/main/java/com/virnect/workspace/WorkspaceApplication.java
+++ src/main/java/com/virnect/workspace/WorkspaceApplication.java
@@ -3,9 +3,10 @@ package com.virnect.workspace;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WorkspaceApplication {
 
