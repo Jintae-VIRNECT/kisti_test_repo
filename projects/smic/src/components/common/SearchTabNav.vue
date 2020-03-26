0b66a33b115b2caa1928b27597f2cffef99dd64b
SangBaek-VIRNECT <lsb@virnect.com>
2020-03-26 10:59:05 +0900

    fix: searchtype change

--- projects/smic/src/components/common/SearchTabNav.vue
+++ projects/smic/src/components/common/SearchTabNav.vue
@@ -64,7 +64,6 @@ export default {
   watch: {
     searchType() {
       this.searchTypeValue = this.searchType.value
-      this.onChangeSearch()
     },
     filter() {
       this.filterValue = this.filter.value
