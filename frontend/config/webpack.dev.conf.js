0262aa612efb61c52acab1b1a3befe976f1f9017
SangBaek-VIRNECT <lsb@virnect.com>
2020-04-09 10:44:41 +0900

    chore: yarn workspace

--- frontend/config/webpack.dev.conf.js
+++ frontend/config/webpack.dev.conf.js
@@ -6,7 +6,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 	mode: 'development',
 	devtool: 'cheap-module-eval-source-map',
 	devServer: {
-		host: '192.168.13.43',
+		host: 'localhost',
 		port: 8888,
 		historyApiFallback: { index: '/main/main.html' },
 		hot: true,
