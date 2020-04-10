3071545b81f76182b88ceaac336a04f82b19af53
JoengHyeon-VIRNECT <sky456139@virnect.com>
2020-04-10 10:51:50 +0900

    feat: swagger centralized

--- src/main/java/com/virnect/gateway/SwaggerConfiguration.java
+++ src/main/java/com/virnect/gateway/SwaggerConfiguration.java
@@ -1,5 +1,30 @@
 package com.virnect.gateway;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.context.annotation.*;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
+import springfox.documentation.spring.web.json.JsonSerializer;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
  * @author jeonghyeon.chang (johnmark)
  * @project PF-Gateway
@@ -7,5 +32,67 @@ package com.virnect.gateway;
  * @description
  * @since 2020.04.09
  */
+
+@Profile({"local", "develop"})
+@Configuration
+@RequiredArgsConstructor
 public class SwaggerConfiguration {
+    private final GatewayProperties gatewayProperties;
+
+    @Bean
+    public JsonSerializer jsonSerializer(List<JacksonModuleRegistrar> moduleRegistrars) {
+        return new JsonSerializer(moduleRegistrars);
+    }
+
+    @Primary
+    @Bean
+    @Lazy
+    public SwaggerResourcesProvider swaggerResourcesProvider() {
+        return () -> gatewayProperties.getRoutes().stream()
+                .map(route -> createResource(route.getId(), getRouteLocation(route), "2.0"))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    private String getRouteLocation(RouteDefinition route) {
+        return Optional.ofNullable(route.getPredicates().get(0).getArgs().values().toArray()[0])
+                .map(String::valueOf)
+                .map(s -> s.replace("*", ""))
+                .orElse(null);
+    }
+
+    private SwaggerResource createResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location + "/v2/api-docs");
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
+    @Bean
+    public Docket docket() {
+        Contact contact = new Contact("장정현", "https://virnect.com", "sky456139@vinrect.com");
+
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .contact(contact)
+                .description("Platform 서버 API 정보 입니다.")
+                .version("v0.0.1")
+                .title("VIRNECT Platform All Service API Document.")
+                .license("VIRNECT INC All rights reserved.")
+                .build();
+
+        List<ResponseMessage> responseMessages = Arrays.asList(new ResponseMessageBuilder().code(400).message("요청에러").build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(true)
+                .globalResponseMessage(RequestMethod.GET, responseMessages)
+                .globalResponseMessage(RequestMethod.POST, responseMessages)
+                .globalResponseMessage(RequestMethod.PUT, responseMessages)
+                .globalResponseMessage(RequestMethod.DELETE, responseMessages)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo);
+    }
 }
--- src/main/java/com/virnect/gateway/SwaggerController.java
+++ src/main/java/com/virnect/gateway/SwaggerController.java
@@ -1,5 +1,18 @@
 package com.virnect.gateway;
 
+import io.swagger.models.Swagger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.spring.web.json.Json;
+import springfox.documentation.spring.web.json.JsonSerializer;
+import springfox.documentation.swagger.web.*;
+
+import java.util.List;
+
 /**
  * @author jeonghyeon.chang (johnmark)
  * @project PF-Gateway
@@ -7,5 +20,39 @@ package com.virnect.gateway;
  * @description
  * @since 2020.04.10
  */
+
+@Controller
+@RequiredArgsConstructor
 public class SwaggerController {
+    private final JsonSerializer jsonSerializer;
+    private final SwaggerResourcesProvider swaggerResources;
+
+    @RequestMapping({"/swagger-resources/configuration/security"})
+    @ResponseBody
+    public ResponseEntity<SecurityConfiguration> securityConfiguration() {
+        return ResponseEntity.ok(SecurityConfigurationBuilder.builder().build());
+    }
+
+    @RequestMapping({"/swagger-resources/configuration/ui"})
+    @ResponseBody
+    public ResponseEntity<UiConfiguration> uiConfiguration() {
+        return ResponseEntity.ok(UiConfigurationBuilder.builder().build());
+    }
+
+    @RequestMapping({"/swagger-resources"})
+    @ResponseBody
+    public ResponseEntity<List<SwaggerResource>> swaggerResources() {
+        return ResponseEntity.ok(this.swaggerResources.get());
+    }
+
+    @RequestMapping(
+            value = {"/v2/api-docs"},
+            method = {RequestMethod.GET},
+            produces = {"application/json", "application/hal+json"}
+    )
+    @ResponseBody
+    public ResponseEntity<Json> getDocumentation() {
+        Swagger swagger = new Swagger();
+        return ResponseEntity.ok(this.jsonSerializer.toJson(swagger));
+    }
 }
