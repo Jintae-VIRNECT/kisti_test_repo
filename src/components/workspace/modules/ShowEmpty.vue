82f185f959c577542ce9b3b937fec2851728f9c6
wooka <kakrwoo@naver.com>
2020-04-10 14:05:43 +0900

    Feat: setting layout

---
+++ src/components/workspace/modules/ShowEmpty.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="show-empty" :style="{ 'background-image': image }">
+    <p class="show-empty-title">
+      {{ title }}
+    </p>
+    <p class="show-empty-description">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowEmpry',
+  components: {},
+  data() {
+    return {}
+  },
+  props: {
+    image: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {},
+
+  /* Lifecycles */
+  mounted() {},
+}
+</script>
+
+<style lang="scss" scoped>
+.show-empty {
+  width: 100%;
+  height: 100%;
+  background: url(~assets/image/img_user_empty.svg) 50% 35% #29292c no-repeat;
+}
+.show-empty-title {
+  padding-top: 400px;
+  color: #fafafa;
+  font-size: 24px;
+  text-align: center;
+}
+.show-empty-description {
+  color: rgba(#fafafa, 0.5);
+  font-weight: 300;
+  font-size: 20px;
+  text-align: center;
+}
+</style>
--- src/components/workspace/partials/WorkspaceTabView.vue
+++ src/components/workspace/partials/WorkspaceTabView.vue
@@ -12,19 +12,30 @@
       ></search>
     </div>
     <div class="tab-view__body">
-      <slot></slot>
+      <slot v-if="!empty"></slot>
+      <show-empty
+        v-else
+        :title="'최근 통화 목록이 없습니다.'"
+        :description="' 원격 협업을 시작해보세요.'"
+      ></show-empty>
     </div>
   </section>
 </template>
 
 <script>
+import ShowEmpty from 'ShowEmpty'
 import Search from 'Search'
 export default {
   name: 'WorkspaceTabView',
   components: {
     Search,
+    ShowEmpty,
   },
   props: {
+    empty: {
+      type: Boolean,
+      default: true,
+    },
     title: {
       type: String,
       required: true,
