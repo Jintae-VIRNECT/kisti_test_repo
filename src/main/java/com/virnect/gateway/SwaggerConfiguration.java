9233a24c010961a3dbfecee6b2c559ae5f5c9bc0
JoengHyeon-VIRNECT <sky456139@virnect.com>
2020-04-10 10:33:25 +0900

    feat: swagger centralized

---
+++ src/main/java/com/virnect/gateway/SwaggerConfiguration.java
@@ -0,0 +1,11 @@
+package com.virnect.gateway;
+
+/**
+ * @author jeonghyeon.chang (johnmark)
+ * @project PF-Gateway
+ * @email practice1356@gmail.com
+ * @description
+ * @since 2020.04.09
+ */
+public class SwaggerConfiguration {
+}
