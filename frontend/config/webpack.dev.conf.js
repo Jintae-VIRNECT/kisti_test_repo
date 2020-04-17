6301259c320d82c32e06512b3550cccca94c3a91
unknown <shkang@virnect.com>
2020-04-17 17:50:43 +0900

    feat: env file update

--- frontend/config/webpack.dev.conf.js
+++ frontend/config/webpack.dev.conf.js
@@ -1,13 +1,17 @@
 'use strict'
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
+const fs = require('fs')
+const dotenv = require('dotenv')
+const filePath = `.env.${process.env.NODE_ENV.trim()}`
+const env = dotenv.parse(fs.readFileSync(filePath))
 
 const devWebpackConfig = merge(baseWebpackConfig, {
-	mode: 'development',
+	mode: process.env.NODE_ENV === 'develop' ? 'development' : 'production',
 	devtool: 'cheap-module-eval-source-map',
 	devServer: {
-		host: 'localhost',
-		port: 8888,
+		host: env.LOCAL_HOST,
+		port: env.LOCAL_PORT,
 		disableHostCheck: true,
 		historyApiFallback: { index: '/main/main.html' },
 		hot: true,
@@ -24,5 +28,4 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 		openPage: '',
 	},
 })
-
 module.exports = devWebpackConfig
