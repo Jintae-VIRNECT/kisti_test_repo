c3ff003e04b0b9bf04d34484d2318090d9c1c172
JoengHyeon-VIRNECT <sky456139@virnect.com>
2020-04-22 09:45:59 +0900

    feat: turn off ribbon and change feign retryer interval 5 to 3

--- src/main/java/com/virnect/license/global/config/FeignConfiguration.java
+++ src/main/java/com/virnect/license/global/config/FeignConfiguration.java
@@ -1,11 +1,17 @@
 package com.virnect.license.global.config;
 
 import feign.Logger;
+import feign.RetryableException;
+import feign.Retryer;
+import feign.codec.ErrorDecoder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.http.HttpStatus;
+
+import static java.lang.String.format;
 
 /**
  * Project: user
@@ -17,6 +23,13 @@ import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 @Configuration
 @EnableFeignClients(basePackages = "com.virnect.license.application")
 public class FeignConfiguration {
+
+    // Feign retry set
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(1000, 2000, 3);
+    }
+
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
