e60d46e1248649234818a1b918b90ca3c045cced
wooka <kakrwoo@naver.com>
2020-04-23 15:47:58 +0900

    Feat: tooptip style setting

--- src/components/workspace/modules/ProfileList.vue
+++ src/components/workspace/modules/ProfileList.vue
@@ -6,25 +6,36 @@
       v-for="user of showUsers"
       :key="user.id"
     >
-      <img
-        class="profilelist-user__image"
-        @error="onImageError"
-        :src="user.path"
-      />
+      <tooltip :content="user.userName">
+        <img
+          class="profilelist-user__image"
+          @error="onImageError"
+          :src="user.path"
+          slot="body"
+        />
+        <span>{{ user.userName }}</span>
+      </tooltip>
     </figure>
     <br />
-    <popover placement="right" trigger="hover" v-if="otherUsers.length > 0">
+    <popover
+      v-if="otherUsers.length > 0"
+      placement="right"
+      trigger="hover"
+      :width="150"
+      popperClass="otheruser-popover"
+    >
       <div>
         <figure
-          class="profilelist-user"
+          class="profilelist-user otheruser"
           v-for="user of otherUsers"
           :key="user.id"
         >
           <img
-            class="profilelist-user__image"
+            class="profilelist-user__image otheruser"
             :src="user.path"
             @error="onImageError"
           />
+          <span class="profilelist-user__name ">{{ user.userName }}</span>
         </figure>
       </div>
       <p slot="reference" class="profilelist-user__expend" :style="customStyle">
@@ -35,10 +46,12 @@
 </template>
 
 <script>
+import Tooltip from 'Tooltip'
 import Popover from 'Popover'
 export default {
   name: 'ProfileList',
   components: {
+    Tooltip,
     Popover,
   },
   props: {
@@ -90,14 +103,38 @@ export default {
   width: 38px;
   height: 38px;
   margin-left: 6px;
-  overflow: hidden;
   border: solid 1px transparent;
   border-radius: 50%;
+  &.otheruser {
+    display: flex;
+    width: 100%;
+    margin-bottom: 10px;
+    margin-left: 0;
+    overflow: unset;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
 }
 .profilelist-user__image {
   width: 100%;
   height: 100%;
   @include image();
+  &.otheruser {
+    width: 36px;
+    height: 36px;
+  }
+}
+.profilelist-user__name {
+  display: inline-block;
+  flex: 1;
+  max-width: 70px;
+  margin-left: 10px;
+  overflow: hidden;
+  color: #fff;
+  line-height: 38px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 }
 
 .profilelist-user__expend {
@@ -112,5 +149,17 @@ export default {
   border: solid 2px #979fb0;
   border-radius: 50%;
   cursor: default;
+}
+</style>
+
+<style lang="scss">
+.otheruser-popover {
+  min-width: 150px;
+  background-color: #242427;
+  border: solid 1px #3a3a3d;
+  transform: translateX(10px);
+  .popover--body {
+    padding: 15px;
+  }
 }
 </style>
