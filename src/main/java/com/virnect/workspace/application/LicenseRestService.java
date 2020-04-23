dabc9944dc7ef88b5ce25817e50c2c533b1fbaf1
jukyoung lee <ljk@virnect.com>
2020-04-23 20:13:56 +0900

    feat: add license server config and revise file upload path

---
+++ src/main/java/com/virnect/workspace/application/LicenseRestService.java
@@ -0,0 +1,15 @@
+package com.virnect.workspace.application;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * Project: PF-Workspace
+ * DATE: 2020-04-23
+ * AUTHOR: jkleee (Jukyoung Lee)
+ * EMAIL: ljk@virnect.com
+ * DESCRIPTION:
+ */
+@FeignClient(name = "license-server")
+public interface LicenseRestService {
+
+}
