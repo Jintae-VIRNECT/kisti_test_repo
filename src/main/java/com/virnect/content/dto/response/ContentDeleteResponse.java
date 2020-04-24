2125aed7a2772804a235a793f6822c7de942e109
hkmin-virnect <virnect.office.hkmin@gmail.com>
2020-04-24 09:34:35 +0900

    fix: delete content

--- src/main/java/com/virnect/content/dto/response/ContentDeleteResponse.java
+++ src/main/java/com/virnect/content/dto/response/ContentDeleteResponse.java
@@ -31,10 +31,10 @@ public class ContentDeleteResponse {
     @ApiModelProperty(value = "삭제 처리 결과 메시지", notes = "컨텐츠가 삭제결과 메시지", example = "success")
     private String msg;
     @ApiModelProperty(value = "삭제 처리 결과", notes = "컨텐츠가 삭제되었으면 true, 아니면 false 값을 리턴합니다", example = "true")
-    private boolean result;
+    private Boolean result;
 
     @Builder
-    public ContentDeleteResponse(String workspaceUUID, String contentUUID, String contentName, YesOrNo shared, String uploaderUUID, YesOrNo converted, LocalDateTime updatedDate, String msg, boolean result) {
+    public ContentDeleteResponse(String workspaceUUID, String contentUUID, String contentName, YesOrNo shared, String uploaderUUID, YesOrNo converted, LocalDateTime updatedDate, String msg, Boolean result) {
         this.workspaceUUID = workspaceUUID;
         this.contentUUID = contentUUID;
         this.contentName = contentName;
