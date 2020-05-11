04a6e03036a9ff0aa674e7137f3316833840087e
JoengHyeon-VIRNECT <sky456139@virnect.com>
2020-05-11 16:41:31 +0900

    feat: delete queryDsl directory and add license metadata column on license plan

--- src/main/java/com/virnect/license/domain/LicensePlan.java
+++ src/main/java/com/virnect/license/domain/LicensePlan.java
@@ -2,6 +2,7 @@ package com.virnect.license.domain;
 
 import lombok.*;
 import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.LastModifiedBy;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -35,10 +36,22 @@ public class LicensePlan extends BaseTimeEntity {
     @Column(name = "user_id", nullable = false)
     private String userId;
 
+    @Column(name = "storage_amount", nullable = false)
+    private Long storageAmount;
+
+    @Column(name = "download_hit", nullable = false)
+    private Long downloadHit;
+
+    @Column(name = "call_time", nullable = false)
+    private Long callTime;
 
     // nullable false 로 나중에 바꾸기
     @Column(name = "workspace_id")
     private String workspaceId;
+
+    @Column(name = "modified_user", nullable = false)
+    @LastModifiedBy
+    private String modifiedUser;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
@@ -52,12 +65,16 @@ public class LicensePlan extends BaseTimeEntity {
     private List<LicenseProduct> licenseProductList;
 
     @Builder
-    public LicensePlan(String userId, String workspaceId, LocalDateTime startDate, LocalDateTime endDate, PlanStatus planStatus, Coupon coupon) {
+    public LicensePlan(String userId, String workspaceId, LocalDateTime startDate, LocalDateTime endDate, PlanStatus planStatus, Coupon coupon, Long downloadHit, Long storageAmount, Long callTime) {
         this.userId = userId;
         this.workspaceId = workspaceId;
         this.startDate = startDate;
         this.endDate = endDate;
         this.planStatus = planStatus;
         this.coupon = coupon;
+        this.downloadHit = downloadHit;
+        this.callTime = callTime;
+        this.storageAmount = storageAmount;
+        this.modifiedUser = userId;
     }
 }
