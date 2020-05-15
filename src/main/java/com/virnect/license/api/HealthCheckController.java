458c13b3252013b9cb169397a81ee19351f8ea0e
JoengHyeon-VIRNECT <sky456139@virnect.com>
2020-05-15 14:13:05 +0900

    feat: add healthcheck controller

---
+++ src/main/java/com/virnect/license/api/HealthCheckController.java
@@ -0,0 +1,24 @@
+package com.virnect.license.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author jeonghyeon.chang (johnmark)
+ * @project PF-License
+ * @email practice1356@gmail.com
+ * @description
+ * @since 2020.05.14
+ */
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("WELCOME LICENSE SERVER - " + LocalDateTime.now());
+    }
+}
