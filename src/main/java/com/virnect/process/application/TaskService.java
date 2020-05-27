299523e2c4954cdc6ad99c6c6bdf338375aea216
JiYong-Virnect <jiyong.heo@virnect.comn>
2020-05-27 18:01:56 +0900

    fix: test fileUpload.

--- src/main/java/com/virnect/process/application/TaskService.java
+++ src/main/java/com/virnect/process/application/TaskService.java
@@ -210,8 +210,7 @@ public class TaskService {
         try {
             String targetData = UUID.randomUUID().toString();
 
-            String imgPath = "";// = this.s3UploadService.base64ImageUpload(targetData);
-
+            String imgPath = this.fileUploadService.base64ImageUpload(targetData);
 
             Target target = Target.builder()
                     .type(targetType)
@@ -242,7 +241,7 @@ public class TaskService {
         try {
             String targetData = contentTargetResponse.getData();
             TargetType targetType = contentTargetResponse.getType();
-            String imgPath = "";// = s3UploadService.base64ImageUpload(targetData);
+            String imgPath = this.fileUploadService.base64ImageUpload(targetData);
 
             Target target = Target.builder()
                     .type(targetType)
