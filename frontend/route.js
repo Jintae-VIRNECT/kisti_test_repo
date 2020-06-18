b15f23154ccf78c4b6ccb383d31a4719736535d5
unknown <shkang@virnect.com>
2020-06-18 11:11:53 +0900

    d

--- frontend/route.js
+++ frontend/route.js
@@ -7,23 +7,23 @@ const path = require('path')
  * Main app route
  */
 
-router.get('/find/email', (req, res) => {
-	res.sendFile(
-		path.join(__dirname, '../src/main/resources/templates/index.html'),
-	)
-})
+// router.get('/find/email', (req, res) => {
+// 	res.sendFile(
+// 		path.join(__dirname, '../src/main/resources/templates/index.html'),
+// 	)
+// })
 
-router.get('/find/reset_password', (req, res) => {
-	res.sendFile(
-		path.join(__dirname, '../src/main/resources/templates/index.html'),
-	)
-})
+// router.get('/find/reset_password', (req, res) => {
+// 	res.sendFile(
+// 		path.join(__dirname, '../src/main/resources/templates/index.html'),
+// 	)
+// })
 
 router.get('/healthcheck', (req, res) => {
 	res.send('Hi Virnect')
 })
 
-router.get('/*', function(req, res) {
+router.get('/**', function(req, res) {
 	res.sendFile(
 		path.join(__dirname, '../src/main/resources/templates/index.html'),
 	)
