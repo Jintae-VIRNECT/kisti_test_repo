fa921d00a4a7b51742284f181b71c359220e8684
ykmo <ykmo@virnect.com>
2020-06-29 12:11:52 +0900

    Fix:resolution ratio bug

--- src/utils/recordOptions.js
+++ src/utils/recordOptions.js
@@ -1,3 +1,6 @@
+const RATIO_16_9 = 1.77
+const RATIO_4_3 = 1.33
+
 // Service Options
 export const localRecTimeOpt = [
   {
@@ -43,3 +46,84 @@ export const localRecIntervalOpt = [
     text: '1분',
   },
 ]
+
+/**
+ *
+ * get resolution object with adjust ratio
+ *
+ * @param {String} resolution stream resolution => 360p, 480p, 720p
+ * @param {Number} inputWidth stream width
+ * @param {Number} inputHeight stream height
+ */
+export const getWH = (resolution, inputWidth, inputHeight) => {
+  //default 16:9 480p
+  const video = {
+    width: 854,
+    height: 480,
+  }
+
+  let inputRatio = null
+
+  if (inputWidth === undefined || inputHeight === undefined) {
+    inputRatio = RATIO_16_9
+  }
+
+  //get ratio
+  if (inputWidth > inputHeight) {
+    inputRatio = inputWidth / inputHeight
+  } else {
+    inputRatio = inputHeight / inputWidth
+  }
+
+  inputRatio = parseFloat(inputRatio.toFixed(2))
+
+  //4:3
+  if (inputRatio === RATIO_4_3) {
+    switch (resolution) {
+      case '360p':
+        video.width = 480
+        video.height = 360
+        break
+      case '480p':
+        video.width = 640
+        video.height = 480
+        break
+
+      case '720p':
+        video.width = 960
+        video.height = 720
+        break
+
+      default:
+        video.width = 640
+        video.height = 480
+        console.log('unknown resolution ::', resolution)
+        break
+    }
+
+    //16:9 and etc...
+  } else {
+    switch (resolution) {
+      case '360p':
+        video.width = 640
+        video.height = 360
+        break
+      case '480p':
+        video.width = 854
+        video.height = 480
+        break
+
+      case '720p':
+        video.width = 1280
+        video.height = 720
+        break
+
+      default:
+        video.width = 854
+        video.height = 480
+        console.log('unknown resolution ::', resolution)
+        break
+    }
+  }
+  return video
+}
