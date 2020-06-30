842261919bd757dab6aa37697f5509c39aeea7fd
wooka <kakrwoo@naver.com>
2020-06-30 12:03:36 +0900

    CSS: remove element ui

--- src/apps/account/app.js
+++ src/apps/account/app.js
@@ -8,12 +8,7 @@ import Store from 'stores/remote/store'
 import App from './app.vue'
 import globalMixin from 'mixins/global'
 
-import ElementUI from 'element-ui'
 import i18n from 'plugins/remote/i18n'
-
-Vue.use(ElementUI, {
-  i18n: (key, value) => i18n.t(key, value),
-})
 
 Vue.mixin(globalMixin)
 
