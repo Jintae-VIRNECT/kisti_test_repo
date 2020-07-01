b5f8ca11e3494e26bbf06a50b2eedf830329a62a
SangBaek-VIRNECT <lsb@virnect.com>
2020-07-01 11:28:34 +0900

    feat: redirect change activeworkspace

---
+++ workstation/src/pages/workspace/_workspaceId.vue
@@ -0,0 +1,14 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  fetch({ params, store }) {
+    store.commit('workspace/SET_ACTIVE_WORKSPACE', params.workspaceId)
+  },
+  beforeMount() {
+    this.$router.replace('/')
+  },
+}
+</script>
