7e5915f1a62f43313e981bdf36d08fcfb41a944c
JiYong-Virnect <jiyong.heo@virnect.comn>
2020-07-03 17:11:14 +0900

    fix:

--- src/main/java/com/virnect/process/application/TaskService.java
+++ src/main/java/com/virnect/process/application/TaskService.java
@@ -2499,7 +2499,7 @@ public class TaskService {
 
         WorkSyncResponse workSyncResponse = new WorkSyncResponse();
 
-        log.info(">>>>>>>>>>> subTaskIds : {}", subTaskIds);
+        log.info(">>>>>>>>>>> 2502 subTaskIds : {}", subTaskIds);
 
         WorkSyncResponse.ProcessResult processResult = buildSyncProcess(process, subTaskIds);
 
@@ -2514,6 +2514,9 @@ public class TaskService {
 
     // CONVERT syncdata - PROCESS LIST
     private WorkSyncResponse.ProcessResult buildSyncProcess(Process process, Long[] subTaskIds) {
+
+        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>> 2518 process.getSubProcessList() : {}", process.getSubProcessList());
+
         List<WorkSyncResponse.SubProcessWorkResult> syncSubProcessList = buildSyncSubProcess(process.getSubProcessList(), subTaskIds);
 
         if (syncSubProcessList.isEmpty()) return null;
@@ -2532,13 +2535,21 @@ public class TaskService {
         for (SubProcess subProcess : subProcesses) {
             WorkSyncResponse.SubProcessWorkResult syncSubProcess = null;
 
-            log.info(">>>>>>>>>>> subTaskIds : {}", subTaskIds);
+
+            log.info(">>>>>>>>>>> 2539 subProcess.getId() : {}", subProcess.getId());
+            log.info(">>>>>>>>>>> 2540 subTaskIds : {}", subTaskIds);
+
+            log.info(">>>>>>>>>>>> 2542 subTaskIds -> : {}", subTaskIds.length);
 
             if (subTaskIds != null && subTaskIds.length > 0) {
+
+                log.info(">>>>>>>>>> 2546 subTaskIds is not null");
                 for (Long subTaskId : subTaskIds) {
+                    log.info(">>>>>>>>>>>> 2548 ubTaskId : {}", subTaskId);
+                    log.info(">>>>>>>>>>>> 2549 subProcess.getId() : {}", subProcess.getId());
                     if (subProcess.getId() == subTaskId) {
-                        log.info(">>>>>>>>>>> subTaskIds : {}", subTaskIds);
-                        log.info(">>>>>>>>>>> subProcess.getId() : {}", subProcess.getId());
+                        log.info(">>>>>>>>>>> 2551 subTaskIds : {}", subTaskIds);
+                        log.info(">>>>>>>>>>> 2552 subProcess.getId() : {}", subProcess.getId());
                         syncSubProcess = buildSyncDataSubProcess(subProcess);
                     }
                 }
