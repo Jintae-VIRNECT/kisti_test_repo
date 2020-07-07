d4a378fb35186a68db6ba30f80600fc88be5ac85
wooka <kakrwoo@naver.com>
2020-07-07 18:54:10 +0900

    Feat: api list

--- src/api/gateway/gateway.js
+++ src/api/gateway/gateway.js
@@ -9,6 +9,7 @@ import { merge } from 'lodash'
 import Axios from 'axios'
 import Cookies from 'js-cookie'
 import urls from '@/server/urls'
+import API from './api'
 
 const TOKEN = Cookies.get('accessToken')
 const axios = Axios.create({
@@ -25,49 +26,7 @@ const axios = Axios.create({
   baseURL: 'https://192.168.6.4:4443',
 })
 
-const URL = {
-  /* Account */
-  LOGIN: ['POST', 'https://192.168.6.3:8073/auth/signin'],
-  TOKEN: ['POST', 'https://192.168.6.3:8073/auth/oauth/token'],
-  ACCOUNT: ['GET', 'https://192.168.6.3:8073/users/info', { type: 'form' }],
-  // ACCESS_TOKEN: ['POST', '/auth/accessToken'],
-
-  /* CONFIGURATION */
-  //!!!!!!Warning! API Request url is not fixed!!!!!!
-  GET_CONFIG: ['GET', '/media/properties/'],
-  UPDATE_CONFIG: ['POST', '/media/properties/'],
-  PUT_CONFIG: ['PUT', '/media/properties/'],
-
-  /* Workspace - History */
-  GET_HISTORY_LIST: ['GET', '/media/history'],
-  GET_HISTORY_ITEM: ['GET', '/media/history/{roomId}'],
-  DELETE_HISTORY_ITEM: ['PUT', '/media/history/{roomId}'],
-  DELETE_HISTORY_ALL: ['DELETE', '/media/history'],
-
-  /* Workspace - Member */
-  GET_MEMBER_LIST: [
-    'GET',
-    'https://192.168.6.3:8073/workspaces/{workspaceId}/members?size={size}',
-  ],
-  // GET_MEMBER_LIST: ['GET', '/media/member/'],
-
-  /* Workspace - Room */
-  ROOM_LIST: ['GET', '/media/room?paging={paging}'],
-  ROOM_INFO: ['GET', '/media/room/{roomId}'],
-  UPDATE_ROOM_INFO: ['PUT', '/media/room/{roomId}', { type: 'form' }],
-  LEAVE_ROOM: ['DELETE', '/media/room/{roomId}/participants/{participantsId}'],
-  PARTICIPANTS_LIST: ['GET', '/media/room/{roomId}/participants'],
-  INVITE_PARTICIPANTS_LIST: ['GET', '/media/room/participants'],
-  CREATE_ROOM: [
-    'POST',
-    '/media/room?workspaceId={workspaceId}',
-    { type: 'form' },
-  ],
-  DELETE_ROOM: ['DELETE', '/media/room/{roomId}'],
-
-  /* CALL */
-  GET_TOKEN: ['POST', '/media/tokens'],
-}
+const URL = API
 
 console.log(`ENV: ${process.env.TARGET_ENV}`)
 
