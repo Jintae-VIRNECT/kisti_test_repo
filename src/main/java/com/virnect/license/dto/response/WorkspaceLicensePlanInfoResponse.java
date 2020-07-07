7ff7b61b9f45bf61ca7d89fe70dad02aaecf1dc8
JeongHyeon-VIRNECT <sky456139@virnect.com>
2020-07-07 17:57:28 +0900

    feat: revise workspace current call time properties on workspace plan info retreive api

--- src/main/java/com/virnect/license/dto/response/WorkspaceLicensePlanInfoResponse.java
+++ src/main/java/com/virnect/license/dto/response/WorkspaceLicensePlanInfoResponse.java
@@ -26,19 +26,19 @@ public class WorkspaceLicensePlanInfoResponse {
     @ApiModelProperty(value = "라이선스 플랜 마스터 사용자 식별자", position = 1, example = "498b1839dc29ed7bb2ee90ad6985c608")
     private String masterUserUUID = "";
     @ApiModelProperty(value = "워크스페이스 이용 최대 사용자 수(마스터 계정 포함 명수)", notes = "마스터 계정 포함", position = 2, example = "9")
-    private Long maxUserAmount;
+    private Long maxUserAmount = 0L;
     @ApiModelProperty(value = "워크스페이스 최대 저장 용량(MB 단위)", notes = "byte 단위의 최대 용량", position = 3, example = "9663676416")
-    private Long maxStorageSize;
+    private Long maxStorageSize = 0L;
     @ApiModelProperty(value = "워크스페이스 최대 컨텐츠 다운로드 수(횟수)", position = 4, example = "5000")
-    private Long maxDownloadHit;
+    private Long maxDownloadHit = 0L;
     @ApiModelProperty(value = "워크스페이스 최대 통화 시간(Hour)", position = 5, example = "100")
-    private Long maxCallTime;
+    private Long maxCallTime = 0L;
     @ApiModelProperty(value = "워크스페이스 현재 다운로드 횟수 정보", position = 6)
-    private Long currentUsageDownloadHit;
+    private Long currentUsageDownloadHit = 0L;
     @ApiModelProperty(value = "워크스페이스 현재 다운로드 횟수 정보", position = 7)
-    private Long currentUsageStorage;
+    private Long currentUsageStorage = 0L;
     @ApiModelProperty(value = "워크스페이스 현재 다운로드 횟수 정보", position = 8)
-    private Long currentUsageCallTime;
+    private Long currentUsageCallTime = 0L;
     @ApiModelProperty(value = "라이선스 플랜 시작일자", position = 9, example = "2020-05-15T13:00:00")
     private LocalDateTime startDate = LocalDateTime.now();
     @ApiModelProperty(value = "라이선스 플랜 종료일자", position = 10, example = "2020-05-15T13:00:00")
