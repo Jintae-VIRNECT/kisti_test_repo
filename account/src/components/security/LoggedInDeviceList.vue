dc58364bb3afba64951d6cadfa9022b13c6d014a
SangBaek-VIRNECT <lsb@virnect.com>
2020-07-08 18:48:27 +0900

    feat: security page

--- account/src/components/security/LoggedInDeviceList.vue
+++ account/src/components/security/LoggedInDeviceList.vue
@@ -1,34 +1,79 @@
 <template>
   <div class="logged-in-device-list">
-    <el-table :data="loggedInDevices">
-      <column-default :label="$t('security.column.deviceName')" prop="name" />
+    <el-table ref="table" :data="accessLogs">
+      <column-default
+        :label="$t('security.column.deviceName')"
+        prop="device"
+        :dangerouslyUseHTMLString="true"
+      />
       <column-default
         :label="$t('security.column.loginLocation')"
         prop="location"
-        :width="220"
+        :width="200"
       />
-      <column-default
+      <column-date
         :label="$t('security.column.loginDate')"
         prop="loginDate"
-        :width="100"
+        type="time"
+        :width="160"
       />
     </el-table>
+    <searchbar-page
+      ref="page"
+      :value.sync="accessLogsPage"
+      :total="accessLogsTotal"
+    />
   </div>
 </template>
 
 <script>
-import securityService from '@/services/security'
-
-import ColumnDefault from '@/components/common/tableColumn/ColumnDefault'
+import profileService from '@/services/profile'
+import columnMixin from '@/mixins/columns'
+import searchMixin from '@/mixins/search'
 
 export default {
-  components: {
-    ColumnDefault,
-  },
+  mixins: [columnMixin, searchMixin],
   data() {
     return {
-      loggedInDevices: securityService.getLoggedInDeviceList(),
+      accessLogs: [],
+      accessLogsTotal: 0,
+      accessLogsPage: 1,
     }
+  },
+  methods: {
+    changedSearchParams(searchParams) {
+      this.getLoggedInDeviceList(searchParams)
+    },
+    async getLoggedInDeviceList(searchParams) {
+      const { list, total } = await profileService.getLoggedInDeviceList(
+        searchParams,
+      )
+      this.accessLogs = list
+      this.accessLogsTotal = total
+      // 현재기기 텍스트 삽입
+      if (this.accessLogs[0]) {
+        this.accessLogs[0].device += `
+        <span class="now">
+          ${this.$t('security.now')}
+        </span>`
+      }
+    },
+  },
+  beforeMount() {
+    this.getLoggedInDeviceList()
   },
 }
 </script>
+
+<style lang="scss">
+.logged-in-device-list {
+  .now {
+    margin-left: 24px;
+    color: #34aa44;
+    &:before {
+      font-weight: bold;
+      content: '·';
+    }
+  }
+}
+</style>
