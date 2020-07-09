e1e42b3644a38c6db8be09f46a2bacc4020be001
wooka <kakrwoo@naver.com>
2020-07-09 12:09:00 +0900

    Fix: remove dummy data

--- src/components/service/ar/ardrawing/DrawingCanvas.vue
+++ src/components/service/ar/ardrawing/DrawingCanvas.vue
@@ -36,8 +36,6 @@ export default {
       isInit: false,
       canvas: null,
       cursor: null,
-      receiveUndoList: {},
-      receiveRedoList: {},
       undoList: [],
       redoList: [],
     }
