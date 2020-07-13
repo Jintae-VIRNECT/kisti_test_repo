762f00b97c7d807a931e0f5d8580838eaf72c964
SangBaek-VIRNECT <lsb@virnect.com>
2020-07-13 15:15:54 +0900

    feat: setps issue show multiple

--- workstation/src/pages/tasks/_taskId/_subTaskId.vue
+++ workstation/src/pages/tasks/_taskId/_subTaskId.vue
@@ -148,7 +148,7 @@
               <template slot-scope="scope">
                 <el-button
                   v-if="scope.row.issue"
-                  @click="moveToIssue(scope.row.issue.issueId)"
+                  @click="moveToIssue(scope.row.id)"
                 >
                   {{ $t('task.subTaskDetail.showIssue') }}
                 </el-button>
@@ -257,10 +257,8 @@ export default {
     },
     showAll() {},
     showMine() {},
-    moveToIssue(issueId) {
-      this.$router.replace(
-        `${this.$router.currentRoute.path}/issues/${issueId}`,
-      )
+    moveToIssue(stepId) {
+      this.$router.replace(`${this.$router.currentRoute.path}/issues/${stepId}`)
     },
     moveToPaper(paperId) {
       this.$router.replace(
