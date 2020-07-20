cbb03c3820e7b26dde4e3513786bee602784fcd3
wooka <kakrwoo@naver.com>
2020-07-20 17:36:20 +0900

    Fix: recent list tablet view setting

---
+++ src/components/workspace/modules/History.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="widecard" :style="{ height: height }" :class="customClass">
+    <div class="card-item">
+      <profile
+        :image="history.path"
+        :imageAlt="'profileImg'"
+        :mainText="history.title"
+        :subText="`참석자 ${history.participantsCount}명`"
+        :thumbStyle="{ width: '3em', height: '3em' }"
+        :group="true"
+      ></profile>
+    </div>
+
+    <div class="card-item hide-tablet">
+      <div class="label label--noraml">
+        {{ `총 이용시간: ${time}` }}
+      </div>
+    </div>
+    <div class="card-item">
+      <div class="label label--date">
+        {{ date }}
+      </div>
+    </div>
+    <div class="card-item">
+      <div class="label label__icon">
+        <img class="icon" :src="require('assets/image/ic_leader.svg')" />
+        <span class="text">{{ `리더 : ${history.leaderNickName}` }}</span>
+      </div>
+    </div>
+
+    <div class="widecard-tools">
+      <button class="btn small" @click="createRoom(history.roomId)">
+        재시작
+      </button>
+      <popover
+        trigger="click"
+        placement="bottom-start"
+        popperClass="custom-popover"
+      >
+        <button slot="reference" class="widecard-tools__menu-button"></button>
+        <ul class="groupcard-popover">
+          <li>
+            <button
+              class="group-pop__button"
+              @click="openRoomInfo(history.roomId)"
+            >
+              상세 보기
+            </button>
+          </li>
+          <li>
+            <button
+              class="group-pop__button"
+              @click="showDeleteDialog(history.roomId)"
+            >
+              목록삭제
+            </button>
+          </li>
+        </ul>
+      </popover>
+    </div>
+  </div>
+</template>
+
+<script>
+import Profile from 'Profile'
+import Popover from 'Popover'
+export default {
+  name: 'History',
+  components: {
+    Profile,
+    Popover,
+  },
+  props: {
+    history: Object,
+    height: {
+      type: [Number, String],
+      default: 86,
+    },
+    menu: {
+      type: Boolean,
+      default: false,
+    },
+    customClass: {
+      type: Object,
+      default: () => {
+        return {}
+      },
+    },
+    room: {
+      type: Object,
+      default: () => {
+        return {}
+      },
+    },
+  },
+  data() {
+    return {
+      showRoomInfo: false,
+    }
+  },
+  computed: {
+    date() {
+      return this.$dayjs(this.history.startDate).calendar(null, {
+        sameDay: 'A h:mm',
+        lastDay: '[어제]',
+        lastWeek: '[지난주] dddd',
+        sameElse: 'YYYY.MM.DD',
+      })
+    },
+    time() {
+      return this.$dayjs(this.history.totalUseTime * 1000).format('m분 s초')
+    },
+  },
+  methods: {},
+
+  /* Lifecycles */
+  mounted() {},
+}
+</script>
+
+<style lang="scss">
+@import '~assets/style/vars';
+@import '~assets/style/_mixin';
+.widecard {
+  display: flex;
+  // flex-basis: 0px;
+  align-items: center;
+  justify-content: flex-start;
+
+  width: 100%;
+  margin-bottom: 0.571em;
+  padding: 1.571em 2.143em;
+  background-color: $color_darkgray_600;
+  border-radius: 2px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: $color_darkgray_500;
+  }
+  .card-item {
+    flex: 1 1 0;
+    min-width: 0;
+    // flex-basis: 0;
+    // flex-grow: 1;
+    // flex-shrink: 1;
+    // @include tablet {
+    //   flex-grow: 0.5;
+    // }
+    > .btn.small {
+      padding: 0.5em 2.143em;
+    }
+  }
+  .card-item:first-of-type {
+    display: inherit;
+    // flex-grow: 0.8;
+    // min-width: 21.4286rem;
+    padding-right: 0.7143rem;
+  }
+  .card-item:nth-of-type(2) {
+    // flex-grow: 0.8;
+    padding-right: 0.7143rem;
+  }
+  .card-item:nth-last-child(2) {
+    // flex-grow: 0.7;
+    padding-right: 0.7143rem;
+  }
+  .card-item:last-of-type {
+    // flex-grow: 0.6;
+  }
+}
+
+.widecard-tools {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+</style>
--- src/plugins/remote/dayjs/day.js
+++ src/plugins/remote/dayjs/day.js
@@ -2,12 +2,17 @@ import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 import utc from 'dayjs/plugin/utc'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
+import calendar from 'dayjs/plugin/calendar'
+import 'dayjs/locale/ko'
 
 export default {
   install(Vue) {
     dayjs.extend(duration)
     dayjs.extend(utc)
     dayjs.extend(localizedFormat)
+    dayjs.extend(calendar)
+
+    dayjs.locale('ko')
 
     Vue.prototype.$dayjs = dayjs
   },
