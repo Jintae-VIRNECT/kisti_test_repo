0f6738cbf5725cbcbe0f03e6ee33645de6ebdc0f
wooka <kakrwoo@naver.com>
2020-07-22 13:39:13 +0900

    Feat: mic setting, coturn config setting

--- src/stores/remote/modules/settings.js
+++ src/stores/remote/modules/settings.js
@@ -70,6 +70,11 @@ const getters = {
   localRecord: state => state.localRecordInfo,
   allow: state => state.allow,
   language: state => state.language,
+
+  //screen stream for local recording
+  screenStream: state => state.settings.screenStream,
+
+  // used Remote.js
   settingInfo: state => {
     return {
       mic: state.mic.deviceId,
