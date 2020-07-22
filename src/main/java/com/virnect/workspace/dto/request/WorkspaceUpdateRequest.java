7ad4133a9704f9dbfcffba43d231ebc0a9cee39b
jukyoung lee <ljk@virnect.com>
2020-07-22 09:45:58 +0900

    fix: limit text length about workspace create,update request

--- src/main/java/com/virnect/workspace/dto/request/WorkspaceUpdateRequest.java
+++ src/main/java/com/virnect/workspace/dto/request/WorkspaceUpdateRequest.java
@@ -2,6 +2,7 @@ package com.virnect.workspace.dto.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -21,10 +22,12 @@ public class WorkspaceUpdateRequest {
     @NotBlank
     private String userId;
     @NotBlank
+    @Length(min = 0, max=9, message = "워크스페이스 이름은 최대 9자까지 가능합니다.")
     private String name;
 
     private MultipartFile profile;
 
     @NotBlank
+    @Length(min = 0, max=39, message = "워크스페이스 설명은 최대 39자까지 가능합니다.")
     private String description;
 }
