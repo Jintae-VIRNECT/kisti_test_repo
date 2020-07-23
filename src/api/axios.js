e6cf5260e9e27e2b45bf369986e08bd481d1203e
wooka <kakrwoo@naver.com>
2020-07-23 14:00:45 +0900

    Fix: axios access control allow origin

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,10 @@
 import Axios from 'axios'
-import urls from '@/server/urls'
 
 export default Axios.create({
   timeout: process.env.NODE_ENV === 'production' ? 2000 : 10000,
   withCredentials: false,
   headers: {
-    'Access-Control-Allow-Origin': urls.api[process.env.TARGET_ENV],
+    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
   },
 })
