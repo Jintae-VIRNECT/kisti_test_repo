7f4da73fde33b4be3e80bb70329e68e66530a496
ykmo <ykmo@virnect.com>
2020-07-23 15:23:30 +0900

    Refactor:remove unuse mapState

--- src/mixins/localRecorder.js
+++ src/mixins/localRecorder.js
@@ -24,10 +24,8 @@ export default {
       'resolutions',
       'control',
       'localRecordStatus',
+      'roomInfo',
     ]),
-    ...mapState({
-      room: state => state.room,
-    }),
     /**
      * get resolution of main view
      */
@@ -126,7 +124,7 @@ export default {
       config.streams = await this.getStreams()
       config.maxTime = this.localRecord.time
       config.interval = this.localRecord.interval
-      config.roomTitle = this.room.title
+      config.roomTitle = this.roomInfo.title
 
       //get nickname
       if (this.account && this.account.nickname) {
