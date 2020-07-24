ca3a6ef61ee8bc376a0dcf78ba925137af85cf9f
ykmo <ykmo@virnect.com>
2020-07-24 14:18:41 +0900

    Chore:remove comment

--- src/api/workspace/license.js
+++ src/api/workspace/license.js
@@ -22,20 +22,4 @@ export const getLicense = async function({ workspaceId, userId }) {
   } else {
     return true
   }
-
-  //Please - Do not remove.
-  //test1, 2, 3 only has license
-  // if (licenseInfo) {
-  //   const license = licenseInfo.some(info => {
-  //     if (info.productName.indexOf('Remote') >= 0 && info.status === 'USE') {
-  //       return true
-  //     } else {
-  //       return false
-  //     }
-  //   })
-
-  //   return license
-  // } else {
-  //   return false
-  // }
 }
