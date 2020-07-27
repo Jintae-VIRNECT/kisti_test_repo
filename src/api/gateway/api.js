f2e371fd237f7e379bb111b517c1f5022db9fb0f
wooka <kakrwoo@naver.com>
2020-07-27 16:26:47 +0900

    Fix: base gateway change

--- src/api/gateway/api.js
+++ src/api/gateway/api.js
@@ -1,49 +1,52 @@
 export default {
   /* Account */
-  LOGIN: ['POST', '/auth/signin'],
+  LOGIN: ['POST', 'https://192.168.6.3:8073/auth/signin'],
   TOKEN: ['POST', '/auth/oauth/token'],
-  ACCOUNT: ['GET', '3/users/info', { type: 'form' }],
+  ACCOUNT: ['GET', '/users/info', { type: 'form' }],
   // ACCESS_TOKEN: ['POST', '/auth/accessToken'],
 
   /* Workspace - History */
-  GET_HISTORY_LIST: ['GET', window.urls.media + '/media/history'],
-  GET_HISTORY_ITEM: ['GET', window.urls.media + '/media/history/{roomId}'],
-  DELETE_HISTORY_ITEM: ['PUT', window.urls.media + '/media/history/{roomId}'],
-  DELETE_HISTORY_ALL: ['DELETE', window.urls.media + '/media/history'],
+  GET_HISTORY_LIST: ['GET', 'https://192.168.6.4:4443/media/history'],
+  GET_HISTORY_ITEM: ['GET', 'https://192.168.6.4:4443/media/history/{roomId}'],
+  DELETE_HISTORY_ITEM: [
+    'PUT',
+    'https://192.168.6.4:4443/media/history/{roomId}',
+  ],
+  DELETE_HISTORY_ALL: ['DELETE', 'https://192.168.6.4:4443/media/history'],
 
   /* Workspace - Member */
   GET_MEMBER_LIST: ['GET', '/workspaces/{workspaceId}/members?size={size}'],
-  // GET_MEMBER_LIST: ['GET', '/media/member/'],
+  // GET_MEMBER_LIST: ['GET', 'https://192.168.6.4:4443/media/member/'],
 
   /* Workspace - Room */
-  ROOM_LIST: ['GET', window.urls.media + '/media/room?paging={paging}'],
-  ROOM_INFO: ['GET', window.urls.media + '/media/room/{roomId}'],
+  ROOM_LIST: ['GET', 'https://192.168.6.4:4443/media/room?paging={paging}'],
+  ROOM_INFO: ['GET', 'https://192.168.6.4:4443/media/room/{roomId}'],
   UPDATE_ROOM_INFO: [
     'PUT',
-    window.urls.media + '/media/room/{roomId}',
+    'https://192.168.6.4:4443/media/room/{roomId}',
     { type: 'form' },
   ],
   LEAVE_ROOM: [
     'DELETE',
-    window.urls.media + '/media/room/{roomId}/participants/{participantsId}',
+    'https://192.168.6.4:4443/media/room/{roomId}/participants/{participantsId}',
   ],
   PARTICIPANTS_LIST: [
     'GET',
-    window.urls.media + '/media/room/{roomId}/participants',
+    'https://192.168.6.4:4443/media/room/{roomId}/participants',
   ],
   INVITE_PARTICIPANTS_LIST: [
     'GET',
-    window.urls.media + '/media/room/participants',
+    'https://192.168.6.4:4443/media/room/participants',
   ],
   CREATE_ROOM: [
     'POST',
-    window.urls.media + '/media/room?workspaceId={workspaceId}',
+    'https://192.168.6.4:4443/media/room?workspaceId={workspaceId}',
     { type: 'form' },
   ],
-  DELETE_ROOM: ['DELETE', window.urls.media + '/media/room/{roomId}'],
+  DELETE_ROOM: ['DELETE', 'https://192.168.6.4:4443/media/room/{roomId}'],
 
   /* CALL */
-  GET_TOKEN: ['POST', window.urls.media + '/media/tokens'],
+  GET_TOKEN: ['POST', 'https://192.168.6.4:4443/media/tokens'],
 
   /* LICENSE */
   GET_LICENSE: ['GET', '/licenses/{workspaceId}/{userId}'],
