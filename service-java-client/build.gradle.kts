c328189b249758827c2877486770444516074552
hoon <tayesymbol@gmail.com>
2020-07-27 17:09:05 +0900

    docker, jenkine fild added

--- service-java-client/build.gradle.kts
+++ service-java-client/build.gradle.kts
@@ -5,15 +5,3 @@ dependencies {
     implementation(group = "com.google.code.gson", name = "gson", version = "2.8.6")
     implementation(group = "org.slf4j", name = "slf4j-api", version = "1.7.30")
 }
-/*
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-}*/
