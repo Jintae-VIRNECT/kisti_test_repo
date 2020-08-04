dddb5938bdfc1f64e3060c459c02e03178a697d8
SangBaek-VIRNECT <lsb@virnect.com>
2020-08-04 15:53:18 +0900

    feat: console log version

--- account/src/layouts/default.vue
+++ account/src/layouts/default.vue
@@ -39,6 +39,15 @@ export default {
       },
     }
   },
+  mounted() {
+    // 콘솔 표시
+    console.log(
+      `%cVirnect Account v${this.$config.VERSION}`,
+      'font-size: 20px; color: #1468e2',
+    )
+    console.log(`env: ${this.$config.TARGET_ENV}`)
+    console.log(`timeout: ${this.$config.API_TIMEOUT}`)
+  },
 }
 </script>
 
