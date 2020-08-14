33a7f173af0f466cebcce049a6c9ce7745795ea8
SangBaek-VIRNECT <lsb@virnect.com>
2020-08-14 17:18:08 +0900

    feat: mobile view

--- download/src/layouts/error.vue
+++ download/src/layouts/error.vue
@@ -1,23 +1,31 @@
 <template>
-  <!-- Not chrome -->
-  <div v-if="error.message === 'BrowserNotSupport'" class="error-container">
-    <browser-not-support />
-  </div>
-  <!-- 404 -->
-  <div v-else-if="error.statusCode === 404" class="error-container">
-    <common-error />
-  </div>
-  <!-- 504 -->
-  <div v-else-if="error.statusCode === 504" class="error-container">
-    <network-error :code="error.statusCode" :message="error.message" />
-  </div>
-  <!-- 500 -->
-  <div v-else class="error-container">
-    <common-error :code="error.statusCode" :message="error.message" />
+  <div>
+    <the-header :showSection="showSection">
+      <template slot="subTitle">
+        {{ $t('home.title') }}
+      </template>
+    </the-header>
+    <!-- Not chrome -->
+    <div v-if="error.message === 'BrowserNotSupport'" class="error-container">
+      <browser-not-support />
+    </div>
+    <!-- 404 -->
+    <div v-else-if="false" class="error-container">
+      <common-error />
+    </div>
+    <!-- 504 -->
+    <div v-else-if="error.statusCode === 504" class="error-container">
+      <network-error :code="error.statusCode" :message="error.message" />
+    </div>
+    <!-- 500 -->
+    <div v-else class="error-container">
+      <common-error :code="error.statusCode" :message="error.message" />
+    </div>
   </div>
 </template>
 
 <script>
+import TheHeader from 'WC-Modules/vue/components/header/TheHeader'
 import CommonError from 'WC-Modules/vue/components/errors/CommonError'
 import NetworkError from 'WC-Modules/vue/components/errors/NetworkError'
 import BrowserNotSupport from 'WC-Modules/vue/components/errors/BrowserNotSupport'
@@ -25,16 +33,30 @@ import BrowserNotSupport from 'WC-Modules/vue/components/errors/BrowserNotSuppor
 export default {
   layout: 'empty',
   components: {
+    TheHeader,
     CommonError,
     BrowserNotSupport,
     NetworkError,
   },
   props: ['error'],
+  data() {
+    return {
+      showSection: {
+        profile: true,
+        link: true,
+      },
+    }
+  },
 }
 </script>
 
 <style lang="scss">
 .error-container {
   padding-top: 35px;
+}
+@media (max-width: $mobile) {
+  .error-container {
+    padding-top: 0;
+  }
 }
 </style>
