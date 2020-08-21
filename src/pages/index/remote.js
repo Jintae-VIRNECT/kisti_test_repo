400416105b95fbf5fadb40b7775d716c1b345c7c
ykmo <ykmo@virnect.com>
2020-08-21 21:01:01 +0900

    Feat:add scroll

--- src/pages/index/remote.js
+++ src/pages/index/remote.js
@@ -3,9 +3,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import Vue2Scrollbar from 'plugins/remote/scrollbar'
+
+Vue.use(Vue2Scrollbar)
 
 // Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+const EventBus = new Vue()
+Vue.prototype.$eventBus = EventBus
 
 const app = new Vue({
   // router,
