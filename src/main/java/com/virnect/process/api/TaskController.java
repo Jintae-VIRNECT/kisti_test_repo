140a15bcc63d0f62d8757fe76328f125cbcec73f
jukyoung lee <ljk@virnect.com>
2020-08-28 10:00:01 +0900

    docs: update releasenote  v1.2.20

--- src/main/java/com/virnect/process/api/TaskController.java
+++ src/main/java/com/virnect/process/api/TaskController.java
@@ -13,7 +13,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
