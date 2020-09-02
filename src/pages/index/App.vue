84dafd4a2a1293c979a4e688bb33c55d75d6469e
ykmo <ykmo@virnect.com>
2020-09-02 09:10:04 +0900

    Feat:hide footer

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <remote-layout />
-    <remote-footer></remote-footer>
+    <!-- <remote-footer></remote-footer> -->
   </div>
 </template>
 
 <script>
 import RemoteLayout from 'components/layout/RemoteLayout'
-import RemoteFooter from 'components/footer/RemoteFooter'
+// import RemoteFooter from 'components/footer/RemoteFooter'
 
 export default {
   name: 'App',
   components: {
     RemoteLayout,
-    RemoteFooter,
+    // RemoteFooter,
   },
 }
 </script>
