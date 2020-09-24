f062e9ac84b827106c43ec97f3e4730aafa67b0d
JeognHyeon-VIRNECT <sky456139@virnect.com>
2020-09-24 15:48:52 +0900

    feat: enable spring cloud config

--- src/main/java/com/virnect/eureka/EurekaApplication.java
+++ src/main/java/com/virnect/eureka/EurekaApplication.java
@@ -2,8 +2,13 @@ package com.virnect.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.event.EventListener;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
@@ -12,4 +17,16 @@ public class EurekaApplication {
         SpringApplication.run(EurekaApplication.class, args);
     }
 
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void applicationReady() {
+		String msg = "\n\n----------------------------------------------------\n" + "\n"
+			+ "   Platform Eureka Server is ready!\n"
+			+ "   ---------------------------\n" + "\n"
+			+ "   * Server Host: [" + System.getenv("eureka.instance.ip-address") + "]\n" + "\n"
+			+ "   * VIRNECT_ENV: [" + System.getenv("VIRNECT_ENV") + "]\n" + "\n"
+			+ "   * Config Server Url: [" + System.getenv("CONFIG_SERVER") + "]\n" + "\n"
+			+ "----------------------------------------------------\n";
+		log.info(msg);
+	}
 }
