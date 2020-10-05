65bf31a0ff1491a72beb2c856b478f38cb0657b4
jukyoung lee <ljk@virnect.com>
2020-10-05 17:37:27 +0900

    feat: add onpremise config, apply minio

--- src/main/java/com/virnect/process/global/error/ErrorCode.java
+++ src/main/java/com/virnect/process/global/error/ErrorCode.java
@@ -11,71 +11,75 @@ import io.swagger.annotations.ApiModel;
  */
 @ApiModel
 public enum ErrorCode {
-    // 공정 관련
-    ERR_OWNERSHIP(5001, "An error occurred in the request. Because it is NOT ownership."),
-    ERR_CAN_CREATE_PROCESS(5003, "The content can create process."),    // make에서 로직처리 되어 있음. 컨텐츠 업데이트가 가능한 메시지
-    ERR_NOT_FOUND_PROCESS(5004, "Could not found process."),
-    ERR_NOT_FOUND_PROCESS_FOR_PROCESS_METADATA(5005, "The requested process id could not be found."),
-    ERR_NOT_FOUND_SUBPROCESS_FOR_PROCESS_METADATA(5006, "The requested sub-process id could not be found."),
-    ERR_NOT_FOUND_PROCESS_OF_TARGET(5007, "Could not found process of target. or This process state was CLOSED, DELETED"),
-    ERR_HAS_ALREADY_PROCESS_OF_THIS_CONTENT(5008, "The content has already been created for the process. Please register new content."),        // 워크스테이션 후처리 있음.
+	// 공정 관련
+	ERR_OWNERSHIP(5001, "An error occurred in the request. Because it is NOT ownership."),
+	ERR_CAN_CREATE_PROCESS(5003, "The content can create process."),    // make에서 로직처리 되어 있음. 컨텐츠 업데이트가 가능한 메시지
+	ERR_NOT_FOUND_PROCESS(5004, "Could not found process."),
+	ERR_NOT_FOUND_PROCESS_FOR_PROCESS_METADATA(5005, "The requested process id could not be found."),
+	ERR_NOT_FOUND_SUBPROCESS_FOR_PROCESS_METADATA(5006, "The requested sub-process id could not be found."),
+	ERR_NOT_FOUND_PROCESS_OF_TARGET(
+		5007, "Could not found process of target. or This process state was CLOSED, DELETED"),
+	ERR_HAS_ALREADY_PROCESS_OF_THIS_CONTENT(
+		5008,
+		"The content has already been created for the process. Please register new content."
+	),        // 워크스테이션 후처리 있음.
 
-    ERR_NOT_FOUND_SUBPROCESS(5011, "Could not found sub-process."),
-    ERR_PROCESS_UPDATED(5012, "Process update Failed."),
-    ERR_SUBPROCESS_UPDATED(5013, "SubProcess update Failed."),
-    ERR_NOT_FOUND_SUBPROCESS_WORKER(5014, "Could not found sub-process worker."),
+	ERR_NOT_FOUND_SUBPROCESS(5011, "Could not found sub-process."),
+	ERR_PROCESS_UPDATED(5012, "Process update Failed."),
+	ERR_SUBPROCESS_UPDATED(5013, "SubProcess update Failed."),
+	ERR_NOT_FOUND_SUBPROCESS_WORKER(5014, "Could not found sub-process worker."),
 
-    ERR_NOT_FOUND_JOB(5021, "Could not found job."),
+	ERR_NOT_FOUND_JOB(5021, "Could not found job."),
 
-    ERR_NOT_FOUND_REPORT(5071, "Not found report."),
-    ERR_NOT_FOUND_REPORT_ITEM(5072, "Not found report item."),
+	ERR_NOT_FOUND_REPORT(5071, "Not found report."),
+	ERR_NOT_FOUND_REPORT_ITEM(5072, "Not found report item."),
 
-    ERR_NOT_FOUND_ISSUE(5091, "Not found issue from this issue_id."),
+	ERR_NOT_FOUND_ISSUE(5091, "Not found issue from this issue_id."),
 
-    ERR_PROCESS_WORK_RESULT_SYNC(5100, "Process work result synchronization failed."),
-    ERR_WORKER_NOT_EQUAL_SYNC(5101, "Process work result synchronization failed. Doesn't Match syncUser."),
+	ERR_PROCESS_WORK_RESULT_SYNC(5100, "Process work result synchronization failed."),
+	ERR_WORKER_NOT_EQUAL_SYNC(5101, "Process work result synchronization failed. Doesn't Match syncUser."),
 
-    ERR_UNSUPPORTED_FILE_EXTENSION(5801, "Content upload file. Unsupported extensions."),
-    ERR_FILE_UPLOAD(5802, "File upload fail."),
+	ERR_UNSUPPORTED_FILE_EXTENSION(5801, "Content upload file. Unsupported extensions."),
+	ERR_FILE_UPLOAD(5802, "File upload fail."),
+	ERR_FILE_DELETE(5803, "File delete fail."),
 
-    ERR_PROCESS_REGISTER(5901, "Process Registration failed."),
-    ERR_SUB_PROCESS_REGISTER(5902, "SubProcess Registration failed."),
-    ERR_JOB_REGISTER(5903, "Job Registration failed."),
-    ERR_REPORT_REGISTER(5904, "Report Registration failed."),
-    ERR_TARGET_REGISTER(5905, "Target Registration failed."),
-    ERR_DELETE_PROCES(5906, "Process Deletion failed."),
-    ERR_SUB_PROCESS_REGISTER_NO_WORKER(5907, "SubProcess Registration failed. No worker selected."),
+	ERR_PROCESS_REGISTER(5901, "Process Registration failed."),
+	ERR_SUB_PROCESS_REGISTER(5902, "SubProcess Registration failed."),
+	ERR_JOB_REGISTER(5903, "Job Registration failed."),
+	ERR_REPORT_REGISTER(5904, "Report Registration failed."),
+	ERR_TARGET_REGISTER(5905, "Target Registration failed."),
+	ERR_DELETE_PROCES(5906, "Process Deletion failed."),
+	ERR_SUB_PROCESS_REGISTER_NO_WORKER(5907, "SubProcess Registration failed. No worker selected."),
 
-    ERR_NO_CONTENT_TARGET(5920, "Not found Content Target. Transform failed."),
-    ERR_ALREADY_TRANSFORMED(5921, "This Content is already Transformed. Transform failed"),
-    ERR_NOT_FOUND_TARGET(5922, "Could not found Target Data. Check your Target Data."),
-    ERR_OVER_MAX_TARGET(5923, "One Contents is mapped One Target Data."),
+	ERR_NO_CONTENT_TARGET(5920, "Not found Content Target. Transform failed."),
+	ERR_ALREADY_TRANSFORMED(5921, "This Content is already Transformed. Transform failed"),
+	ERR_NOT_FOUND_TARGET(5922, "Could not found Target Data. Check your Target Data."),
+	ERR_OVER_MAX_TARGET(5923, "One Contents is mapped One Target Data."),
 
-    // 공통 에러
-    ERR_INVALID_REQUEST_PARAMETER(8001, "Invalid request parameter cause api errors");
+	// 공통 에러
+	ERR_INVALID_REQUEST_PARAMETER(8001, "Invalid request parameter cause api errors");
 
-    private int code;
-    private String message;
+	private int code;
+	private String message;
 
-    ErrorCode(final int code, final String message) {
-        this.code = code;
-        this.message = message;
-    }
+	ErrorCode(final int code, final String message) {
+		this.code = code;
+		this.message = message;
+	}
 
-    public int getCode() {
-        return code;
-    }
+	public int getCode() {
+		return code;
+	}
 
-    public String getMessage() {
-        return message;
-    }
+	public String getMessage() {
+		return message;
+	}
 
-
-    @Override
-    public String toString() {
-        return "ErrorCode{" +
-                "code=" + code +
-                ", message='" + message + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "ErrorCode{" +
+			"code=" + code +
+			", message='" + message + '\'' +
+			'}';
+	}
 }
---
+++ src/main/java/com/virnect/process/infra/file/MinioUploadService.java
@@ -0,0 +1,130 @@
+package com.virnect.process.infra.file;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.util.Base64;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidBucketNameException;
+import io.minio.errors.InvalidResponseException;
+import io.minio.errors.ServerException;
+import io.minio.errors.XmlParserException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import com.virnect.process.exception.ProcessServiceException;
+import com.virnect.process.global.error.ErrorCode;
+
+/**
+ * Project: PF-ProcessManagement
+ * DATE: 2020-10-05
+ * AUTHOR: jkleee (Jukyoung Lee)
+ * EMAIL: ljk@virnect.com
+ * DESCRIPTION:
+ */
+@Slf4j
+@Profile({"local", "develop", "onpremise"})
+@Component
+@RequiredArgsConstructor
+public class MinioUploadService implements FileUploadService {
+	private final MinioClient minioClient;
+
+	private static String CONTENT_DIRECTORY = "contents";
+	private static String REPORT_DIRECTORY = "report";
+	private static String REPORT_FILE_EXTENSION = ".jpg";
+
+	@Value("${minio.bucket}")
+	private String bucketName;
+
+	@Value("${minio.bucket-resource}")
+	private String bucketResource;
+
+	@Override
+	public String upload(MultipartFile file) throws IOException {
+		return null;
+	}
+
+	@Override
+	public String upload(MultipartFile file, String fileName) throws IOException {
+		return null;
+	}
+
+	@Override
+	public boolean delete(String url) {
+
+		String objectName = bucketResource + REPORT_DIRECTORY + "/" + FilenameUtils.getName(url);
+		RemoveObjectArgs removeObjectArgs = RemoveObjectArgs.builder()
+			.bucket(bucketName)
+			.object(objectName)
+			.build();
+		try {
+			minioClient.removeObject(removeObjectArgs);
+			log.info(FilenameUtils.getName(url) + " 파일이 삭제되었습니다.");
+		} catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidBucketNameException | InvalidKeyException | InvalidResponseException | IOException |
+			NoSuchAlgorithmException | ServerException | XmlParserException exception) {
+			log.error(exception.getMessage());
+			throw new ProcessServiceException(ErrorCode.ERR_FILE_DELETE);
+		}
+
+		return true;
+	}
+
+	@Override
+	public String getFileExtension(String originFileName) {
+		return null;
+	}
+
+	@Override
+	public boolean isAllowFileExtension(String fileExtension) {
+		return false;
+	}
+
+	@Override
+	public File getFile(String url) {
+		return null;
+	}
+
+	@Override
+	public String base64ImageUpload(String base64Image) {
+		try {
+			byte[] image = Base64.getDecoder().decode(base64Image);
+			String randomFileName = String.format(
+				"%s_%s%s", LocalDate.now().toString(), RandomStringUtils.randomAlphanumeric(10).toLowerCase(),
+				REPORT_FILE_EXTENSION
+			);
+			String objectName = String.format("%s%s/%s", bucketResource, REPORT_DIRECTORY, randomFileName);
+
+			InputStream inputStream = new ByteArrayInputStream(image);
+			PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+				.bucket(bucketName)
+				.object(objectName)
+				.contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE)
+				.stream(inputStream, image.length, -1)
+				.build();
+			minioClient.putObject(putObjectArgs);
+			return minioClient.getObjectUrl(bucketName, objectName);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			throw new ProcessServiceException(ErrorCode.ERR_FILE_UPLOAD);
+		}
+	}
+
+}
