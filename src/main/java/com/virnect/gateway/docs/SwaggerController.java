49dd721b108b73a8221fa5e48dcd992c8be991f8
JeognHyeon-VIRNECT <sky456139@virnect.com>
2020-10-06 17:30:01 +0900

    swagger enable except staging and production env. add ssl certification file and copy command on Dockerfile. and change GC Model Parallel GC to G1 GC

--- src/main/java/com/virnect/gateway/docs/SwaggerController.java
+++ src/main/java/com/virnect/gateway/docs/SwaggerController.java
@@ -26,7 +26,7 @@ import springfox.documentation.swagger.web.UiConfigurationBuilder;
  * @description
  * @since 2020.04.10
  */
-@Profile({"local", "develop","onpremise"})
+@Profile({"!staging", "!production"})
 @Controller
 @RequiredArgsConstructor
 public class SwaggerController {
