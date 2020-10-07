399a6b2b24376a821505a0d1d8b17806c7ed4532
ykmo <ykmo@virnect.com>
2020-10-07 19:00:08 +0900

    Feat:add server config

--- route.js
+++ route.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const config = require('./server/config')
+const url = require('url')
 
 router.get('/healthcheck', function(req, res) {
   res.send('200')
+})
+
+router.get('/urls', function(req, res) {
+  res.header('Content-Type', 'application/json')
+  res.send(JSON.stringify(config.getUrls()))
 })
 
 router.get('/*', function(req, res) {
