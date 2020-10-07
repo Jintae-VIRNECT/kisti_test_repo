7a379b0e88a0fa8a0e6c69685eaad3b5bf8db381
ykmo <ykmo@virnect.com>
2020-10-07 11:11:00 +0900

    Chore:update alias path

--- vue.config.js
+++ vue.config.js
@@ -15,13 +15,15 @@ module.exports = {
     resolve: {
       modules: ['node_modules', 'modules'],
       alias: {
-        '@': path.join(__dirname, '/src'),
+        '@': path.join(__dirname, '../'),
         components: path.join(__dirname, '/src/components'),
         assets: path.join(__dirname, '/src/assets'),
         plugins: path.join(__dirname, '/src/plugins'),
         api: path.join(__dirname, '/src/api'),
         utils: path.join(__dirname, '/src/utils'),
         mixins: path.join(__dirname, '/src/mixins'),
+        configs: path.join(__dirname, '/src/configs'),
+        stores: path.join(__dirname, '/src/stores'),
       },
     },
   },
