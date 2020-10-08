a02bec4ece50bac08f8cf71fe89dd7b179e74ca7
ykmo <ykmo@virnect.com>
2020-10-08 19:01:41 +0900

    Feat:add tab

---
+++ src/components/partials/TabButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <li class="dashboard-tab__button">
+    <button
+      :class="{ active: active, nolicense: !(hasLicense && !expireLicense) }"
+    >
+      {{ text }}
+    </button>
+  </li>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'TabButton',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapGetters(['expireLicense']),
+  },
+  watch: {},
+  methods: {},
+
+  /* Lifecycles */
+  mounted() {},
+}
+</script>
