c03f3f647702f752112a6baabcb63d57575d6a4a
Sunghyun-VIRNECT <58674642+Sunghyun-VIRNECT@users.noreply.github.com>
2020-10-13 12:29:29 +0900

    feat: local url set

--- configs/runtime.js
+++ configs/runtime.js
@@ -6,6 +6,18 @@ const axios = require('axios')
 let envConfig = {}
 let urlConfig = {}
 
+const localUrls = {
+	console: 'https://192.168.13.72:8888',
+	api: 'https://192.168.6.3:8073',
+	// api: 'http://192.168.13.34:8085',
+	accout: 'https://localhost:8822',
+	workstation: 'https://localhost:8878',
+	download: 'https://localhost:8833',
+	remote: 'https://localhost:8886',
+	pay: 'https://192.168.13.72:7070',
+	env: 'local',
+}
+
 class Config {
 	constructor() {}
 
@@ -17,15 +29,20 @@ class Config {
 	}
 
 	async init() {
-		const { data } = await axios.get(
-			`${configServer}/web-url/${env === 'local' ? 'develop' : env}`,
-		)
-		const res = await axios.get(
-			`${configServer}/login-web/${env === 'local' ? 'develop' : env}`,
-		)
-		urlConfig = data.propertySources[0].source
-		envConfig = res.data.propertySources[0].source
-		urlConfig.env = data.profiles[0]
+		if (env === 'local') {
+			urlConfig = localUrls
+			envConfig = localUrls.env
+		} else {
+			const { data } = await axios.get(
+				`${configServer}/web-url/${env === 'local' ? 'develop' : env}`,
+			)
+			const res = await axios.get(
+				`${configServer}/login-web/${env === 'local' ? 'develop' : env}`,
+			)
+			urlConfig = data.propertySources[0].source
+			envConfig = res.data.propertySources[0].source
+			urlConfig.env = data.profiles[0]
+		}
 		return this
 	}
 }
