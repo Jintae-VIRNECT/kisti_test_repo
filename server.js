c674baf518902fc046f6e0fa76d0ee36e85e556b
Sunghyun-VIRNECT <58674642+Sunghyun-VIRNECT@users.noreply.github.com>
2020-10-13 16:10:13 +0900

    fix: check

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const https = require('https')
 const fs = require('fs')
 const path = require('path')
 const route = require('./route')
-const env = process.env.NODE_ENV
+const env = process.env.VIRNECT_ENV ? process.env.VIRNECT_ENV : process.env.NODE_ENV
 const config = require('./configs/runtime')
 
 app.use(express.static(path.join(__dirname, './dist')))
@@ -15,8 +15,9 @@ app.use(route)
 ;(async () => {
 	await config.init()
 	const envSet = config.envConfig
+	console.log(env)
 	if (/local|develop|onpremise/.test(env)) {
-		console.log(envSet)
+		console.log(config.urlConfig)
 		const options = {
 			key: fs.readFileSync('./cert/virnect.key'),
 			cert: fs.readFileSync('./cert/virnect.crt')
