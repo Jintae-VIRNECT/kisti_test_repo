07aa2a1927602c5553823a74e8788ae1200fc5cd
ykmo <ykmo@virnect.com>
2020-10-13 19:08:07 +0900

    Feat:add mixins and plugins

---
+++ src/mixins/toast.js
@@ -0,0 +1,40 @@
+export default {
+  methods: {
+    toastError(message) {
+      this.$toasted.error(message, {
+        position: 'bottom-center',
+        duration: 5000,
+        action: {
+          icon: 'close',
+          onClick: (e, toastObject) => {
+            toastObject.goAway(0)
+          },
+        },
+      })
+    },
+    toastNotify(message) {
+      this.$toasted.show(message, {
+        position: 'bottom-center',
+        duration: 5000,
+        action: {
+          icon: 'close',
+          onClick: (e, toastObject) => {
+            toastObject.goAway(0)
+          },
+        },
+      })
+    },
+    toastDefault(message, options) {
+      this.$toasted.clear()
+      this.$toasted.show(message, {
+        position: 'top-center',
+        className: ['remote-toast'],
+        type: 'info',
+        duration: 2000,
+        action: null,
+        containerClass: 'toast-default',
+        ...options,
+      })
+    },
+  },
+}
