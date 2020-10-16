499f96454677742c12496797fe7570bea08b281e
ykmo <ykmo@virnect.com>
2020-10-16 16:31:44 +0900

    Feat:add filter

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -32,6 +32,52 @@ export default {
         return '약함'
       }
     },
+    convertTime(duration) {
+      let sec_num = Number.parseInt(duration, 10)
+      let hours = Math.floor(sec_num / 3600)
+      let minutes = Math.floor((sec_num - hours * 3600) / 60)
+      let seconds = sec_num - hours * 3600 - minutes * 60
+
+      let hText = '시 '
+      let mText = '분 '
+      let sText = '초'
+
+      if (hours === 0 && minutes === 0 && seconds < 1) {
+        hours = ''
+        hText = ''
+
+        minutes = ''
+        mText = ''
+
+        seconds = '0'
+      } else {
+        if (hours === 0) {
+          hours = ''
+          hText = ''
+        }
+        if (minutes === 0) {
+          minutes = ''
+          mText = ''
+        }
+        if (seconds === 0) {
+          seconds = ''
+          sText = ''
+        }
+      }
+
+      return hours + hText + minutes + mText + seconds + sText
+    },
+    convertSize(size) {
+      const mb = 1048576
+
+      if (size >= mb) {
+        size = size / 1024 / 1024
+        return `${size.toFixed(1)}MB`
+      } else {
+        size = size / 1024
+        return `${size.toFixed(1)}KB`
+      }
+    },
   },
   computed: {
     ...mapGetters(['account', 'workspace', 'deviceType', 'hasLicense']),
