387a5f7ea2f8ba53673cc70c561faf4673abb989
ykmo <ykmo@virnect.com>
2020-10-19 19:31:00 +0900

    Feat:change chart

--- src/components/section/BoardDaily.vue
+++ src/components/section/BoardDaily.vue
@@ -1,50 +1,72 @@
 <template>
-  <section class="board-daily">
-    <div class="board-daily__header">
-      <span class="board-daily__header--title">일일 협업</span>
-      <span class="board-daily__header--description">
-        설정한 날짜의 일일 협업 내용을 보여줍니다.
-      </span>
-      <datepicker
-        class="board-daily__header--datepicker"
-        :pickerName="'daily'"
-        :minimumView="'day'"
-        :maximumView="'day'"
-      ></datepicker>
-    </div>
-    <div class="board-daily__body">
-      <card :customClass="'custom-card-chart'">
-        <div name="header">asdf</div>
-        <div class="chart-holder">
-          <canvas id="myChart" width="890" height="260"></canvas></div
-      ></card>
-      <card-board
-        :count="5"
-        :countDescription="'일별 총 협업 수'"
-        :time="5219611"
-        :timeDescription="'일별 총 협업 시간'"
-      ></card-board>
+  <section class="board">
+    <card :customClass="'custom-card-chart'">
+      <div name="header" class="board__header">
+        <span class="board__header--title">일일 협업</span>
+        <span class="board__header--description">
+          설정한 날짜의 일일 협업 내용을 보여줍니다.
+        </span>
+        <datepicker
+          class="board__header--datepicker"
+          :pickerName="'daily'"
+          :minimumView="'day'"
+          :maximumView="'day'"
+        ></datepicker>
+      </div>
+      <div class="chart-legend">
+        <chart-legend text="개인 협업 내역" shape="round"></chart-legend>
+        <chart-legend
+          text="전체 협업 내역"
+          shape="round"
+          customClass="grey"
+        ></chart-legend>
+      </div>
+      <div class="chart-holder">
+        <canvas id="chart-dayily" width="1145" height="230"></canvas></div
+    ></card>
+    <div class="board-figures">
+      <figure-board
+        header="전체 일별 협업 수"
+        :count="48"
+        :imgSrc="require('assets/image/figure/ic_figure_calendar.svg')"
+      ></figure-board>
+      <figure-board
+        header="전체 일별 협업 시간"
+        :time="999999"
+        :imgSrc="require('assets/image/figure/ic_figure_date_all.svg')"
+      ></figure-board>
+      <figure-board
+        header="나의 일별 협업 수"
+        :onlyMe="true"
+        :count="999999"
+        :imgSrc="require('assets/image/figure/ic_figure_chart.svg')"
+      ></figure-board>
+      <figure-board
+        header="나의 일별 협업 시간"
+        :onlyMe="true"
+        :time="999999"
+        :imgSrc="require('assets/image/figure/ic_figure_date_time.svg')"
+      ></figure-board>
     </div>
   </section>
 </template>
 
 <script>
 import Card from 'Card'
-import CardBoard from 'CardBoard'
 import Datepicker from 'Datepicker'
 import Chart from 'chart.js'
+import ChartLegend from 'Legend'
+import FigureBoard from 'FigureBoard'
 export default {
   name: 'BoardDaily',
   components: {
     Card,
-    CardBoard,
+    ChartLegend,
+    FigureBoard,
     Datepicker,
   },
   mounted() {
-    const ctx = document.getElementById('myChart').getContext('2d')
-    const gradientFill = ctx.createLinearGradient(0, 500, 0, 100)
-    gradientFill.addColorStop(1, 'rgba(137,187,250, 1)')
-    gradientFill.addColorStop(0, 'rgba(255, 255, 255, 0.6)')
+    const ctx = document.getElementById('chart-dayily').getContext('2d')
 
     const custom = function(tooltipModel) {
       // Tooltip Element
@@ -81,19 +103,25 @@ export default {
         let titleLines = tooltipModel.title || []
         let bodyLines = tooltipModel.body.map(getBody)
 
-        let innerHtml = '<thead>'
+        let innerHtml = '<thead class="chartjs-tooltip-head">'
 
         titleLines.forEach(function(title) {
-          innerHtml += '<tr><th>' + title + '</th></tr>'
+          innerHtml += '<tr><th><p>' + title + '</p></th></tr>'
         })
         innerHtml += '</thead><tbody>'
 
         bodyLines.forEach(function(body, i) {
+          console.log(tooltipModel)
           let colors = tooltipModel.labelColors[i]
           let style = 'background:' + colors.backgroundColor
           style += '; border-color:' + colors.borderColor
           style += '; border-width: 1px'
-          let span = '<span style="' + style + '"></span>'
+          let span =
+            '<span class="tooil-tip-legend" style="display:inline-block; background:#fff; border-radius: 50%; border: 3px solid' +
+            colors.borderColor +
+            '"></span><span style="' +
+            style +
+            '"></span>'
           innerHtml += '<tr><td>' + span + body + '</td></tr>'
         })
         innerHtml += '</tbody>'
@@ -118,40 +146,124 @@ export default {
         tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px'
       tooltipEl.style.pointerEvents = 'none'
     }
-    console.log(custom)
+
     let myChart = new Chart(ctx, {
       type: 'line',
       data: {
-        labels: ['06', '08', '10', '12', '14', '16', '18', '20', '22', '24'],
+        labels: [
+          '00',
+          '01',
+          '02',
+          '03',
+          '04',
+          '05',
+          '06',
+          '07',
+          '08',
+          '09',
+          '10',
+          '11',
+          '12',
+          '13',
+          '14',
+          '15',
+          '16',
+          '17',
+          '18',
+          '19',
+          '20',
+          '21',
+          '22',
+          '23',
+        ],
         datasets: [
           {
-            label: '',
-            data: [0, 0, 2, 0, 6, 8, 2, 0, 0, 0],
-            backgroundColor: gradientFill,
+            label: '개인 협업 내역',
+            data: [
+              0,
+              2,
+              4,
+              5,
+              10,
+              8,
+              2,
+              0,
+              0,
+              4,
+              0,
+              5,
+              6,
+              5,
+              0,
+              1,
+              2,
+              3,
+              4,
+              0,
+              0,
+              0,
+              0,
+              0,
+            ],
             borderColor: '#0f75f5',
             borderWidth: 4,
-            pointRadius: 4,
+            pointRadius: 0,
             pointBackgroundColor: '#fff',
-            fill: 'start',
             borderJoinStyle: 'bevel',
             lineTension: 0,
+            fill: false,
+          },
+          {
+            label: '전체 협업 내역',
+            data: [
+              0,
+              2,
+              4,
+              5,
+              10,
+              8,
+              2,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              0,
+              20,
+              22,
+              25,
+              0,
+              0,
+              0,
+              0,
+              0,
+            ],
+            borderColor: '#bbc8d9',
+            borderWidth: 4,
+            pointRadius: 0,
+            pointBackgroundColor: '#fff',
+            borderJoinStyle: 'bevel',
+            lineTension: 0,
+            fill: false,
           },
         ],
       },
       options: {
         hover: {
-          mode: 'index',
+          mode: 'nearest',
           intersect: false,
         },
         tooltips: {
-          mode: 'index',
+          mode: 'nearest',
           intersect: false,
           position: 'average',
           enabled: false,
           custom: custom,
           titleFontSize: '15',
           bodyFontSize: '14',
-          // caretSize: 7,
           displayColors: false,
           backgroundColor: '#516277',
           bodyFontStyle: 'bold',
@@ -179,9 +291,8 @@ export default {
             {
               ticks: {
                 beginAtZero: true,
-                max: 10,
                 min: 0,
-                stepSize: 2,
+                stepSize: 4,
               },
               gridLines: {
                 borderDash: [1, 2],
@@ -197,61 +308,14 @@ export default {
 </script>
 
 <style lang="scss">
-.board-daily {
-  height: 371px;
-  margin-top: 40px;
-}
-
-.board-daily__header {
-  position: relative;
-  height: 60px;
-}
-
-.board-daily__header--title {
-  margin-right: 10px;
-  color: rgb(11, 31, 72);
-  font-weight: 500;
-  font-size: 22px;
-}
-
-.board-daily__header--description {
-  color: rgb(122, 122, 122);
-  font-weight: 500;
-  font-size: 15px;
-}
-
-.board-daily__header--datepicker {
-  &.datepicker {
-    position: absolute;
-    top: 0px;
-    right: 0px;
-  }
-}
-
-.board-daily__body {
-  display: flex;
-}
-
-.custom-card-chart {
-  width: 968px;
-  height: 310px;
-  margin-right: 12px;
-}
-
-.chart-holder {
-  position: relative;
-  width: 64.2857rem;
-  height: 19.2857rem;
-  padding-left: 30px;
-}
-
 #chartjs-tooltip {
   position: absolute;
-  width: 132px;
-  height: 69px;
+  width: 128px;
+  height: 80px;
+  // min-height: 40px;
   color: white;
-  background: rgb(81, 98, 119);
-  border: 1px solid rgb(52, 65, 81);
+  background: #3b3b3b;
+  // border: 1px solid rgb(52, 65, 81);
   border-radius: 4px;
   -webkit-transform: translate(-50%, 0);
   transform: translate(-50%, 0);
@@ -262,30 +326,45 @@ export default {
 
   table {
     position: relative;
+    width: 100%;
+
+    thead {
+      color: rgb(255, 255, 255);
+      font-weight: 400;
+      font-size: 15px;
+    }
 
     &::after {
       position: absolute;
-      top: 60px;
-      right: 31px;
+      top: 73px;
+      right: 50px;
       z-index: 999;
       width: 0;
       height: 0;
-      border-top: 9px solid rgb(81, 98, 119);
+      border-top: 9px solid #3b3b3b;
       border-right: 9px solid transparent;
       border-left: 9px solid transparent;
       content: '';
     }
+  }
 
+  .tooil-tip-legend {
+    width: 10px;
+    height: 10px;
+    margin-right: 5px;
+    margin-left: 5px;
+  }
+}
+
+.chart-legend {
+  display: flex;
+  align-items: center;
+  height: 68px;
+  padding-left: 30px;
+
+  & > .legend.round.grey {
     &::before {
-      position: absolute;
-      top: 60px;
-      right: 29px;
-      width: 0;
-      height: 0;
-      border-top: 11px solid rgb(52, 65, 81);
-      border-right: 11px solid transparent;
-      border-left: 11px solid transparent;
-      content: '';
+      border: 4px solid #bbc8d9;
     }
   }
 }
