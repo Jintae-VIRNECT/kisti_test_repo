e2696cf538757dae35fd4bcf47bc834d27447a68
ykmo <ykmo@virnect.com>
2020-10-19 19:31:53 +0900

    Feat:add chart legend

---
+++ src/components/modules/Legend.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="legend" :class="[shape, customClass]">
+    <p class="legend--text">
+      {{ text }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Legend',
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    shape: {
+      type: String,
+      default: 'round',
+      validate(value) {
+        return ['round', 'square'].indexOf(value) >= 0
+      },
+    },
+    customClass: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {},
+}
+</script>
+
+<style lang="scss">
+.legend {
+  position: relative;
+  margin-right: 22px;
+
+  &.round {
+    &::before {
+      position: absolute;
+      top: 5px;
+      left: 6px;
+      width: 5px;
+      height: 5px;
+      background-color: #ffffff;
+      border: 4px solid #0f75f5;
+      border-radius: 50%;
+      content: '';
+    }
+  }
+
+  &.square {
+    &::before {
+      position: absolute;
+      top: 5px;
+      left: 6px;
+      width: 12px;
+      height: 12px;
+      background-color: #0f75f5;
+      content: '';
+    }
+  }
+}
+
+.legend--text {
+  padding-left: 25px;
+  color: rgb(122, 122, 122);
+  font-weight: normal;
+  font-size: 14px;
+  letter-spacing: 0px;
+}
+</style>
--- src/components/section/BoardMonthly.vue
+++ src/components/section/BoardMonthly.vue
@@ -1,61 +1,83 @@
 <template>
-  <section class="board-monthly">
-    <div class="board-monthly__header">
-      <span class="board-monthly__header--title">월별 협업</span>
-      <span class="board-monthly__header--description">
-        설정한 기간의 협업 내용을 보여줍니다.
-      </span>
-      <datepicker
-        class="board-daily__header--datepicker"
-        :pickerName="'monthly'"
-        :minimumView="'month'"
-        :maximumView="'month'"
-        :format="'yyyy-MM'"
-      ></datepicker>
-    </div>
-    <div class="board-monthly__body">
-      <card :customClass="'custom-card-chart'">
-        <div name="header">asdf</div>
-        <div class="chart-holder">
-          <canvas id="chart-month" width="890" height="260"></canvas>
-        </div>
-      </card>
-      <card-board
-        :count="5"
-        :countDescription="'월별 총 협업 수'"
-        :time="36555"
-        :timeDescription="'월별 총 협업 시간'"
-      ></card-board>
+  <section class="board">
+    <card :customClass="'custom-card-chart'">
+      <div name="header" class="board__header">
+        <span class="board__header--title">월별 협업</span>
+        <span class="board__header--description">
+          설정한 기간의 협업 내용을 보여줍니다.
+        </span>
+        <datepicker
+          class="board__header--datepicker"
+          :pickerName="'monthly'"
+          :minimumView="'month'"
+          :maximumView="'month'"
+          :format="'yyyy-MM'"
+        ></datepicker>
+      </div>
+      <div class="chart-legend">
+        <chart-legend text="개인 협업 내역" shape="square"></chart-legend>
+        <chart-legend
+          text="전체 협업 내역"
+          shape="square"
+          customClass="grey"
+        ></chart-legend>
+      </div>
+      <div class="chart-holder">
+        <canvas id="chart-month" width="1145" height="230"></canvas>
+      </div>
+    </card>
+    <div class="board-figures">
+      <figure-board
+        header="전체 월별 협업 수"
+        :count="48"
+        :imgSrc="require('assets/image/figure/ic_figure_calendar.svg')"
+      ></figure-board>
+      <figure-board
+        header="전체 월별 협업 시간"
+        :time="999999"
+        :imgSrc="require('assets/image/figure/ic_figure_date_all.svg')"
+      ></figure-board>
+      <figure-board
+        header="나의 월별 협업 수"
+        :onlyMe="true"
+        :count="999999"
+        :imgSrc="require('assets/image/figure/ic_figure_chart.svg')"
+      ></figure-board>
+      <figure-board
+        header="나의 월별 협업 시간"
+        :onlyMe="true"
+        :time="999999"
+        :imgSrc="require('assets/image/figure/ic_figure_date_time.svg')"
+      ></figure-board>
     </div>
   </section>
 </template>
 
 <script>
 import Card from 'Card'
-import CardBoard from 'CardBoard'
 import Datepicker from 'Datepicker'
 import Chart from 'chart.js'
+import ChartLegend from 'Legend'
+import FigureBoard from 'FigureBoard'
 export default {
   name: 'BoardMonthly',
   components: {
     Card,
-    CardBoard,
+    ChartLegend,
+    FigureBoard,
     Datepicker,
   },
   mounted() {
     const ctx = document.getElementById('chart-month').getContext('2d')
-    const gradientFill = ctx.createLinearGradient(0, 500, 0, 100)
-    gradientFill.addColorStop(1, 'rgba(137,187,250, 1)')
-    gradientFill.addColorStop(0, 'rgba(255, 255, 255, 0.6)')
 
     const custom = function(tooltipModel) {
       // Tooltip Element
-      let tooltipEl = document.getElementById('chartjs-tooltip')
+      let tooltipEl = document.getElementById('chartjs-noarrow')
 
       // Create element on first render
       if (!tooltipEl) {
         tooltipEl = document.createElement('div')
-        tooltipEl.id = 'chartjs-tooltip'
+        tooltipEl.id = 'chartjs-noarrow'
         tooltipEl.innerHTML = '<table></table>'
         document.body.appendChild(tooltipEl)
       }
@@ -83,10 +105,10 @@ export default {
         let titleLines = tooltipModel.title || []
         let bodyLines = tooltipModel.body.map(getBody)
 
-        let innerHtml = '<thead>'
+        let innerHtml = '<thead class="chartjs-tooltip-head">'
 
         titleLines.forEach(function(title) {
-          innerHtml += '<tr><th>' + title + '</th></tr>'
+          innerHtml += '<tr><th><p>' + title + '</p></th></tr>'
         })
         innerHtml += '</thead><tbody>'
 
@@ -95,7 +117,12 @@ export default {
           let style = 'background:' + colors.backgroundColor
           style += '; border-color:' + colors.borderColor
           style += '; border-width: 1px'
-          let span = '<span style="' + style + '"></span>'
+          let span =
+            '<span class="tooil-tip-legend" style="display:inline-block; background:' +
+            colors.backgroundColor +
+            '"></span><span style="' +
+            style +
+            '"></span>'
           innerHtml += '<tr><td>' + span + body + '</td></tr>'
         })
         innerHtml += '</tbody>'
@@ -120,24 +147,29 @@ export default {
         tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px'
       tooltipEl.style.pointerEvents = 'none'
     }
-    console.log(custom)
+
     let myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['09.11', '08', '10', '12', '14', '16', '18', '20', '22', '24'],
+        labels: this.getMonth(),
         datasets: [
           {
-            label: '',
-            data: [7, 5, 10, 10, 11, 10, 2, 11, 5, 5],
-            backgroundColor: '#bdcade',
+            label: '개인 협업 내역',
+            data: [5, 3],
+            backgroundColor: '#0f75f5',
             borderColor: 'none',
             borderWidth: 4,
-            pointRadius: 4,
-            pointBackgroundColor: '#fff',
-            fill: 'start',
-            borderJoinStyle: 'bevel',
-            lineTension: 0,
             hoverBackgroundColor: '#0f75f5',
+            hoverBorderColor: 'none',
+            barThickness: 11,
+          },
+          {
+            label: '전체 협업 내역',
+            data: [1, 2],
+            backgroundColor: '#bbc8d9',
+            borderColor: 'none',
+            borderWidth: 4,
+            hoverBackgroundColor: '#bbc8d9',
             hoverBorderColor: 'none',
             barThickness: 11,
           },
@@ -146,11 +178,9 @@ export default {
       options: {
         hover: {
           mode: 'index',
-          intersect: false,
         },
         tooltips: {
           mode: 'index',
-          intersect: false,
           position: 'average',
           enabled: false,
           custom: custom,
@@ -174,79 +204,95 @@ export default {
         },
         scales: {
           xAxes: [
-            // { barPercentage: 0.4 },
             {
-              gridLines: {
-                display: false,
-              },
+              stacked: true,
             },
           ],
           yAxes: [
             {
-              ticks: {
-                beginAtZero: true,
-                max: 15,
-                min: 0,
-                stepSize: 5,
-              },
-              gridLines: {
-                borderDash: [1, 2],
-              },
+              stacked: true,
             },
           ],
         },
+        // scales: {
+        //   xAxes: [
+        //     {
+        //       stacked: true,
+        //     },
+        //     {
+        //       gridLines: {
+        //         display: false,
+        //       },
+        //     },
+        //   ],
+        //   yAxes: [
+        //     {
+        //       stacked: true,
+        //     },
+        //     // {
+        //     //   gridLines: {
+        //     //     borderDash: [1, 2],
+        //     //   },
+        //     // },
+        //   ],
+        // },
       },
     })
     console.log(myChart)
+  },
+
+  methods: {
+    //temp
+    getMonth() {
+      const dayList = []
+      for (let i = 1; i <= 31; i++) {
+        dayList.push(i + '일')
+      }
+      return dayList
+    },
   },
 }
 </script>
 
 <style lang="scss">
-.board-monthly {
-  height: 371px;
-  margin-top: 40px;
-}
-.board-monthly__header {
-  position: relative;
-  height: 60px;
-}
+#chartjs-noarrow {
+  position: absolute;
+  width: 128px;
+  height: 80px;
+  color: white;
+  background: #3b3b3b;
+  border: 1px solid rgb(52, 65, 81);
+  border-radius: 4px;
+  -webkit-transform: translate(-50%, 0);
+  transform: translate(-50%, 0);
+  opacity: 1;
+  -webkit-transition: all 0.1s ease;
+  transition: all 0.1s ease;
+  pointer-events: none;
 
-.board-monthly__header--title {
-  margin-right: 10px;
-  color: rgb(11, 31, 72);
-  font-weight: 500;
-  font-size: 22px;
-}
+  table {
+    position: relative;
+    width: 100%;
+  }
 
-.board-monthly__header--description {
-  color: rgb(122, 122, 122);
-  font-weight: 500;
-  font-size: 15px;
-}
-
-.board-monthly__header--datepicker {
-  &.datepicker {
-    position: absolute;
-    top: 0px;
-    right: 0px;
+  .tooil-tip-legend {
+    width: 10px;
+    height: 10px;
+    margin-right: 5px;
+    margin-left: 5px;
+    border-radius: 50%;
   }
 }
-
-.board-monthly__body {
+.chart-legend {
   display: flex;
-}
-
-.custom-card-chart {
-  width: 968px;
-  height: 310px;
-  margin-right: 12px;
-}
-
-.chart-holder {
-  position: relative;
-  width: 64.2857rem;
-  height: 19.2857rem;
+  align-items: center;
+  height: 68px;
   padding-left: 30px;
+
+  & > .legend.square.grey {
+    &::before {
+      background-color: #bbc8d9;
+    }
+  }
 }
 </style>
