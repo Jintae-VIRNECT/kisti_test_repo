ab5d0a07c9b0a6cee7299989b3a7ddb0843d10e9
jukyoung lee <ljk@virnect.com>
2020-10-20 14:21:58 +0900

    fix: revise contents duplicate logic

--- src/main/java/com/virnect/content/application/ContentService.java
+++ src/main/java/com/virnect/content/application/ContentService.java
@@ -785,7 +785,8 @@ public class ContentService {
 		ContentUploadRequest uploadRequest = ContentUploadRequest.builder()
 			.workspaceUUID(content.getWorkspaceUUID())
 			//.content(convertFileToMultipart(uploadPath.concat(contentUUID).concat(ARES_FILE_EXTENSION)))
-			.content(convertFileToMultipart(contentUUID.concat(ARES_FILE_EXTENSION)))
+			//.content(convertFileToMultipart(contentUUID.concat(ARES_FILE_EXTENSION)))
+			.content(fileDownloadService.getMultipartfile(contentUUID.concat(ARES_FILE_EXTENSION)))
 			// TODO : 공정 수정 후 반영 예정
 			//                .contentType(content.getType().getType())
 			.name(response.getData().getName())
