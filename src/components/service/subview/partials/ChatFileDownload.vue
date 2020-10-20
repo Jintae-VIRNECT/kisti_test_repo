e05bfd70f1b6aa38b2b83183d48ff2b4af02994c
wooka <wooka@virnect.com>
2020-10-20 18:33:41 +0900

    Feat: add zip download

--- src/components/service/subview/partials/ChatFileDownload.vue
+++ src/components/service/subview/partials/ChatFileDownload.vue
@@ -14,10 +14,33 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import { downloadFile } from 'api/http/file'
-import { downloadByURL } from 'utils/file'
+import { downloadFileUrl } from 'api/http/file'
+import JSZip from 'jszip'
+import FileSaver from 'file-saver'
+
+const getFile = url => {
+  return new Promise(resolve => {
+    var xhr = new XMLHttpRequest()
+
+    xhr.addEventListener('load', function() {
+      if (xhr.status == 200) {
+        resolve(xhr.response)
+      }
+    })
+
+    xhr.open('GET', url)
+    xhr.responseType = 'blob'
+    xhr.send(null)
+  })
+}
 export default {
   name: 'ChatFileDownload',
+  data() {
+    return {
+      chunk: [],
+      zip: null,
+    }
+  },
   props: {
     fileList: {
       type: Array,
@@ -31,17 +54,25 @@ export default {
   },
   methods: {
     async download() {
+      this.zip = new JSZip()
       for (let file of this.fileList) {
-        const res = await downloadFile({
+        const res = await downloadFileUrl({
           objectName: file.objectName,
           sessionId: this.roomInfo.sessionId,
           workspaceId: this.workspace.uuid,
           userId: this.account.uuid,
         })
-
-        downloadByURL(res, file.name)
+        await this.get(res.name, res.url)
       }
+      this.zip.generateAsync({ type: 'blob' }).then(content => {
+        FileSaver.saveAs(content, 'vremote_files.zip')
+      })
+
       this.$emit('clear')
+    },
+    async get(name, url) {
+      const data = await getFile(url)
+      this.zip.file(name, data, { binary: true })
     },
   },
 }
