f094341b38e3b49272113a341aa3e318469f6ea8
JeognHyeon-VIRNECT <sky456139@virnect.com>
2020-10-23 11:41:28 +0900

    fix: revise gateway error log format

--- src/main/java/com/virnect/gateway/error/GatewayExceptionHandler.java
+++ src/main/java/com/virnect/gateway/error/GatewayExceptionHandler.java
@@ -33,7 +33,7 @@ public class GatewayExceptionHandler implements ErrorWebExceptionHandler {
 
 	@Override
 	public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
-		log.error("[GATEWAY EXCEPTION HANDLER] : {}" + ex.getMessage(), ex);
+		log.error("[GATEWAY EXCEPTION HANDLER] : {}", ex.getMessage(), ex);
 
 		String message = "";
 		// Gateway Security Related Exception Handling
