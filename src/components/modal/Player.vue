fcb2552409aa96ea831306439c45d49a39ffa0c0
ykmo <ykmo@virnect.com>
2020-10-29 17:30:46 +0900

    Feat:add Player

---
+++ src/components/modal/Player.vue
@@ -0,0 +1,68 @@
+<template>
+  <modal
+    :width="'90%'"
+    :height="'90%'"
+    :visible.sync="visibleFlag"
+    :beforeClose="beforeClose"
+    :showHeader="false"
+    customClass="modal-player"
+    ><video :src="url" controls></video
+  ></modal>
+</template>
+
+<script>
+import Modal from 'Modal'
+export default {
+  name: 'Player',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      visibleFlag: false,
+    }
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    url: {
+      type: String,
+      default: null,
+    },
+  },
+  watch: {
+    async visible(flag) {
+      this.visibleFlag = flag
+    },
+  },
+  methods: {
+    beforeClose() {
+      this.$emit('update:visible', false)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.modal.modal-player {
+  .modal--inner {
+    background-color: transparent;
+    border: none;
+  }
+
+  .modal--body {
+    width: 100%;
+    height: 100%;
+    padding: 0px;
+
+    > video {
+      width: 100%;
+      height: 100%;
+      object-fit: fill;
+      border: none;
+    }
+  }
+}
+</style>
