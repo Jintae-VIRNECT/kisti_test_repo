76e4c55d173912cae49924fc8f1a841c35f5952f
Sunghyun-VIRNECT <58674642+Sunghyun-VIRNECT@users.noreply.github.com>
2020-11-03 15:59:11 +0900

    fix: proxy replace urls

--- src/store/custom.js
+++ src/store/custom.js
@@ -1,4 +1,5 @@
 import WorkspaceService from 'service/workspace-service'
+
 export const custom = {
 	state: {
 		customInfo: null,
@@ -16,6 +17,24 @@ export const custom = {
 	actions: {
 		async SET_CUSTOM({ commit }) {
 			const { data } = await WorkspaceService.customConfig()
+
+			if (window.env === 'onpremise') {
+				console.log(`replace proxy url`)
+				const URLS = window.urls
+				// URLS.minio = 'https://192.168.13.64:4545'
+				const proxyUrl = url => {
+					if ('minio' in URLS && url !== null) {
+						return url.replace(/^((http[s]?|ftp):\/\/)([^/]+)/, URLS['minio'])
+					} else {
+						return url
+					}
+				}
+				for (const [key, url] of Object.entries(data)) {
+					data[key] = proxyUrl(url)
+				}
+				console.log(data)
+			}
+
 			document.title = `${data.workspaceTitle} Login Center`
 			let favicon = document.querySelector("link[rel*='icon']")
 			favicon.href = data.favicon
