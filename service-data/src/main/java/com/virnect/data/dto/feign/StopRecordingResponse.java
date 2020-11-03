c3ffca859c37785017b5512b7632d7d46e35bee1
hoon <tayesymbol@gmail.com>
2020-11-03 16:56:58 +0900

    fix stop recording response

--- service-data/src/main/java/com/virnect/data/dto/feign/StopRecordingResponse.java
+++ service-data/src/main/java/com/virnect/data/dto/feign/StopRecordingResponse.java
@@ -4,9 +4,11 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class StopRecordingResponse {
     @ApiModelProperty(value = "Recording Identifier")
-    private String recordingIds;
+    private List<String> recordingIds;
 }
