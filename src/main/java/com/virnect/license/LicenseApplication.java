bc873f33319913f580445b8dd08c7a7f5e70d95d
JeognHyeon-VIRNECT <sky456139@virnect.com>
2020-11-11 16:39:00 +0900

    feat: add eureka discovery client list log

--- src/main/java/com/virnect/license/LicenseApplication.java
+++ src/main/java/com/virnect/license/LicenseApplication.java
@@ -3,9 +3,7 @@ package com.virnect.license;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,17 +24,5 @@ public class LicenseApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
-	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void applicationReady() {
-		String msg = "\n\n----------------------------------------------------\n" + "\n"
-			+ "   Platform License Server is ready!\n"
-			+ "   ---------------------------\n" + "\n"
-			+ "   * Server Host: [" + System.getenv("eureka.instance.ip-address") + "]\n" + "\n"
-			+ "   * VIRNECT_ENV: [" + System.getenv("VIRNECT_ENV") + "]\n" + "\n"
-			+ "   * Config Server Url: [" + System.getenv("CONFIG_SERVER") + "]\n" + "\n"
-			+ "----------------------------------------------------\n";
-		log.info(msg);
 	}
 }
