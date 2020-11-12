132b0c7063b6368ae3a47ded268d4ef8a1e6de67
JeognHyeon-VIRNECT <sky456139@virnect.com>
2020-11-12 15:49:22 +0900

    feat: remove display server list that registered on eureka

--- src/main/java/com/virnect/license/event/ApplicationEventHandler.java
+++ src/main/java/com/virnect/license/event/ApplicationEventHandler.java
@@ -1,18 +1,14 @@
 package com.virnect.license.event;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class ApplicationEventHandler {
-	private final EurekaDiscoveryClient eurekaDiscoveryClient;
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void applicationReady() {
@@ -24,12 +20,5 @@ public class ApplicationEventHandler {
 			+ "   * Config Server Url: [" + System.getenv("CONFIG_SERVER") + "]\n" + "\n"
 			+ "----------------------------------------------------\n";
 		log.info(msg);
-
-		for (String service : eurekaDiscoveryClient.getServices()) {
-			eurekaDiscoveryClient.getInstances(service).forEach(instance -> {
-				log.info(String.format("[ SERVICE_ID: %-25s HOST: %-13s PORT: %-6s SECURE(HTTPS): %-5s ]"
-					, service, instance.getHost(), instance.getPort(), instance.isSecure()));
-			});
-		}
 	}
 }
