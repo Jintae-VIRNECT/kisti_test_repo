ddcda413a18b8fe4c0ffb5b5461d5cd1c1fa6c5b
hoon <tayesymbol@gmail.com>
2020-11-19 17:42:06 +0900

    s3 file management service changed

--- service-server/src/main/java/com/virnect/serviceserver/infra/file/S3FileManagementService.java
+++ service-server/src/main/java/com/virnect/serviceserver/infra/file/S3FileManagementService.java
@@ -5,12 +5,13 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.google.common.io.Files;
-import com.virnect.service.error.ErrorCode;
-import com.virnect.service.error.exception.RestServiceException;
 import com.virnect.file.FileType;
 import com.virnect.file.IFileManagementService;
+import com.virnect.service.error.ErrorCode;
+import com.virnect.service.error.exception.RestServiceException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -23,7 +24,10 @@ import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Profile({"staging", "production"})
 @Slf4j
@@ -32,10 +36,16 @@ import java.util.*;
 public class S3FileManagementService implements IFileManagementService {
 
     @Value("${cloud.aws.s3.bucket.name}")
-    private String publicBucketName;
+    private String bucketName;
 
-    @Value("${cloud.aws.s3.bucket.resource}")
-    private String resourceProfile;
+    @Value("${cloud.aws.s3.bucket.file}")
+    private String fileBucketName;
+
+    @Value("${cloud.aws.s3.bucket.profile}")
+    private String profileBucketName;
+
+    @Value("${cloud.aws.s3.bucket.record}")
+    private String recordBucketName;
 
     private final AmazonS3 amazonS3Client;
 
@@ -53,45 +63,106 @@ public class S3FileManagementService implements IFileManagementService {
     @Override
     public String upload(MultipartFile file, String dirPath, FileType fileType)
             throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-        return null;
+        // 1. check is file dummy
+        if (file.getSize() == 0) {
+            throw new RestServiceException(ErrorCode.ERR_FILE_ASSUME_DUMMY);
+        }
+
+        // 2. check file extension
+        String fileExtension = Files.getFileExtension(Objects.requireNonNull(file.getOriginalFilename()));
+        if (!fileAllowExtensionList.contains(fileExtension)) {
+            log.error("UPLOAD FILE::#upload::unsupported_file [{}]", file.getOriginalFilename());
+            throw new RestServiceException(ErrorCode.ERR_FILE_UNSUPPORTED_EXTENSION);
+        }
+
+        // 3. check file size
+        /*
+         * if (file.getSize() >= MAX_USER_PROFILE_IMAGE_SIZE) { throw new
+         * RestServiceException(ErrorCode.ERR_FILE_SIZE_LIMIT); }
+         */
+        log.info("UPLOAD FILE::#upload::result => [originName: {}, name: {} , size: {}]",
+                file.getOriginalFilename(),
+                file.getName(),
+                file.getSize());
+        String objectName = String.format("%s_%s", LocalDate.now(), RandomStringUtils.randomAlphabetic(20));
+
+        log.info("UPLOAD FILE::#upload::result => [{}, {}]", objectName, fileExtension);
+
+        // 4. file upload
+        // Create a InputStream for object upload.
+        StringBuilder objectPath = new StringBuilder();
+        switch (fileType) {
+            case FILE: {
+                objectPath.append(dirPath).append(fileBucketName).append("/").append(objectName);
+                // Create headers
+                ObjectMetadata objectMetadata = new ObjectMetadata();
+                objectMetadata.setContentType(file.getContentType());
+                objectMetadata.setContentLength(file.getSize());
+
+                putObjectToAWSS3(bucketName, file, objectPath.toString(), objectMetadata,
+                        CannedAccessControlList.BucketOwnerRead);
+                log.info("UPLOAD FILE::#upload:: {}, {}", objectPath.toString(), file.getContentType());
+                break;
+            }
+            case RECORD: {
+                objectPath.append(dirPath).append(recordBucketName).append("/").append(objectName);
+                // Create headers
+                ObjectMetadata objectMetadata = new ObjectMetadata();
+                objectMetadata.setContentType(file.getContentType());
+                objectMetadata.setContentLength(file.getSize());
+
+                putObjectToAWSS3(objectPath.toString(), file, objectPath.toString(), objectMetadata,
+                        CannedAccessControlList.BucketOwnerRead);
+                log.info("SAVE FILE_URL: {}, {}", objectPath.toString(), file.getContentType());
+                break;
+            }
+        }
+
+        return objectName;
     }
 
     @Override
     public String uploadProfile(MultipartFile file, String dirPath)
             throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-        // 1. 빈 파일 여부 확인
+        // check file is dummy
         if (file.getSize() == 0) {
             throw new RestServiceException(ErrorCode.ERR_FILE_ASSUME_DUMMY);
         }
 
-        // 2. 확장자 검사
+        // check file extension
         String fileExtension = Files.getFileExtension(Objects.requireNonNull(file.getOriginalFilename()));
         if (!FILE_IMAGE_ALLOW_EXTENSION.contains(fileExtension)) {
             log.error("[FILE_UPLOAD_SERVICE] [UNSUPPORTED_FILE] [{}]", file.getOriginalFilename());
             throw new RestServiceException(ErrorCode.ERR_FILE_UNSUPPORTED_EXTENSION);
         }
 
-        // 3. 파일 용량 검사
+        // check file size
         if (file.getSize() >= MAX_USER_PROFILE_IMAGE_SIZE) {
             throw new RestServiceException(ErrorCode.ERR_FILE_SIZE_LIMIT);
         }
 
-        log.info("UPLOAD SERVICE: ==> originName: [{}], name: {} , size: {}", file.getOriginalFilename(),
-                file.getName(), file.getSize());
-        log.info("BUCKET NAME:{}, {}, {}", publicBucketName, dirPath, fileExtension);
+        // check profile directory name or path
+        if(dirPath == null)
+            dirPath = profileBucketName;
 
-        // 4. file upload
-        // String objectName = String.format("%s_%s", LocalDate.now(),
-        // RandomStringUtils.randomAlphabetic(20));
-        String uniqueObjectName = String.format("%s_%s", LocalDate.now(),
-                UUID.randomUUID().toString().replace("-", ""));
-        String objectName = String.format("%s/%s%s", resourceProfile, uniqueObjectName, fileExtension);
+        log.info("UPLOAD SERVICE: ==> originName: [{}], name: {} , size: {}",
+                file.getOriginalFilename(),
+                file.getName(),
+                file.getSize());
 
+        log.info("{}, {}",dirPath, fileExtension);
+
+        // file upload with create a InputStream for object upload.
+        String objectName = String.format("%s_%s", LocalDate.now(), RandomStringUtils.randomAlphabetic(20));
+        StringBuilder objectPath;
+        objectPath = new StringBuilder();
+        objectPath.append(dirPath).append("/").append(objectName).append(".").append(fileExtension);
+        // Create headers
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(file.getContentType());
         objectMetadata.setContentLength(file.getSize());
 
-        return putObjectToAWSS3(publicBucketName, file, objectName, objectMetadata,
+        return putObjectToAWSS3(objectPath.toString(), file, objectName, objectMetadata,
                 CannedAccessControlList.BucketOwnerRead);
     }
 
@@ -110,7 +181,8 @@ public class S3FileManagementService implements IFileManagementService {
     @Override
     public boolean removeObject(String objectPathToName)
             throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-        return false;
+        amazonS3Client.deleteObject(bucketName, objectPathToName);
+        return true;
     }
 
     @Override
@@ -119,24 +191,16 @@ public class S3FileManagementService implements IFileManagementService {
     }
 
     @Override
-    public void deleteProfile(String url) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-        if (DEFAULT_ROOM_PROFILE.equals(url)) {
-            log.info("기본 이미지는 삭제하지 않습니다.");
+    public void deleteProfile(String objectPathToName) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+        if (DEFAULT_ROOM_PROFILE.equals(objectPathToName)) {
+            log.info("PROFILE REMOVE::#deleteProfile::do not delete default profile name");
         } else {
-            if (url != null) {
-                /*
-                 * String resourceEndPoint = String.format("%s/%s", publicBucketName,
-                 * resourceProfile); String key = url.split(String.format("/%s/",
-                 * resourceProfile))[1]; amazonS3Client.deleteObject(resourceEndPoint, key);
-                 */
-                String resourceEndPoint = String.format("%s/%s", publicBucketName, resourceProfile);
-                int index = url.indexOf(resourceProfile);
-                String key = url.substring(index);
-                log.info("DELETE OBJECT: ==> BUCKET NAME:[{}], KEY: [{}]", publicBucketName, key);
-                amazonS3Client.deleteObject(publicBucketName, key);
-                log.info(key + " 파일이 AWS S3(" + resourceEndPoint + ")에서 삭제되었습니다.");
-            }
+            String objectName = objectPathToName.replaceAll(HOST_REGEX, "").replace("\\", "/");
+            removeObject(objectName);
+
+            log.info("PROFILE REMOVE::#deleteProfile::for not using anymore => [{}]", objectName);
         }
+
     }
 
     @Override
@@ -167,7 +231,24 @@ public class S3FileManagementService implements IFileManagementService {
     @Override
     public String filePreSignedUrl(String dirPath, String objectName, int expiry, String fileName, FileType fileType)
             throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-        return null;
+        StringBuilder objectPath = new StringBuilder();
+        String url = null;
+        switch (fileType) {
+            case FILE: {
+                objectPath.append(dirPath).append(fileBucketName).append("/").append(objectName);
+                url = amazonS3Client.getUrl(bucketName, objectPath.toString()).toString();
+                log.info("DOWNLOAD FILE::#filePreSignedUrl::file result::[{}]", url);
+                break;
+            }
+
+            case RECORD: {
+                objectPath.append(dirPath).append(recordBucketName).append("/").append(objectName);
+                url = amazonS3Client.getUrl(bucketName, objectPath.toString()).toString();
+                log.info("DOWNLOAD FILE::#filePreSignedUrl::record result::[{}]", url);
+                break;
+            }
+        }
+        return url;
     }
 
     @Override
