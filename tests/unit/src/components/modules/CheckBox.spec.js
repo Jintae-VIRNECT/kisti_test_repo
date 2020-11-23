47355375ce5a747b7f6f00259682eb9927035501
ykmo <ykmo@virnect.com>
2020-11-23 11:46:16 +0900

    Test:update checkbox

--- tests/unit/src/components/modules/CheckBox.spec.js
+++ tests/unit/src/components/modules/CheckBox.spec.js
@@ -1,11 +1,29 @@
-import { shallowMount } from '@vue/test-utils'
-import Card from 'components/modules/CheckBox'
+import { shallowMount, mount } from '@vue/test-utils'
+import CheckBox from 'components/modules/CheckBox'
+
+import localVue from '__utils__/localVue'
 
 describe('CheckBox.vue', () => {
+  const checkBoxText = 'checkbox test'
+
   it('커스텀 클래스 전달', () => {
-    const wrapper = shallowMount(Card, {
-      propsData: { value: true, text: 'checkbox test' },
+    const wrapper = shallowMount(CheckBox, {
+      localVue,
+      propsData: { value: true, text: checkBoxText },
     })
-    expect(wrapper.text()).toMatch('checkbox test')
+    expect(wrapper.text()).toMatch(checkBoxText)
+  })
+
+  it('체크 박스 클릭시 value(true) 반환', async () => {
+    const wrapper = mount(CheckBox, {
+      localVue,
+      propsData: { value: false, text: checkBoxText },
+    })
+
+    expect(wrapper.find('div').exists()).toBe(true)
+
+    await wrapper.find('div').trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.emitted()['update:value']).toEqual([[true]])
   })
 })
