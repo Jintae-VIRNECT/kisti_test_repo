c71859763852fd5473633c1353b767e7ee8de71b
wooka <wooka@virnect.com>
2020-11-26 19:11:07 +0900

    Fix: show pointing color tool

--- src/components/service/tools/StreamTools.vue
+++ src/components/service/tools/StreamTools.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="stream-tools tools">
     <pointing></pointing>
-    <color v-if="isLeader"></color>
+    <color></color>
   </div>
 </template>
 
 <script>
 import { Pointing, Color } from './partials'
-import { ROLE } from 'configs/remote.config'
 
 export default {
   name: 'StreamTools',
@@ -15,22 +14,5 @@ export default {
     Pointing,
     Color,
   },
-  data() {
-    return {
-      active: 'pointing',
-      isRecording: false,
-    }
-  },
-  computed: {
-    isLeader() {
-      return this.account.roleType === ROLE.LEADER
-    },
-  },
-  watch: {},
-  methods: {},
-
-  /* Lifecycles */
-  beforeDestroy() {},
-  mounted() {},
 }
 </script>
