bcaece97378274af827049551786afc872b61ade
ykmo <ykmo@virnect.com>
2020-12-01 10:48:31 +0900

    Feat:update workspace selector

--- src/components/header/partial/HeaderNav.vue
+++ src/components/header/partial/HeaderNav.vue
@@ -21,8 +21,9 @@ export default {
   height: 2.8571rem;
   margin: auto 0;
   text-align: left;
-  background: #454545;
-  border-radius: 4px;
+  background: #424242;
+  border: 1px solid #5d5d5d;
+  border-radius: 8px;
   .popover--wrapper {
     height: 100%;
   }
@@ -41,9 +42,10 @@ export default {
       position: absolute;
       top: 0.6429rem;
       right: 0.8571rem;
-      width: 1.7143rem;
-      height: 1.7143rem;
+      width: 20px;
+      height: 20px;
       background: url(~assets/image/ic_dropdown.svg) 50% no-repeat;
+      border-radius: 4px;
       content: '';
     }
   }
