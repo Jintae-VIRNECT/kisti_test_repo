815fd35fe6ee296d5414d55031da5c2bab3c5f1c
wooka <wooka@virnect.com>
2020-12-11 17:43:49 +0900

    Fix: add chat icon active style

--- src/components/header/tools/Chat.vue
+++ src/components/header/tools/Chat.vue
@@ -2,13 +2,16 @@
   <tooltip :content="`${$t('common.chatting')} on/off`">
     <toggle-button
       slot="body"
-      customClass="toggle-header__small header-tools__chatbox"
+      :customClass="
+        `toggle-header__small header-tools__chatbox ${chatBox ? 'visible' : ''}`
+      "
       :description="`${$t('common.chatting')} on/off`"
       size="2.429rem"
       :activeSrc="require('assets/image/call/ic_chat_toggle.svg')"
+      :inactiveSrc="require('assets/image/call/ic_chat_toggle.svg')"
       @action="toggle"
       :toggle="chatBox"
-      :active="chatBox"
+      :active="active"
     ></toggle-button>
   </tooltip>
 </template>
@@ -24,13 +27,27 @@ export default {
     Tooltip,
     ToggleButton,
   },
+  data() {
+    return {
+      active: false,
+    }
+  },
   computed: {
-    ...mapGetters(['chatBox']),
+    ...mapGetters(['chatBox', 'chatList']),
+  },
+  watch: {
+    'chatList.length': 'listChange',
   },
   methods: {
     ...mapActions(['toggleChat']),
     toggle() {
+      this.active = false
       this.toggleChat(!this.chatBox)
+    },
+    listChange() {
+      if (!this.chatBox) {
+        this.active = true
+      }
     },
   },
 }
