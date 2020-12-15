8287bbd45e4c0d37e63facbad82b3608c28c4eb9
ykmo <ykmo@virnect.com>
2020-12-15 11:58:03 +0900

    Style:update checkbox rem

--- src/components/modules/CheckBox.vue
+++ src/components/modules/CheckBox.vue
@@ -42,19 +42,15 @@ export default {
 .checkbox-text {
   margin: auto;
   margin-left: 0.714rem;
-  // color: rgba($color_white, 0.6);
   color: #434b58;
   font-weight: 500;
-  font-size: 14px;
+  font-size: 1rem;
   line-height: 1.429rem;
 }
 .checkbox-toggle {
   position: relative;
-  width: 24.006px;
-  height: 24.006px;
-  // background-color: rgb(209, 77, 77);
-  // background-color: #17171a;
-  // border: solid 1px #333335;
+  width: 1.7147rem;
+  height: 1.7147rem;
   background-color: #ffffff;
   border: 1px solid #c2c6ce;
   border-radius: 2px;
@@ -65,12 +61,10 @@ export default {
     border-color: transparent;
     &:after {
       position: absolute;
-      top: 5.002px;
-      left: 5.002px;
-      width: 11.996px;
-      height: 5.006px;
-      // border-bottom: solid 3px $color_primary;
-      // border-left: solid 3px $color_primary;
+      top: 0.3573rem;
+      left: 0.3573rem;
+      width: 0.8569rem;
+      height: 0.3576rem;
       border-bottom: solid 2px #ffffff;
       border-left: solid 2px #ffffff;
       transform: rotate(-45deg);
