ee8c32559060e1327ddf535cb18dc1b0ba7ba936
SangBaek-VIRNECT <lsb@virnect.com>
2020-12-29 15:28:52 +0900

    Revert "chore: remove wc-modules alias"
    This reverts commit fa63c4d7ae8b6848b6db97e2820cc611bac04d06.

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,6 +69,14 @@ module.exports = async () => {
     /**
      * build
      */
+    build: {
+      extend(config, { isDev, isClient }) {
+        config.resolve.alias['WC-Modules'] = resolve(
+          __dirname,
+          '../WC-Modules/src',
+        )
+      },
+    },
     server: {
       port: env.NUXT_PORT,
       host: env.NUXT_HOST,
