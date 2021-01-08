2f07ff0bee2841822f185a49c298e0e0a6b1b042
SangBaek-VIRNECT <lsb@virnect.com>
2021-01-08 14:11:10 +0900

    feat: X-Frame-Options deny

--- src/middleware/profile.js
+++ src/middleware/profile.js
@@ -1,4 +1,6 @@
-export default function({ req, store, redirect }) {
+export default function({ req, res, store, redirect }) {
+  if (res) res.setHeader('X-Frame-Options', 'deny')
+
   // 사용자가 인증을 하지 않은 경우.
   const requireAuth = !store.state.auth.authenticated
   const authAlready = req && /\/profile$/.test(req.headers.referer)
