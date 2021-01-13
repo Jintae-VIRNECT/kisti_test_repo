f8a04ce0835de2b3b378ad8ba12b30bbb97baeaf
hoon <tayesymbol@gmail.com>
2021-01-13 16:03:59 +0900

    version chaged to v2.2.23.2

--- service-server/build.gradle.kts
+++ service-server/build.gradle.kts
@@ -135,7 +135,7 @@ tasks.getByName<BootJar>("bootJar") {
     manifest {
         attributes(
             "Implementation-Title" to  "Remote Service Server",
-            "Implementation-Version" to "2.0 revision 2223v1")
+            "Implementation-Version" to "2.0 revision 2223v2")
     }
     archiveFileName.set("RM-Service-${archiveVersion.get()}.${archiveExtension.get()}")
 
