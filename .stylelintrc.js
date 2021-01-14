9ad2a9a4ef24b68087ea80dd52cdc415a0820470
SangBaek-VIRNECT <lsb@virnect.com>
2021-01-14 19:04:43 +0900

    chore: convention check

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  plugins: [
-    "stylelint-prettier", 
-    "stylelint-scss", 
-    "stylelint-order", 
-    "stylelint-config-rational-order/plugin",
-  ],
-  extends: [
-    "stylelint-prettier/recommended", 
-    "stylelint-config-rational-order",
-  ],
-  rules: {
-    "prettier/prettier": true,
-    "order/properties-order": [],
-    "plugin/rational-order": [true, {}]
-  }
+  extends: ['@virnect/stylelint-config/scss'],
 }
