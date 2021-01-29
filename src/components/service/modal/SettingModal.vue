e8494c3b816577d8b89bc3fbdbcbaa9161825b4f
ykmo-VIRNECT <ykmo@virnect.com>
2021-01-29 16:12:47 +0900

    Feat:update service setting modal

--- src/components/service/modal/SettingModal.vue
+++ src/components/service/modal/SettingModal.vue
@@ -2,7 +2,8 @@
   <modal
     :visible.sync="visibleFlag"
     :showClose="true"
-    width="auto"
+    width="840px"
+    height="607px"
     :beforeClose="beforeClose"
     class="service-setting-modal"
     :title="$t('환경 설정')"
@@ -10,8 +11,9 @@
     <div class="service-setting">
       <section class="service-setting-nav">
         <button
+          v-if="isLeader"
           class="service-setting-nav__menu"
-          :class="{ active: tabview === 'group' }"
+          :class="{ active: tabview === 'pointing' }"
           :data-text="$t('service.setting_pointing')"
           @click="tabChange('pointing')"
         >
@@ -44,366 +46,60 @@
       </section>
 
       <!-- 포인팅 리더 only -->
-      <template v-if="tabview === 'pointing'">
-        <template v-if="isLeader">
-          <p class="service-setting--header">
-            {{ $t('service.setting_pointing') }}
-          </p>
-          <div class="service-setting__row">
-            <p class="service-setting__text">
-              {{ $t('service.setting_pointing_participant') }}
-            </p>
-
-            <r-check
-              :text="$t('service.setting_pointing_allow')"
-              :value.sync="pointing"
-            ></r-check>
-          </div>
-        </template>
-      </template>
+      <set-pointing v-if="tabview === 'pointing'"></set-pointing>
 
       <!-- 로컬 녹화 / 로컬녹화중에는 사용 불가능해야함.-->
-      <template v-if="tabview === 'local-record'">
-        <p
-          class="service-setting--header"
-          :class="{ disable: isLocalRecording }"
-        >
-          {{ $t('service.setting_local_record') }}
-          <span v-if="isLocalRecording" class="service-setting--warning">
-            {{ $t('service.setting_local_record_warning') }}
-          </span>
-        </p>
-
-        <div
-          class="service-setting__row"
-          v-if="!isTablet"
-          :class="{ disable: isLocalRecording }"
-        >
-          <p class="service-setting__text">
-            {{ $t('service.setting_record_target') }}
-          </p>
-          <div class="service-setting__selector">
-            <r-radio
-              :options="localRecordTargetOpt"
-              value="value"
-              text="text"
-              :selectedOption.sync="recordTarget"
-            ></r-radio>
-          </div>
-        </div>
-
-        <!-- 사파리인 경우 로컬녹화 설정의 녹화대상(영상 녹화, 화면녹화), 참가자 로컬 녹화 여부만 출력됨 -->
-        <template v-if="!isSafari">
-          <div
-            class="service-setting__row"
-            :class="{ disable: isLocalRecording }"
-          >
-            <p class="service-setting__text">
-              {{ $t('service.setting_record_max_time') }}
-            </p>
-            <r-select
-              class="service-setting__selector"
-              :options="localRecTimeOpt"
-              value="value"
-              text="text"
-              :selectedValue.sync="maxRecordTime"
-            >
-            </r-select>
-          </div>
-          <div
-            class="service-setting__row"
-            :class="{ disable: isLocalRecording }"
-          >
-            <div class="service-setting__text custom">
-              <p>
-                {{ $t('service.setting_record_interval') }}
-              </p>
-              <tooltip
-                customClass="tooltip-guide"
-                :content="$t('service.setting_record_time_tooltip')"
-                :placement="isTablet ? 'bottom' : 'right'"
-                effect="blue"
-                :guide="true"
-              >
-                <img
-                  slot="body"
-                  class="setting__tooltip--icon"
-                  src="~assets/image/ic_tool_tip.svg"
-                />
-              </tooltip>
-            </div>
-            <r-select
-              class="service-setting__selector"
-              :options="localRecIntervalOpt"
-              value="value"
-              text="text"
-              :selectedValue.sync="maxRecordInterval"
-            >
-            </r-select>
-          </div>
-
-          <div
-            class="service-setting__row"
-            :class="{ disable: isLocalRecording }"
-          >
-            <div class="service-setting__text custom">
-              <p>
-                {{ $t('service.setting_record_resolution') }}
-              </p>
-              <tooltip
-                customClass="tooltip-guide"
-                :content="$t('service.setting_record_resolution_tooltip')"
-                :placement="isTablet ? 'bottom' : 'right'"
-                effect="blue"
-                :guide="true"
-              >
-                <img
-                  slot="body"
-                  class="setting__tooltip--icon"
-                  src="~assets/image/ic_tool_tip.svg"
-                />
-              </tooltip>
-            </div>
-
-            <r-select
-              class="service-setting__selector"
-              :options="localRecResOpt"
-              value="value"
-              text="text"
-              :selectedValue.sync="recordResolution"
-            >
-            </r-select>
-          </div>
-        </template>
-        <div
-          class="service-setting__row"
-          v-if="isLeader"
-          :class="{ disable: isLocalRecording }"
-        >
-          <p class="service-setting__text">
-            {{ $t('service.setting_local_record_participant') }}
-          </p>
-          <r-check
-            :text="$t('service.setting_local_record_allow')"
-            :value.sync="localRecording"
-          ></r-check>
-        </div>
-      </template>
+      <set-local-record v-if="tabview === 'local-record'"></set-local-record>
 
       <!-- 서버 녹화 관련 옵션 -->
-      <template v-if="tabview === 'server-record'">
-        <template v-if="isLeader && useRecording">
-          <p
-            class="service-setting--header"
-            :class="{ disable: isServerRecording }"
-          >
-            {{ $t('service.setting_server_record') }}
-            <span v-if="isServerRecording" class="service-setting--warning">
-              {{ $t('service.setting_server_record_warning') }}
-            </span>
-          </p>
-          <div
-            class="service-setting__row"
-            :class="{ disable: isServerRecording }"
-          >
-            <p class="service-setting__text">
-              {{ $t('service.setting_record_max_time') }}
-            </p>
-            <r-select
-              class="service-setting__selector"
-              :options="serverRecTime"
-              value="value"
-              text="text"
-              :selectedValue.sync="serverMaxRecordTime"
-            >
-            </r-select>
-          </div>
-
-          <div
-            class="service-setting__row"
-            :class="{ disable: isServerRecording }"
-          >
-            <div class="service-setting__text custom">
-              <p>
-                {{ $t('service.setting_record_resolution') }}
-              </p>
-            </div>
-
-            <r-select
-              class="service-setting__selector"
-              :options="serverRecResOpt"
-              value="value"
-              text="text"
-              :selectedValue.sync="serverRecordResolution"
-            >
-            </r-select>
-          </div>
-        </template>
-      </template>
+      <set-server-record
+        v-if="isLeader && useRecording && tabview === 'server-record'"
+      ></set-server-record>
 
       <!-- 번역 관련 기능 (전체 기능 포함 필요함)-->
-      <template v-if="tabview === 'translate'">
-        <template v-if="useTranslate">
-          <p class="service-setting--header">
-            {{ $t('service.setting_translate') }}
-          </p>
-          <div class="service-setting__row">
-            <p class="service-setting__text">
-              {{ $t('service.setting_translate_use') }}
-            </p>
-            <r-check
-              :text="$t('service.setting_translate_use_allow')"
-              :value.sync="useTranslateAllow"
-            ></r-check>
-          </div>
-          <div class="service-setting__row">
-            <div class="service-setting__text custom">
-              <p>{{ $t('service.setting_translate_language') }}</p>
-              <tooltip
-                customClass="tooltip-guide"
-                :content="$t('service.setting_translate_language_tooltip')"
-                :placement="isTablet ? 'bottom' : 'right'"
-                effect="blue"
-              >
-                <img
-                  slot="body"
-                  class="setting__tooltip--icon"
-                  src="~assets/image/ic_tool_tip.svg"
-                />
-              </tooltip>
-            </div>
-            <r-select
-              class="service-setting__selector"
-              :options="languageCodes"
-              value="code"
-              text="text"
-              :disabled="!useTranslateAllow"
-              :selectedValue.sync="translateCode"
-            >
-            </r-select>
-          </div>
-        </template>
-      </template>
+      <!-- 서비스 서버 컴패니 코드에서 번역 쓸건지 체크  -->
+      <set-translate v-if="tabview === 'translate'"></set-translate>
     </div>
   </modal>
 </template>
 
 <script>
 import Modal from 'Modal'
-import RSelect from 'RemoteSelect'
-import RCheck from 'RemoteCheckBox'
-import RRadio from 'RemoteRadio'
-import Tooltip from 'Tooltip'
 
 import toastMixin from 'mixins/toast'
 
 import { mapGetters, mapActions } from 'vuex'
-import { ROLE, CONTROL } from 'configs/remote.config'
-import {
-  localRecTime,
-  localRecResOpt,
-  localRecInterval,
-  serverRecTime,
-  serverRecResOpt,
-  RECORD_TARGET,
-} from 'utils/recordOptions'
+import { ROLE } from 'configs/remote.config'
+
+import SetLocalRecord from './partials/ServiceSetLocalRecord'
+import SetPointing from './partials/ServiceSetPointing'
+import SetServerRecord from './partials/ServiceSetServerRecord'
+import SetTranslate from './partials/ServiceSetTranslate'
 
 export default {
   name: 'SettingModal',
   mixins: [toastMixin],
   components: {
     Modal,
-    RSelect,
-    RCheck,
-    RRadio,
-    Tooltip,
+
+    SetLocalRecord,
+    SetPointing,
+    SetServerRecord,
+    SetTranslate,
   },
   data() {
     return {
-      initing: false,
-      localRecording: false,
-      pointing: false,
-
       visibleFlag: false,
-
-      recordTarget: this.$store.state.settings.localRecordTarget,
-
-      localRecResOpt: localRecResOpt,
-      serverRecResOpt: serverRecResOpt,
-      maxRecordTime: '',
-      maxRecordInterval: '',
-      recordResolution: '',
-      serverMaxRecordTime: '',
-      serverRecordResolution: '',
-      useTranslateAllow: false,
-      translateCode: 'ko-KR',
 
       tabview: 'pointing',
     }
   },
 
   computed: {
-    ...mapGetters([
-      'view',
-      'serverRecord',
-      'serverRecordStatus',
-      'localRecord',
-      'localRecordStatus',
-      'allowLocalRecord',
-      'allowPointing',
-      'translate',
-      'useTranslate',
-      'languageCodes',
-      'modalSetting',
-      'useRecording',
-    ]),
-    localRecTimeOpt() {
-      const options = localRecTime.map(time => {
-        return {
-          value: time,
-          text: `${time} ${this.$t('date.minute')}`,
-        }
-      })
-      return options
-    },
-    localRecIntervalOpt() {
-      const options = localRecInterval.map(interval => {
-        return {
-          value: interval,
-          text: `${interval} ${this.$t('date.minute')}`,
-        }
-      })
-      return options
-    },
-    localRecordTargetOpt() {
-      return [
-        {
-          text: this.$t('service.setting_record_stream'),
-          value: RECORD_TARGET.WORKER,
-        },
-        {
-          text: this.$t('service.setting_record_screen'),
-          value: RECORD_TARGET.SCREEN,
-        },
-      ]
-    },
-    serverRecTime() {
-      const options = serverRecTime.map(time => {
-        return {
-          value: time,
-          text: `${time} ${this.$t('date.minute')}`,
-        }
-      })
-      return options
-    },
+    ...mapGetters(['modalSetting', 'useRecording']),
+
     isLeader() {
       return this.account.roleType === ROLE.LEADER
-    },
-    isLocalRecording() {
-      return this.localRecordStatus === 'START'
-    },
-    isServerRecording() {
-      return this.serverRecordStatus !== 'STOP'
     },
   },
 
@@ -411,84 +107,9 @@ export default {
     modalSetting(flag) {
       this.visibleFlag = flag
     },
-    localRecording(flag) {
-      if (this.initing === false) return
-      this.$call.sendControl(CONTROL.LOCAL_RECORD, !!flag)
-      // this.$localStorage.setAllow('localRecord', !!flag)
-    },
-    pointing(flag) {
-      if (this.initing === false) return
-      this.$call.sendControl(CONTROL.POINTING, !!flag)
-      // this.$localStorage.setAllow('pointing', !!flag)
-    },
-
-    recordTarget(target) {
-      if (this.initing === false) return
-      switch (target) {
-        case RECORD_TARGET.WORKER:
-          this.setLocalRecordTarget(target)
-          this.showToast()
-          break
-        case RECORD_TARGET.SCREEN:
-          this.setLocalRecordTarget(target)
-          this.showToast()
-          break
-        default:
-          console.error('recordTarget :: Unknown local record target', target)
-          break
-      }
-    },
-    maxRecordTime(time) {
-      this.changeSetting('time', time)
-    },
-    maxRecordInterval(interval) {
-      this.changeSetting('interval', interval)
-    },
-    recordResolution(resolution) {
-      this.changeSetting('resolution', resolution)
-    },
-    serverMaxRecordTime(time) {
-      this.changeServerSetting('time', time)
-    },
-    serverRecordResolution(resolution) {
-      this.changeServerSetting('resolution', resolution)
-    },
-    // translateCode(code) {
-    //   this.changeTranslate('code', code)
-    // },
-    // useTranslateAllow(flag) {
-    //   this.changeTranslate('flag', flag)
-    // },
   },
   methods: {
-    ...mapActions([
-      'setRecord',
-      'setServerRecord',
-      'setLocalRecordTarget',
-      'setTranslate',
-      'showModalSetting',
-    ]),
-    changeSetting(item, setting) {
-      const param = {}
-      param[item] = setting
-      this.setRecord(param)
-      this.$localStorage.setRecord(item, setting)
-      // this.showToast()
-    },
-    changeServerSetting(item, setting) {
-      const param = {}
-      param[item] = setting
-      this.setServerRecord(param)
-      this.$localStorage.setServerRecord(item, setting)
-      // this.showToast()
-    },
-    changeTranslate(item, setting) {
-      const param = {}
-      param[item] = setting
-      this.setTranslate(param)
-      this.$localStorage.setTranslate(item, setting)
-      // this.showToast()
-    },
+    ...mapActions(['setTranslate', 'showModalSetting']),
 
     beforeClose() {
       this.showModalSetting(false)
@@ -498,20 +119,6 @@ export default {
       this.toastNotify(this.$t('service.setting_save'))
     },
     init() {
-      this.initing = false
-      this.translateCode = this.translate.code
-      this.useTranslateAllow = this.translate.flag
-      this.maxRecordTime = this.localRecord.time
-      this.maxRecordInterval = this.localRecord.interval
-      this.recordResolution = this.localRecord.resolution
-      if (this.account.roleType === ROLE.LEADER) {
-        this.localRecording = this.allowLocalRecord
-        this.pointing = this.allowPointing
-      }
-      if (this.useRecording) {
-        this.serverMaxRecordTime = this.serverRecord.time
-        this.serverRecordResolution = this.serverRecord.resolution
-      }
       this.$nextTick(() => {
         this.initing = true
       })
---
+++ src/components/service/modal/partials/ServiceSetTranslate.vue
@@ -0,0 +1,181 @@
+<template>
+  <section class="service-setting__view">
+    <!-- 서비스 서버 컴패니 코드에서 번역 쓸건지 체크  -->
+    <template>
+      <p class="service-setting--header">
+        {{ $t('service.setting_translate') }}
+      </p>
+      <div class="service-setting__row">
+        <p class="service-setting__text">
+          {{ $t('service.setting_translate_use') }}
+        </p>
+        <r-check
+          :text="$t('service.setting_translate_use_allow')"
+          :value.sync="useTranslate"
+        ></r-check>
+      </div>
+      <div class="service-setting__row">
+        <div class="service-setting__text custom">
+          <p>{{ $t('service.setting_translate_language') }}</p>
+          <tooltip
+            customClass="tooltip-guide"
+            :content="$t('service.setting_translate_language_tooltip')"
+            :placement="isTablet ? 'bottom' : 'right'"
+            effect="blue"
+          >
+            <img
+              slot="body"
+              class="setting__tooltip--icon"
+              src="~assets/image/ic_tool_tip.svg"
+            />
+          </tooltip>
+        </div>
+        <r-select
+          class="service-setting__selector"
+          :options="languageCodes"
+          value="code"
+          text="text"
+          :disabled="!useTranslate"
+          :selectedValue.sync="transCode"
+        >
+        </r-select>
+      </div>
+      <div class="service-setting__row">
+        <div class="service-setting__text" :class="{ disabled: !useTranslate }">
+          {{ $t('workspace.setting_translate_output') }}
+        </div>
+        <slider
+          :first="$t('workspace.setting_translate_output_both')"
+          :second="$t('workspace.setting_translate_output_each')"
+          :isFirst.sync="transMultiple"
+          :disabled="!useTranslate"
+          @slider:disable="disableTranslate"
+        ></slider>
+      </div>
+      <div class="service-setting__row">
+        <div class="service-setting__text" :class="{ disabled: !useTranslate }">
+          {{ $t('workspace.setting_stt') }}
+        </div>
+        <slider
+          :first="$t('workspace.setting_stt_sync')"
+          :second="$t('workspace.setting_stt_streaming')"
+          :isFirst.sync="sttSync"
+          :disabled="!useTranslate"
+          @slider:disable="disableTranslate"
+        ></slider>
+      </div>
+      <div class="service-setting__row">
+        <div class="service-setting__text" :class="{ disabled: !useTranslate }">
+          {{ $t('workspace.setting_tts') }}
+        </div>
+        <check
+          :text="$t('workspace.setting_tts_allow')"
+          :value.sync="ttsAllow"
+          :disabled="!useTranslate"
+          @check:disable="disableTranslate"
+        ></check>
+      </div>
+    </template>
+  </section>
+</template>
+
+<script>
+import RSelect from 'RemoteSelect'
+import Check from 'Check'
+import Tooltip from 'Tooltip'
+import Slider from 'Slider'
+import RCheck from 'RemoteCheckBox'
+
+import toastMixin from 'mixins/toast'
+
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'ServiceSetStranslate',
+  mixins: [toastMixin],
+  components: {
+    RSelect,
+    RCheck,
+    Check,
+    Tooltip,
+    Slider,
+  },
+
+  data() {
+    return {
+      useTranslate: false,
+      transCode: 'ko-KR',
+      transMultiple: true,
+      sttSync: true,
+      ttsAllow: false,
+    }
+  },
+  computed: {
+    ...mapGetters(['translate', 'languageCodes']),
+  },
+  watch: {
+    transCode(language) {
+      this.setTranslateCode(language)
+    },
+    transMultiple(flag) {
+      this.setTranslateMultiple(flag)
+    },
+    useTranslate(flag) {
+      this.setTranslateFlag(flag)
+    },
+    sttSync(flag) {
+      this.setSttSync(flag)
+    },
+    ttsAllow(flag) {
+      this.setTtsAllow(flag)
+    },
+  },
+  methods: {
+    ...mapActions(['setTranslate']),
+    changeTranslate(item, setting) {
+      const param = {}
+      param[item] = setting
+      this.setTranslate(param)
+      this.$localStorage.setTranslate(item, setting)
+      // this.showToast()
+    },
+    setTranslateCode(code) {
+      this.setTranslate({ code })
+      this.$localStorage.setTranslate('code', code)
+    },
+    disableTranslate() {
+      this.toastDefault(this.$t('workspace.setting_translate_disable_message'))
+    },
+    setTranslateFlag(flag) {
+      this.setTranslate({
+        flag,
+      })
+      this.$localStorage.setTranslate('flag', flag)
+    },
+    setTranslateMultiple(multiple) {
+      this.setTranslate({ multiple })
+      this.$localStorage.setTranslate('multiple', multiple)
+    },
+    setSttSync(sttSync) {
+      this.setTranslate({ sttSync })
+      this.$localStorage.setTranslate('sttSync', sttSync)
+    },
+    setTtsAllow(ttsAllow) {
+      this.setTranslate({ ttsAllow })
+      this.$localStorage.setTranslate('ttsAllow', ttsAllow)
+    },
+    init() {
+      this.useTranslate = this.translate.flag
+      this.transCode = this.translate.code
+      this.transMultiple = this.translate.multiple
+      this.sttSync = this.translate.sttSync
+      this.ttsAllow = this.translate.ttsAllow
+    },
+  },
+  created() {
+    this.init()
+  },
+}
+</script>
+
+<style></style>
