ac72be9698edbeded4aa47ed51084b6f2b5baf52
JeongHyeon-VIRNECT <sky456139@virnect.com>
2021-02-09 14:06:25 +0900

    feat: gradle kotlin dsl migrate to groovy dsl and refactoring library dependencies

--- service-java-client/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    //implementation(kotlin("stdlib-jdk8"))
-    testImplementation("junit", "junit", "4.12")
-    implementation(group = "org.apache.httpcomponents", name = "httpclient", version = "4.5.11")
-    implementation(group = "com.google.code.gson", name = "gson", version = "2.8.6")
-    implementation(group = "org.slf4j", name = "slf4j-api", version = "1.7.30")
-}
