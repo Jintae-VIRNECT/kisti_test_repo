ec748b25976cf0ddff74debacfb4366857965c1b
ykmo <ykmo@virnect.com>
2021-02-16 14:45:49 +0900

    Feat:add service screen vue file

---
+++ src/components/service/ServiceScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main-body screen">
+    <screen-stop-button></screen-stop-button>
+    <menus></menus>
+    <main-video></main-video>
+  </div>
+</template>
+
+<script>
+import Menus from './tools/Menus'
+import MainVideo from './stream/MainVideo'
+import ScreenStopButton from './screen/ScreenStopButton'
+export default {
+  name: 'ServiceScreen',
+  components: {
+    Menus,
+    MainVideo,
+    ScreenStopButton,
+  },
+}
+</script>
