9f1af5b7545d73a5467ad5d1cd7d95e96b432c5c
ykmo <ykmo@virnect.com>
2021-02-22 19:57:52 +0900

    Feat:add screen stop button on service stream

--- src/components/service/ServiceStream.vue
+++ src/components/service/ServiceStream.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main-body stream">
-    <stream-tools></stream-tools>
+    <stream-tools v-if="isStreamView"></stream-tools>
+    <screen-stop-button v-else></screen-stop-button>
+
     <menus></menus>
     <main-video></main-video>
   </div>
@@ -10,12 +12,24 @@
 import StreamTools from './tools/StreamTools'
 import Menus from './tools/Menus'
 import MainVideo from './stream/MainVideo'
+import ScreenStopButton from './stream/ScreenStopButton'
+import { VIEW } from 'configs/view.config'
+
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'ServiceStream',
   components: {
     StreamTools,
     Menus,
     MainVideo,
+    ScreenStopButton,
+  },
+  computed: {
+    ...mapGetters(['view']),
+    isStreamView() {
+      return this.view === VIEW.STREAM
+    },
   },
 }
 </script>
