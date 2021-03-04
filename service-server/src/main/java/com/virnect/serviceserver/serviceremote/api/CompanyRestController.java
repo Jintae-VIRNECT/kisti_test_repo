50ec7b5b49476a540c5c6434610e40ac599baa87
Jiwoong-VIRNECT <jwhan@virnect.com>
2021-03-04 11:58:20 +0900

    fix; admin change to company, company api move to CompanyController

---
+++ service-server/src/main/java/com/virnect/serviceserver/serviceremote/api/CompanyRestController.java
@@ -0,0 +1,172 @@
+package com.virnect.serviceserver.serviceremote.api;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import com.virnect.serviceserver.global.config.RemoteServiceConfig;
+import com.virnect.serviceserver.serviceremote.application.CompanyService;
+import com.virnect.serviceserver.serviceremote.dto.request.company.CompanyRequest;
+import com.virnect.serviceserver.serviceremote.dto.request.company.CompanyResponse;
+import com.virnect.data.error.ErrorCode;
+import com.virnect.data.error.exception.RestServiceException;
+import com.virnect.data.global.common.ApiResponse;
+import com.virnect.data.infra.utils.LogMessage;
+import com.virnect.serviceserver.serviceremote.dto.response.company.CompanyInfoResponse;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/remote")
+public class CompanyRestController {
+
+    private static final String TAG = CompanyRestController.class.getSimpleName();
+    private static final String REST_PATH = "/remote/admin";
+
+    private final CompanyService companyService;
+    private final RemoteServiceConfig config;
+
+    @ApiOperation(value = "Create Company Information ", notes = "회사별 서비스 정보를 생성 합니다.")
+    @PostMapping(value = "admin/company")
+    public ResponseEntity<ApiResponse<CompanyResponse>> createCompanyRequestHandler(
+        @RequestBody @Valid CompanyRequest companyRequest,
+        BindingResult result
+    ) {
+        LogMessage.formedInfo(
+            TAG,
+            "REST API: POST "
+                + REST_PATH + "::"
+                + (companyRequest.toString() != null ? companyRequest.toString() : "{}"),
+            "createCompanyRequestHandler"
+        );
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(message ->
+                LogMessage.formedError(
+                    TAG,
+                    "REST API: POST " + REST_PATH,
+                    "createCompanyRequestHandler",
+                    LogMessage.PARAMETER_ERROR,
+                    message.toString()
+                )
+            );
+            throw new RestServiceException(ErrorCode.ERR_INVALID_REQUEST_PARAMETER);
+        }
+
+        ApiResponse<CompanyResponse> responseData = companyService.createCompany(companyRequest);
+
+        return ResponseEntity.ok(responseData);
+    }
+
+    @ApiOperation(value = "Update Company Information ", notes = "생성한 회사 정보를 수정합니다.")
+    @PutMapping(value = "admin/company")
+    public ResponseEntity<ApiResponse<CompanyResponse>> updateCompanyRequestHandler(
+        @RequestBody @Valid CompanyRequest companyRequest,
+        BindingResult result
+    ) {
+        LogMessage.formedInfo(
+            TAG,
+            "REST API: POST " + REST_PATH,
+            "updateCompanyRequestHandler"
+        );
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(message ->
+                LogMessage.formedError(
+                    TAG,
+                    "REST API: POST " + REST_PATH,
+                    "updateCompanyRequestHandler",
+                    LogMessage.PARAMETER_ERROR,
+                    message.toString()
+                )
+            );
+            throw new RestServiceException(ErrorCode.ERR_INVALID_REQUEST_PARAMETER);
+        }
+
+        ApiResponse<CompanyResponse> responseData = companyService.updateCompany(companyRequest);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @ApiOperation(value = "Service Company Information", notes = "회사별 서비스 정보를 제공합니다.")
+    @GetMapping(value = "company/{workspaceId}/{userId}")
+    public ResponseEntity<ApiResponse<CompanyInfoResponse>> getCompanyInfo(
+        @PathVariable String workspaceId,
+        @PathVariable String userId
+    ) {
+        LogMessage.formedInfo(
+            TAG,
+            "REST API: GET "
+                + REST_PATH + "/"
+                + (workspaceId != null ? workspaceId : "{}") + "::"
+                + (userId != null ? userId : "{}"),
+            "getCompanyInfo"
+        );
+
+        if (userId == null || userId.isEmpty()) {
+            throw new RestServiceException(ErrorCode.ERR_INVALID_REQUEST_PARAMETER);
+        }
+
+        if (workspaceId == null || workspaceId.isEmpty()) {
+            throw new RestServiceException(ErrorCode.ERR_INVALID_REQUEST_PARAMETER);
+        }
+
+        //todo: delete check user is valid
+        //DataProcess<UserInfoResponse> userInfo = this.dataRepository.checkUserValidation(userId);
+        //log.info("COMPANY INFO :: USER INFO :: {}", userInfo.getData().getDescription());
+
+        String policyLocation = config.remoteServiceProperties.getServicePolicyLocation();
+        //ApiResponse<CompanyInfoResponse> responseData = validationService.getCompanyInfo(workspaceId, userId, policyLocation);
+        //ApiResponse<CompanyInfoResponse> responseData = validationService.getCompanyInfo(workspaceId, userId);
+        ApiResponse<CompanyInfoResponse> responseData = companyService.getCompanyInfoByCompanyCode(workspaceId, userId, 1, policyLocation);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @ApiOperation(value = "Service Company Information", notes = "회사별 서비스 정보를 제공합니다.")
+    @GetMapping(value = "company")
+    public ResponseEntity<ApiResponse<CompanyInfoResponse>> getCompanyInfoRequestHandler(
+        @RequestParam(name = "companyCode") int companyCode,
+        @RequestParam(name = "workspaceId") String workspaceId,
+        @RequestParam(name = "userId") String userId
+    ) {
+        LogMessage.formedInfo(
+            TAG,
+            "REST API: GET "
+                + REST_PATH + "::"
+                + "workspaceId:" + (workspaceId != null ? workspaceId : "{}") + "/"
+                + "userId:" + (userId != null ? userId : "{}") + "/"
+                + "companyCode:" + companyCode,
+            "getCompanyInfoRequestHandler"
+        );
+
+        String policyLocation = config.remoteServiceProperties.getServicePolicyLocation();
+        ApiResponse<CompanyInfoResponse> responseData = companyService.getCompanyInfoByCompanyCode(workspaceId, userId, companyCode, policyLocation);
+        return ResponseEntity.ok(responseData);
+    }
+
+    /*@ApiOperation(value = "Service License Validity ", notes = "서비스 라이선스 유효성을 확인합니다.")
+    @GetMapping(value = "licenses/{workspaceId}/{userId}")
+    public ResponseEntity<ApiResponse<LicenseInfoListResponse>> getLicenseInfo(
+            @PathVariable String workspaceId,
+            @PathVariable String userId) {
+
+        log.info("REST API: GET {}/licenses/{}/{}", REST_PATH,
+                workspaceId != null ? workspaceId.toString() : "{}",
+                userId != null ? userId : "{}");
+        if(workspaceId.isEmpty() || userId.isEmpty()) {
+            throw new RestServiceException(ErrorCode.ERR_INVALID_REQUEST_PARAMETER);
+        }
+        ApiResponse<LicenseInfoListResponse> response = this.remoteGatewayService.getLicenseValidity(workspaceId, userId);
+        return ResponseEntity.ok(response);
+    }*/
+}
---
+++ service-server/src/main/java/com/virnect/serviceserver/serviceremote/application/CompanyService.java
@@ -0,0 +1,362 @@
+package com.virnect.serviceserver.serviceremote.application;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import com.virnect.data.dao.company.CompanyRepository;
+import com.virnect.data.domain.Company;
+import com.virnect.data.domain.Language;
+import com.virnect.data.domain.session.SessionType;
+import com.virnect.data.error.ErrorCode;
+import com.virnect.data.global.common.ApiResponse;
+import com.virnect.data.infra.utils.JsonUtil;
+import com.virnect.data.infra.utils.LogMessage;
+import com.virnect.serviceserver.serviceremote.dto.constraint.CompanyConstants;
+import com.virnect.serviceserver.serviceremote.dto.constraint.LanguageCode;
+import com.virnect.serviceserver.serviceremote.dto.constraint.LicenseItem;
+import com.virnect.serviceserver.serviceremote.dto.constraint.TranslationItem;
+import com.virnect.serviceserver.serviceremote.dto.request.company.CompanyRequest;
+import com.virnect.serviceserver.serviceremote.dto.request.company.CompanyResponse;
+import com.virnect.serviceserver.serviceremote.dto.response.company.CompanyInfoResponse;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CompanyService {
+
+	private static final String TAG = CompanyService.class.getSimpleName();
+
+	private final ModelMapper modelMapper;
+	private final SessionTransactionalService sessionService;
+	private final CompanyRepository companyRepository;
+
+	public ApiResponse<CompanyResponse> createCompany(CompanyRequest companyRequest) {
+
+		ApiResponse<CompanyResponse> responseData;
+
+		Company company = Company.builder()
+			.companyCode(companyRequest.getCompanyCode())
+			.workspaceId(companyRequest.getWorkspaceId())
+			.licenseName(companyRequest.getLicenseName())
+			.sessionType(companyRequest.getSessionType())
+			.recording(companyRequest.isRecording())
+			.storage(companyRequest.isStorage())
+			.translation(companyRequest.isTranslation())
+			.sttStreaming(companyRequest.isSttStreaming())
+			.sttSync(companyRequest.isSttSync())
+			.tts(companyRequest.isTts())
+			.videoRestrictedMode(companyRequest.isVideoRestrictedMode())
+			.audioRestrictedMode(companyRequest.isAudioRestrictedMode())
+			.localRecording(companyRequest.isLocalRecording())
+			.build();
+
+		company.setLanguage(
+			Language.builder()
+			.transKoKr(companyRequest.getLanguage().isTransKoKr())
+			.transEnUs(companyRequest.getLanguage().isTransEnUs())
+			.transJaJp(companyRequest.getLanguage().isTransJaJp())
+			.transZh(companyRequest.getLanguage().isTransZh())
+			.transEsEs(companyRequest.getLanguage().isTransEsEs())
+			.transFrFr(companyRequest.getLanguage().isTransFrFr())
+			.transPlPl(companyRequest.getLanguage().isTransPlPl())
+			.transRuRu(companyRequest.getLanguage().isTransRuRu())
+			.transUkUa(companyRequest.getLanguage().isTransUkUa())
+			.company(company)
+			.build()
+		);
+
+		try{
+			if (sessionService.createCompany(company) != null) {
+				CompanyResponse companyResponse = new CompanyResponse();
+				companyResponse.setWorkspaceId(company.getWorkspaceId());
+				companyResponse.setLicenseName(company.getLicenseName());
+				companyResponse.setSessionType(company.getSessionType());
+				responseData = new ApiResponse<>(companyResponse);
+			} else {
+				responseData = new ApiResponse<>(ErrorCode.ERR_COMPANY_CREATE_FAIL);
+			}
+		} catch (Exception e) {
+			responseData = new ApiResponse<>(ErrorCode.ERR_COMPANY_CREATE_FAIL);
+		}
+		return responseData;
+	}
+
+	public ApiResponse<CompanyResponse> updateCompany(CompanyRequest companyRequest) {
+
+		ApiResponse<CompanyResponse> responseData;
+
+		Company company = sessionService.getCompany(companyRequest.getWorkspaceId());
+
+		if (company == null) {
+			responseData = new ApiResponse<>(ErrorCode.ERR_COMPANY_NOT_EXIST);
+		} else {
+
+			company.setCompanyCode(companyRequest.getCompanyCode());
+			company.setLicenseName(companyRequest.getLicenseName());
+			company.setSessionType(companyRequest.getSessionType());
+			company.setRecording(companyRequest.isRecording());
+			company.setStorage(companyRequest.isStorage());
+			company.setTranslation(companyRequest.isTranslation());
+			company.setSttStreaming(companyRequest.isSttStreaming());
+			company.setSttSync(companyRequest.isSttSync());
+			company.setTts(companyRequest.isTts());
+			company.setVideoRestrictedMode(companyRequest.isVideoRestrictedMode());
+			company.setAudioRestrictedMode(companyRequest.isAudioRestrictedMode());
+			company.setLocalRecording(companyRequest.isLocalRecording());
+
+			company.setLanguage(
+				Language.builder()
+				.transKoKr(companyRequest.getLanguage().isTransKoKr())
+				.transEnUs(companyRequest.getLanguage().isTransEnUs())
+				.transJaJp(companyRequest.getLanguage().isTransJaJp())
+				.transZh(companyRequest.getLanguage().isTransZh())
+				.transEsEs(companyRequest.getLanguage().isTransEsEs())
+				.transFrFr(companyRequest.getLanguage().isTransFrFr())
+				.transPlPl(companyRequest.getLanguage().isTransPlPl())
+				.transRuRu(companyRequest.getLanguage().isTransRuRu())
+				.transUkUa(companyRequest.getLanguage().isTransUkUa())
+				.company(company)
+				.build()
+			);
+
+			try{
+				if (sessionService.updateCompany(company) != null) {
+					CompanyResponse companyResponse = new CompanyResponse();
+					companyResponse.setWorkspaceId(company.getWorkspaceId());
+					companyResponse.setLicenseName(company.getLicenseName());
+					companyResponse.setSessionType(company.getSessionType());
+					responseData = new ApiResponse<>(companyResponse);
+				} else {
+					responseData = new ApiResponse<>(ErrorCode.ERR_COMPANY_UPDATE_FAIL);
+				}
+			} catch (Exception e) {
+				responseData = new ApiResponse<>(ErrorCode.ERR_COMPANY_UPDATE_FAIL);
+			}
+		}
+		return responseData;
+	}
+
+	public ApiResponse<CompanyInfoResponse> getCompanyInfo(
+		String workspaceId,
+		String userId,
+		String policyLocation
+	) {
+		ApiResponse<CompanyInfoResponse> responseData;
+
+		Company company = companyRepository.findByWorkspaceId(workspaceId).orElse(null);
+		CompanyInfoResponse companyInfoResponse = null;
+		try {
+			companyInfoResponse = loadServicePolicy(workspaceId, policyLocation);
+		} catch (IOException e) {
+			new ApiResponse<>(new CompanyInfoResponse(), ErrorCode.ERR_IO_EXCEPTION);
+		}
+
+		responseData = new ApiResponse<>(companyInfoResponse);
+
+		return responseData;
+	}
+
+	public ApiResponse<CompanyInfoResponse> getCompanyInfoByCompanyCode(
+		String workspaceId,
+		String userId,
+		int companyCode,
+		String policyLocation
+	) {
+
+		ApiResponse<CompanyInfoResponse> responseData;
+
+		CompanyInfoResponse companyInfoResponse = null;
+
+		companyCode = 0;
+
+		if (companyCode != CompanyConstants.COMPANY_VIRNECT) {
+			Company company = companyRepository.findByWorkspaceId(workspaceId).orElse(null);
+			if (company != null) {
+				companyInfoResponse = modelMapper.map(company, CompanyInfoResponse.class);
+				Language language = company.getLanguage();
+				List<LanguageCode> languageCodes = combineLanguageCode(language);
+				companyInfoResponse.setLanguageCodes(languageCodes);
+				responseData = new ApiResponse<>(companyInfoResponse);
+			} else {
+				CompanyInfoResponse empty = new CompanyInfoResponse();
+				responseData = new ApiResponse<>(empty, ErrorCode.ERR_COMPANY_INVALID_CODE);
+			}
+		} else {
+			try {
+				companyInfoResponse = loadServicePolicy(workspaceId, policyLocation);
+			} catch (IOException e) {
+				new ApiResponse<>(new CompanyInfoResponse(), ErrorCode.ERR_IO_EXCEPTION);
+			}
+			responseData = new ApiResponse<>(companyInfoResponse);
+		}
+		return responseData;
+	}
+
+	private CompanyInfoResponse loadServicePolicy(String workspaceId, String policyLocation) throws IOException {
+		//String policyLocation = config.remoteServiceProperties.getServicePolicyLocation();
+		if (policyLocation == null || policyLocation.isEmpty()) {
+			LogMessage.formedError(
+				TAG,
+				"initialise service policy",
+				"loadServicePolicy",
+				"service policy file path is null or empty. trying to set default service policy."
+			);
+			return defaultCompanyInfo(workspaceId);
+
+		} else {
+			JsonUtil jsonUtil = new JsonUtil();
+			JsonObject jsonObject;
+			if (policyLocation.startsWith("/")) {
+				Path path = Paths.get(policyLocation);
+				jsonObject = jsonUtil.fromFileToJsonObject(path.toAbsolutePath().toString());
+			} else {
+				InputStream inputStream = getClass().getClassLoader().getResourceAsStream(policyLocation);
+				if (inputStream == null) {
+					LogMessage.formedError(
+						TAG,
+						"initialise service policy",
+						"loadServicePolicy",
+						"service policy file path is null or empty. trying to set default service policy."
+					);
+					return defaultCompanyInfo(workspaceId);
+				} else {
+					LogMessage.formedError(
+						TAG,
+						"initialise service policy",
+						"loadServicePolicy",
+						"service policy file path is null or empty. set service policy using service policy file."
+					);
+					jsonObject = jsonUtil.fromInputStreamToJsonObject(inputStream);
+					inputStream.close();
+
+					return parseServicePolicyJsonObject(jsonObject, workspaceId);
+				}
+			}
+			LogMessage.formedInfo(
+				TAG,
+				"initialise service policy",
+				"loadServicePolicy",
+				"load service policy is success."
+			);
+			return parseServicePolicyJsonObject(jsonObject, workspaceId);
+		}
+	}
+
+	private CompanyInfoResponse defaultCompanyInfo(String workspaceId) {
+		List<LanguageCode> languageCodes = new ArrayList<>(Arrays.asList(
+			new LanguageCode(TranslationItem.LANGUAGE_KR),
+			new LanguageCode(TranslationItem.LANGUAGE_EN),
+			new LanguageCode(TranslationItem.LANGUAGE_JP),
+			new LanguageCode(TranslationItem.LANGUAGE_ZH),
+			new LanguageCode(TranslationItem.LANGUAGE_FR),
+			new LanguageCode(TranslationItem.LANGUAGE_ES),
+			new LanguageCode(TranslationItem.LANGUAGE_RU),
+			new LanguageCode(TranslationItem.LANGUAGE_UK),
+			new LanguageCode(TranslationItem.LANGUAGE_PL)
+		));
+
+		CompanyInfoResponse companyInfoResponse = new CompanyInfoResponse();
+		companyInfoResponse.setCompanyCode(0);
+		companyInfoResponse.setWorkspaceId(workspaceId);
+		companyInfoResponse.setLicenseName(LicenseItem.ITEM_PRODUCT.getItemName());
+		companyInfoResponse.setSessionType(SessionType.OPEN);
+		companyInfoResponse.setRecording(true);
+		companyInfoResponse.setStorage(true);
+		companyInfoResponse.setTranslation(true);
+		companyInfoResponse.setSttSync(true);
+		companyInfoResponse.setSttStreaming(true);
+		companyInfoResponse.setTts(true);
+		companyInfoResponse.setLanguageCodes(languageCodes);
+
+		return companyInfoResponse;
+	}
+
+	private CompanyInfoResponse parseServicePolicyJsonObject(JsonObject jsonObject, String workspaceId) {
+		CompanyInfoResponse companyInfoResponse = new CompanyInfoResponse();
+		JsonObject policyObject = jsonObject.getAsJsonObject("company_info");
+		companyInfoResponse.setCompanyCode(policyObject.get("company_code").getAsInt());
+
+		if (policyObject.get("workspace_id").getAsString().isEmpty()) {
+			companyInfoResponse.setWorkspaceId(workspaceId);
+		} else {
+			companyInfoResponse.setWorkspaceId(policyObject.get("workspace_id").getAsString());
+		}
+
+		companyInfoResponse.setLicenseName(policyObject.get("license_name").getAsString());
+		companyInfoResponse.setSessionType(SessionType.findBy(policyObject.get("session_type").getAsString()));
+		companyInfoResponse.setRecording(policyObject.get("recording").getAsBoolean());
+		companyInfoResponse.setStorage(policyObject.get("storage").getAsBoolean());
+		companyInfoResponse.setTranslation(policyObject.get("translation").getAsBoolean());
+		companyInfoResponse.setSttSync(policyObject.get("stt_sync").getAsBoolean());
+		companyInfoResponse.setSttStreaming(policyObject.get("stt_streaming").getAsBoolean());
+		companyInfoResponse.setTts(policyObject.get("tts").getAsBoolean());
+
+		JsonArray jsonArray = policyObject.getAsJsonArray("language_codes");
+		ArrayList<LanguageCode> languageCodes = new ArrayList<>();
+		for (int i = 0; i < jsonArray.size(); i++) {
+			LanguageCode languageCode = new LanguageCode(
+				jsonArray.get(i).getAsJsonObject().get("text").getAsString(),
+				jsonArray.get(i).getAsJsonObject().get("code").getAsString()
+			);
+			languageCodes.add(languageCode);
+		}
+		companyInfoResponse.setLanguageCodes(languageCodes);
+		return companyInfoResponse;
+	}
+
+	private List<LanguageCode> combineLanguageCode(Language language) {
+		List<LanguageCode> languageCodes = new ArrayList<>();
+		if (language.isTransKoKr()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_KR);
+			languageCodes.add(languageCode);
+		}
+		if (language.isTransEnUs()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_EN);
+			languageCodes.add(languageCode);
+		}
+		if (language.isTransJaJp()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_JP);
+			languageCodes.add(languageCode);
+		}
+		if (language.isTransZh()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_ZH);
+			languageCodes.add(languageCode);
+		}
+		if (language.isTransFrFr()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_FR);
+			languageCodes.add(languageCode);
+		}
+		if (language.isTransEsEs()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_ES);
+			languageCodes.add(languageCode);
+		}
+		if (language.isTransRuRu()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_RU);
+			languageCodes.add(languageCode);
+		}
+		if (language.isTransUkUa()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_UK);
+			languageCodes.add(languageCode);
+		}
+		if (language.isTransPlPl()) {
+			LanguageCode languageCode = new LanguageCode(TranslationItem.LANGUAGE_PL);
+			languageCodes.add(languageCode);
+		}
+		return languageCodes;
+	}
+
+}
