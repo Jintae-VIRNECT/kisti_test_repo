1809b9a3a12079e5c76a2b85601c625f437744d3
SangBaek-VIRNECT <lsb@virnect.com>
2021-03-04 11:21:29 +0900

    feat: LNB fold unfold

--- src/components/layout/TheSidebarMenuList.vue
+++ src/components/layout/TheSidebarMenuList.vue
@@ -17,10 +17,16 @@
           @click.native.stop="openCollapse(menu.collapse)"
         >
           <img :src="menu.image" :alt="$t(menu.label)" />
+          <span>{{ $t(menu.label) }}</span>
         </nuxt-link>
         <!-- 링크 -->
-        <nuxt-link v-else :to="menu.path">
+        <nuxt-link
+          v-else
+          :to="menu.path"
+          @click.native="$emit('closeCollapse')"
+        >
           <img :src="menu.image" :alt="$t(menu.label)" />
+          <span>{{ $t(menu.label) }}</span>
         </nuxt-link>
       </el-tooltip>
     </li>
@@ -47,23 +53,36 @@ export default {
   padding: 0 12px;
 
   & > a {
+    position: relative;
     display: block;
     width: 100%;
     padding: 7px;
     line-height: 0;
     background-repeat: no-repeat;
     background-size: 100%;
+    border-radius: 4px;
     mask: url('~assets/images/icon/ic-bg.svg');
     mask-size: 100%;
+    mask-position: center;
 
     & > img {
-      width: 100%;
+      width: 21px;
       opacity: 0.65;
+    }
+    & > span {
+      position: absolute;
+      width: 200px;
+      margin: auto 18px;
+      overflow: hidden;
+      color: #fff;
+      line-height: 22px;
+      opacity: 0.75;
     }
 
     &:hover {
       background: rgba(65, 81, 109, 0.7);
-      & > img {
+      & > img,
+      & > span {
         opacity: 0.9;
       }
     }
@@ -77,7 +96,7 @@ export default {
   }
 
   .el-divider {
-    width: 30px;
+    width: 100%;
     margin: 0 auto;
     background: rgba(255, 255, 255, 0.2);
   }
@@ -85,5 +104,11 @@ export default {
 
 .the-sidebar__tooltip {
   transform: translateX(8px);
+}
+
+.the-sidebar__inner--wide .the-sidebar__menus__item {
+  & > a {
+    mask-size: cover;
+  }
 }
 </style>
