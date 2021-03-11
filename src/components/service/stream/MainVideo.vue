9997ac77bb0366028bb85bae3390b33285a75350
wooka <wooka@virnect.com>
2021-03-11 11:10:46 +0900

    Fix: screen share main view full size allow

--- src/components/service/stream/MainVideo.vue
+++ src/components/service/stream/MainVideo.vue
@@ -65,7 +65,7 @@
         <transition name="opacity">
           <fullscreen
             :hide.sync="hideFullBtn"
-            v-show="!hideFullBtn && !mainView.screenShare"
+            v-show="!hideFullBtn"
           ></fullscreen>
         </transition>
       </template>
@@ -243,9 +243,12 @@ export default {
       const validCameraStatus = this.cameraStatus !== -1
       const cameraOff = this.cameraStatus.state === 'off'
       const cameraBackground = this.cameraStatus.state === 'background'
+      const screenSharing = this.mainView.screenShare
 
       return (
-        validCameraStatus && ((this.loaded && cameraOff) || cameraBackground)
+        validCameraStatus &&
+        ((this.loaded && cameraOff) || cameraBackground) &&
+        !screenSharing
       )
     },
   },
