50a1944d4c5dd4c24c86129bcfb3cf915b7480c9
SangBaek-VIRNECT <lsb@virnect.com>
2021-03-22 16:25:27 +0900

    fix: safari ui bug

--- src/mixins/columns.js
+++ src/mixins/columns.js
@@ -24,4 +24,15 @@ export default {
     ColumnPrice,
     ColumnPlan,
   },
+  mounted() {
+    // 사파리 테이블 버그
+    document
+      .querySelectorAll('.el-table__body')
+      .forEach(table => (table.style.tableLayout = 'auto'))
+    setTimeout(() => {
+      document
+        .querySelectorAll('.el-table__body')
+        .forEach(table => (table.style.tableLayout = 'fixed'))
+    }, 10)
+  },
 }
