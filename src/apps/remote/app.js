ccc54a4055d7819feef0d99b18a9b6f49009f216
wooka <wooka@virnect.com>
2021-03-30 16:35:11 +0900

    Version: v2.3.1.0

--- src/apps/remote/app.js
+++ src/apps/remote/app.js
@@ -53,7 +53,7 @@ if ('remote.virnect.com' === location.host) {
 
 const EventBus = new Vue()
 Vue.prototype.$eventBus = EventBus
-Vue.prototype.$version = version.replace(/-\w/, '')
+Vue.prototype.$version = version
 
 const app = new Vue({
   el: '#container',
@@ -70,7 +70,7 @@ export default app
 setTimeout(
   console.log.bind(
     console,
-    `%cVIRNECT Remote v${version.replace(/-/, '.')}`,
+    `%cVIRNECT Remote v${version}`,
     'font-size:25px;color:#0f75f5;font-weight:700;font-family:roboto',
   ),
 )
