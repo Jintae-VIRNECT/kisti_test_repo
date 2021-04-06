8a26f34fb2d254eb79e3be6ad0886408eff754b0
ykmo <ykmo@virnect.com>
2021-04-06 15:10:52 +0900

    Refactor:add comment in config

--- src/configs/push.config.js
+++ src/configs/push.config.js
@@ -1,15 +1,29 @@
+/**
+ * 메시지 서버에서 클라이언트로 전송되는 이벤트 관련 값 정의
+ * @see https://www.notion.so/virnect/Remote-2-0-Notification-Server-Message-e2a87653711c4782808e38b3b5f16820
+ */
+
+/**
+ * 메시지 서버 경로
+ */
 export const DESTINATION = {
   PUSH: '/topic/push',
 }
 
+/**
+ * 메시지 서버 키값
+ */
 export const KEY = {
   SERVICE_TYPE: 'remote',
 }
 
+/**
+ * 메시지 서버에서 오는 이벤트 정의
+ */
 export const EVENT = {
-  LICENSE_EXPIRATION: 'licenseExpiration',
-  LICENSE_EXPIRED: 'licenseExpired',
-  INVITE: 'invite',
-  INVITE_DENIED: 'invitationDenied',
-  INVITE_ACCEPTED: 'invitationAccepted',
+  LICENSE_EXPIRATION: 'licenseExpiration', //라이센스 시간 만료 임박
+  LICENSE_EXPIRED: 'licenseExpired', //라이센스 만료
+  INVITE: 'invite', //협업 초대
+  INVITE_DENIED: 'invitationDenied', //협업 초대 거부
+  INVITE_ACCEPTED: 'invitationAccepted', //협업 초대 승인
 }
--- src/configs/status.config.js
+++ src/configs/status.config.js
@@ -1,22 +1,38 @@
+/**
+ * 상태값 관련 정의
+ */
+
+/**
+ * 협업 방 내부에 참가되어 있는지 여부 정의
+ */
 export const STATUS = {
   UNLOAD: 'UNLOAD',
-  LOAD: 'LOAD',
+  LOAD: 'LOAD', //대상 유저가 협업 내에 접속되어 있는 상태
 }
 
+/**
+ * 워크스페이스에 부여된 역할 정의
+ */
 export const WORKSPACE_ROLE = {
   MASTER: 'MASTER',
   MANAGER: 'MANAGER',
   MEMBER: 'MEMBER',
 }
 
+/**
+ * 계정의 플랜 상태 정의
+ */
 export const PLAN_STATUS = {
-  ACTIVE: 'ACTIVE',
-  INACTIVE: 'INACTIVE',
-  EXCEEDED: 'EXCEEDED',
+  ACTIVE: 'ACTIVE', //정상
+  INACTIVE: 'INACTIVE', //만료
+  EXCEEDED: 'EXCEEDED', //할당된 플랜 초과
 }
 
+/**
+ * 협업 유형 정의
+ */
 export const ROOM_STATUS = {
-  PUBLIC: 'PUBLIC',
-  PRIVATE: 'PRIVATE',
-  OPEN: 'OPEN',
+  PUBLIC: 'PUBLIC', //TBD
+  PRIVATE: 'PRIVATE', //원격 협업
+  OPEN: 'OPEN', //오픈방
 }
