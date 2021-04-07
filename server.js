6478e446d353951bb92a8a7d6293619c537c06a2
ykmo <ykmo@virnect.com>
2021-04-07 12:25:42 +0900

    Feat:update helmet

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const route = require('./route')
 const app = express()
 const server = require('./server/module')
+const { initHelmet } = require('./server/helmet')
 const path = require('path')
-const helmet = require('helmet')
 const config = require('./server/config')
 const util = require('./server/util')
 const meta = require('./server/meta')
@@ -40,53 +40,7 @@ app.use((req, res, next) => {
   }
 })
 
-const initHelmet = async () => {
-  await config.init()
-
-  const urls = config.getUrls()
-  const allows = []
-  const cntAllows = []
-
-  for (const property in urls) {
-    if (property.includes('minio')) {
-      allows.push(urls[property])
-    }
-
-    if (property.includes('csp.wss')) {
-      cntAllows.push(urls[property])
-    } else if (property.includes('csp.')) {
-      allows.push(urls[property])
-    }
-  }
-
-  cntAllows.push(...allows)
-
-  app.use(
-    helmet({
-      frameguard: {
-        action: 'deny',
-      },
-      contentSecurityPolicy: {
-        directives: {
-          ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-          'script-src': ["'self'", ...allows],
-          'style-src': [
-            "'self'",
-            'https:',
-            'blob:',
-            "'unsafe-inline'",
-            ...allows,
-          ],
-          'connect-src': ["'self'", 'data:', ...cntAllows],
-          'img-src': ["'self'", 'data:', ...allows],
-          'media-src': ["'self'", ...allows],
-        },
-      },
-    }),
-  )
-}
-
-initHelmet().then(() => {
+initHelmet(app).then(() => {
   app.use(express.static(path.join(__dirname, 'dist')))
   app.use(route)
 
