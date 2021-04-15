8aadf99948e2d11c23da79ae38547ea98d4dbdf4
Jukyoung-VIRNECT <ljk@virnect.com>
2021-04-15 15:38:34 +0900

    docs: update release v1.2.27.1

--- src/main/java/com/virnect/download/application/DownloadService.java
+++ src/main/java/com/virnect/download/application/DownloadService.java
@@ -1,36 +1,26 @@
 package com.virnect.download.application;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.context.MessageSource;
-import org.springframework.stereotype.Service;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import com.virnect.download.dao.AppRepository;
-import com.virnect.download.dao.ProductRepository;
 import com.virnect.download.domain.App;
-import com.virnect.download.domain.Product;
 import com.virnect.download.dto.response.AppInfoListResponse;
 import com.virnect.download.dto.response.AppInfoResponse;
 import com.virnect.download.exception.DownloadException;
 import com.virnect.download.global.error.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DownloadService {
     private final AppRepository appRepository;
-    private final ProductRepository productRepository;
     private final ModelMapper modelMapper;
-    private final MessageSource messageSource;
 
 
     //TODO: 응답 데이터 수정필요,,
@@ -52,24 +42,24 @@ public class DownloadService {
 
     public AppInfoListResponse getAppList(String productName, Locale locale) {
         List<App> apps = appRepository.getActiveAppList(productName.toUpperCase());
-        Map<List<Object>, List<App>> result = apps.stream()
-                .collect(Collectors.groupingBy(app -> Arrays.asList(app.getDevice().getType(), app.getDevice().getModel(), app.getOs())));
+        Map<List<Object>, List<App>> result = apps.stream().collect(Collectors.groupingBy(app -> Arrays.asList(app.getDevice(), app.getOs())));
+        List<AppInfoResponse> appInfoResponseList = new ArrayList<>();
 
-        List<AppInfoResponse> appInfoResponseList = result.values().stream().map(appList -> {
-            App app = appList.stream().findFirst().orElse(null);
-            AppInfoResponse appInfoResponse = modelMapper.map(app, AppInfoResponse.class);
-            if (Objects.nonNull(app)) {
+        result.values().forEach(appList -> {
+            Optional<App> optionalApp = appList.stream().findFirst();
+            optionalApp.ifPresent(app -> {
+                AppInfoResponse appInfoResponse = modelMapper.map(app, AppInfoResponse.class);
                 appInfoResponse.setDeviceType(app.getDevice().getTypeDescription());
-                if(locale.getLanguage().equalsIgnoreCase("en")){
+                if (StringUtils.hasText(locale.getLanguage()) && locale.getLanguage().equalsIgnoreCase("en")) {
                     appInfoResponse.setDeviceName(app.getDevice().getModelDescriptionEng());
-                } else{
+                } else {
                     appInfoResponse.setDeviceName(app.getDevice().getModelDescription());
                 }
                 appInfoResponse.setReleaseTime(app.getCreatedDate());
                 appInfoResponse.setVersion("v" + app.getVersionName());
-            }
-            return appInfoResponse;
-        }).collect(Collectors.toList());
+                appInfoResponseList.add(appInfoResponse);
+            });
+        });
         return new AppInfoListResponse(appInfoResponseList);
     }
 
