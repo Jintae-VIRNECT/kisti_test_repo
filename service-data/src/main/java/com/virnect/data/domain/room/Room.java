a7128231986ab928d9b1ace09447d318d940ba9e
Jiwoong-VIRNECT <jwhan@virnect.com>
2021-04-22 13:27:53 +0900

    Test : cascade type test in room entity

--- service-data/src/main/java/com/virnect/data/domain/room/Room.java
+++ service-data/src/main/java/com/virnect/data/domain/room/Room.java
@@ -87,10 +87,10 @@ public class Room extends BaseTimeEntity {
     //private Set<Member> members = new HashSet<>();
     //private Collection<Member> Member;
     //add active or un-active type later
-    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER, orphanRemoval = true)
     private List<Member> members = new ArrayList<>();
 
-    @OneToOne(mappedBy = "room", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @OneToOne(mappedBy = "room", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER, orphanRemoval = true)
     private SessionProperty sessionProperty;
 
     @Builder
