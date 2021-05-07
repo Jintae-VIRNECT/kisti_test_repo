5385f48d5ed8e5b79047ed71d098155795204856
Yunsuk-VIRNECT <66057430+Yunsuk-VIRNECT@users.noreply.github.com>
2021-05-07 17:28:28 +0900

    Feat: 강제 로그아웃 선택, 수신 (강제로그아웃 API 미적용)

--- src/components/RemoteLayout.vue
+++ src/components/RemoteLayout.vue
@@ -4,8 +4,12 @@
 </template>
 
 <script>
+import confirmMixin from 'mixins/confirm'
+import auth from 'utils/auth'
+
 export default {
   name: 'RemoteLayout',
+  mixins: [confirmMixin],
   data() {
     return {
       key: 1,
@@ -41,11 +45,24 @@ export default {
       // window.addEventListener('orientationchange', this.viewPointSetting)
       this.viewPointSetting()
     }
+    //마스터에 의해 강제 로그아웃 당할 시
+    this.$eventBus.$on('forceLogout', () => {
+      console.log('forceLogout')
+
+      //악의적으로 UI를 제거할 경우를 대비하여, 팝업의 action이 아닌 클릭 이벤트 발생 시 로그아웃 처리하도록 한다.
+      const eventHandler = () => auth.logout()
+      window.addEventListener('click', eventHandler, { once: true })
+
+      //강제 로그아웃 알림 팝업
+      this.confirmDefault(this.$t('workspace.confirm_force_logout_received'))
+    })
   },
   beforeDestroy() {
+    //이벤트 버스 리스너 청소
     this.$eventBus.$off('reJoin', () => {
       this.key++
     })
+    this.$eventBus.$off('forceLogout')
     document.body.onorientationchange = () => {}
   },
 }
