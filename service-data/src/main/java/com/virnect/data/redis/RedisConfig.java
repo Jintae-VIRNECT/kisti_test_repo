7466a2c451fd995f03dfecb1806efe3743f15e08
Jiwoong-VIRNECT <jwhan@virnect.com>
2021-05-31 10:59:00 +0900

    Fix : redis profile (only local)

--- service-data/src/main/java/com/virnect/data/redis/RedisConfig.java
+++ service-data/src/main/java/com/virnect/data/redis/RedisConfig.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@Profile(value = {"local", "develop", "onpremise"})
+@Profile(value = {"local"})
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
