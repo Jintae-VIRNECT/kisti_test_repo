28701b522c1da5c03c0404d65f5ecf277dedef94
Yongho-VIRNECT <yhkim@virnect.com>
2021-07-07 10:34:22 +0900

    [DPLA-2983] Fix: 비밀번호 변경 시 변경 불가한 상태에도 파란색 처리
    ### :bug: 비밀번호 변경 시 변경 불가한 상태에도 파란색 처리
    - QA: https://virtualconnection.atlassian.net/browse/DPLA-2983
    - 처리: https://virtualconnection.atlassian.net/browse/DPLA-3250

--- src/components/profile/PasswordModal.vue
+++ src/components/profile/PasswordModal.vue
@@ -15,20 +15,26 @@
         @submit.native.prevent="submit"
       >
         <el-form-item :label="$t('profile.passwordChangeModal.newPassword')">
-          <el-input
-            show-password
-            v-model="form.password1"
-            :placeholder="
-              $t('profile.passwordChangeModal.newPasswordPlaceholder')
-            "
-          />
-          <el-input
-            show-password
-            v-model="form.password2"
-            :placeholder="
-              $t('profile.passwordChangeModal.newPasswordRepeatPlaceholder')
-            "
-          />
+          <ValidationProvider rules="password1Check" v-slot="{ errors }">
+            <el-input
+              show-password
+              v-model="form.password1"
+              :placeholder="
+                $t('profile.passwordChangeModal.newPasswordPlaceholder')
+              "
+              :class="{ 'input-danger': errors[0] }"
+            />
+          </ValidationProvider>
+          <ValidationProvider rules="password2Check" v-slot="{ errors }">
+            <el-input
+              show-password
+              v-model="form.password2"
+              :placeholder="
+                $t('profile.passwordChangeModal.newPasswordRepeatPlaceholder')
+              "
+              :class="{ 'input-danger': errors[0] }"
+            />
+          </ValidationProvider>
           <p
             class="caution"
             v-html="$t('profile.passwordChangeModal.caution')"
@@ -38,7 +44,7 @@
     </div>
 
     <div slot="footer" class="dialog-footer">
-      <el-button type="primary" @click="submit">
+      <el-button type="primary" @click="submit" :disabled="disabled">
         {{ $t('profile.passwordChangeModal.submit') }}
       </el-button>
     </div>
@@ -48,6 +54,7 @@
 <script>
 import dialogMixin from '@/mixins/dialog'
 import profileService from '@/services/profile'
+import { ValidationProvider, Validator } from 'vee-validate'
 
 export default {
   mixins: [dialogMixin],
@@ -59,6 +66,8 @@ export default {
       form: {
         password1: '',
         password2: '',
+        confirmPassword1: false,
+        confirmPassword2: false,
       },
     }
   },
@@ -66,6 +75,15 @@ export default {
     visible() {
       this.form.password1 = ''
       this.form.password2 = ''
+    },
+  },
+  computed: {
+    disabled() {
+      if (this.form.confirmPassword1 && this.form.confirmPassword2) {
+        return false
+      } else {
+        return true
+      }
     },
   },
   methods: {
@@ -76,13 +94,24 @@ export default {
       if (/[A-Z]/.test(password)) typeCount++
       if (/[$.$,$!$@$#$$$%]/.test(password)) typeCount++
 
-      if (typeCount < 3) return false
+      if (typeCount < 4) return false
       if (!/^.{8,20}$/.test(password)) return false
       if (/(.)\1\1\1/.test(password)) return false
       if (/(0123|1234|2345|3456|4567|5678|6789|7890)/.test(password))
         return false
       if (/(0987|9876|8765|7654|6543|5432|4321|3210)/.test(password))
         return false
+      return true
+    },
+    password1Check(password) {
+      if (!this.validate(password)) return false
+      this.form.confirmPassword1 = true
+      return true
+    },
+    password2Check(password) {
+      if (!this.validate(password)) return false
+      if (this.form.password1 !== password) return false
+      this.form.confirmPassword2 = true
       return true
     },
     async submit() {
@@ -120,6 +149,19 @@ export default {
         }
       }
     },
+  },
+  components: {
+    ValidationProvider,
+  },
+  created() {
+    // vee-validate 를 이용한 처리
+    Validator.extend('password1Check', {
+      validate: this.password1Check,
+    })
+
+    Validator.extend('password2Check', {
+      validate: this.password2Check,
+    })
   },
 }
 </script>
