81a2a1c4d8333c9577651361111a250ed3865c54
Yunsuk-VIRNECT <66057430+Yunsuk-VIRNECT@users.noreply.github.com>
2021-07-15 15:37:03 +0900

    Fix: backcanvas cors

--- src/components/service/drawing/DrawingCanvas.vue
+++ src/components/service/drawing/DrawingCanvas.vue
@@ -118,17 +118,23 @@ export default {
             canvas.renderAll()
 
             fabricImage.clone(cbImg => {
-              this.backCanvas.setBackgroundImage(cbImg, () => {
-                this.backCanvas.setWidth(canvasSize.width)
-                this.backCanvas.setHeight(canvasSize.height)
-                this.backCanvas.backgroundImage.set({
-                  scaleX: canvasSize.scale,
-                  scaleY: canvasSize.scale,
-                })
-                this.backCanvas.renderAll()
+              this.backCanvas.setBackgroundImage(
+                cbImg,
+                () => {
+                  this.backCanvas.setWidth(canvasSize.width)
+                  this.backCanvas.setHeight(canvasSize.height)
+                  this.backCanvas.backgroundImage.set({
+                    scaleX: canvasSize.scale,
+                    scaleY: canvasSize.scale,
+                  })
+                  this.backCanvas.renderAll()
 
-                this.updateHistory()
-              })
+                  this.updateHistory()
+                },
+                {
+                  crossOrigin: 'anonymous',
+                },
+              )
             })
 
             resolve(canvas)
