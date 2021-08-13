9dd8a8f3c7f75e63537a98905c0b29567f35a9b9
ykmo <ykmo@virnect.com>
2021-08-13 14:41:12 +0900

    Feat:불필요한 props 제거

--- src/components/workspace/partials/ModalCreateRoomInvite.vue
+++ src/components/workspace/partials/ModalCreateRoomInvite.vue
@@ -13,7 +13,7 @@
       ></icon-button>
     </div>
     <div class="createroom-user__body">
-      <scroller v-if="users.length > 0" :height="scrollHeight">
+      <scroller v-if="users.length > 0">
         <div>
           <wide-card
             v-for="(user, idx) of users"
@@ -97,10 +97,6 @@ export default {
     loading: {
       type: Boolean,
       default: false,
-    },
-    scrollHeight: {
-      type: String,
-      default: null,
     },
   },
   computed: {
