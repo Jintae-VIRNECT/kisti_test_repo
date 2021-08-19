7c3433e8871b465e06e29abb49458d8b5e8b2e0a
JeongHyeon-VIRNECT <sky456139@virnect.com>
2021-08-19 17:26:49 +0900

    DEVCENTER-4784 [PF-API][LGD 보안요건] 서버 스케쥴링 기능 구현 : 스케쥴링 설절 클래스 구현

---
+++ src/main/java/com/virnect/uaa/domain/user/schedule/SchedulerConfiguration.java
@@ -0,0 +1,12 @@
+package com.virnect.uaa.domain.user.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+public class SchedulerConfiguration {
+}
