a1e81ba2fef8f35a374f0269bea561722edb3a84
Yongho-VIRNECT <yhkim@virnect.com>
2021-08-24 14:25:52 +0900

    Update index.vue
    - 컴포넌트 랜더링 시 깜빡이는 현상 때문에 v-if 에서 v-show로 변경

--- src/pages/members/index.vue
+++ src/pages/members/index.vue
@@ -54,7 +54,7 @@
       />
     </div>
     <MemberAddModal
-      v-if="showAddModal"
+      v-show="showAddModal"
       :membersTotal="membersTotal"
       @refresh="searchMembers()"
       @close="closeMemberAddModal"
