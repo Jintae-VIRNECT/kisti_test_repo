05c2092a92bcedae84c72c94ba0bd331a0f84169
ykmo-VIRNECT <62318056+ykmo-VIRNECT@users.noreply.github.com>
2021-08-26 15:29:19 +0900

    Revert "불필요한 페이지 삭제(약관, 정책)"

--- src/routers/extra.js
+++ src/routers/extra.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+const PolicyComponent = () => import('components/policy/PolicyLayout')
+const OSSComponent = () => import('components/oss/OSS')
 const SupportComponent = () => import('components/support/SupportComponent')
+import Terms from 'components/policy/PolicyTerms'
+import Privacy from 'components/policy/PolicyPrivacy'
 
 Vue.use(VueRouter)
 
@@ -9,13 +13,55 @@ export default new VueRouter({
   mode: 'history',
   routes: [
     {
+      path: '/policy',
+      component: PolicyComponent,
+      children: [
+        {
+          path: '',
+          redirect: { name: 'terms' },
+        },
+        {
+          // 이용약관
+          path: 'terms',
+          name: 'terms',
+          component: Terms,
+        },
+        {
+          // 개인정보처리방침
+          path: 'privacy',
+          name: 'privacy',
+          component: Privacy,
+        },
+        // {
+        //   // 쿠키취급방침
+        //   path: 'cookie',
+        //   name: 'cookie',
+        //   component: Cookie,
+        // },
+        {
+          path: '*',
+          redirect: { name: 'terms' },
+        },
+      ],
+    },
+    {
+      path: '/OSS',
+      redirect: '/OSS/mobile',
+    },
+    {
+      path: '/OSS/:type',
+      name: 'OSS',
+      props: true,
+      component: OSSComponent,
+    },
+    {
       path: '/support',
       name: 'support',
       component: SupportComponent,
     },
     {
       path: '*',
-      redirect: 'support',
+      redirect: 'privacy',
     },
   ],
 })
