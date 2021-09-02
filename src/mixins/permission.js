922ba3651591f7c80787ba36f3c89dba4c7fbbf5
Yongho-VIRNECT <yhkim@virnect.com>
2021-09-02 15:23:58 +0900

    Refactor: 공통으로 사용될 수 있는 로직 Mixins 으로 분리
    - 에러코드를 받아서 메시지창을 출력하는 message mixin
    - 권한 확인 및 처리를 하는 permission mixns
    - 벨리데이션 조건과 메시지 처리를 담당하는 validateion mixin

---
+++ src/mixins/permission.js
@@ -0,0 +1,56 @@
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters({
+      activeWorkspace: 'auth/activeWorkspace',
+      plansInfo: 'plan/plansInfo',
+      auth: 'auth/auth',
+    }),
+  },
+  methods: {
+    mine(userId) {
+      return userId === this.auth.myInfo.uuid
+    },
+    canManage(role) {
+      if (this.activeWorkspace.role === 'MANAGER') {
+        if (role !== 'MASTER') return true
+        else return false
+      } else if (this.activeWorkspace.role === 'MASTER') return true
+      else return false
+    },
+    canEdit(type, role) {
+      if (this.canManage(role)) {
+        // 일반 계정 일 때 수정 불가
+        if (this.userTypeIsUser(type)) return false
+        else return true
+      }
+      return false
+    },
+    canKick(type, role) {
+      if (this.canManage(role)) {
+        // 일반 계정 일 때 내보내기
+        if (this.userTypeIsUser(type)) return true
+        else return false
+      }
+      return false
+    },
+    roleIsMaster(role) {
+      return role === 'MASTER'
+    },
+    roleIsManager(role) {
+      return role === 'MANAGER'
+    },
+    roleIsSeat(role) {
+      return role === 'SEAT'
+    },
+    userTypeIsUser(type) {
+      return type === 'USER'
+    },
+    userTypeIsSeat(type) {
+      return type === 'SEAT_USER'
+    },
+    userTypeIsWorkspaceOnlyUser(type) {
+      return type === 'WORKSPACE_ONLY_USER'
+    },
+  },
+}
