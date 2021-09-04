525b1d4c5ce309c6c7b2da4329e0435db3f0f22f
JeongHyeon-VIRNECT <sky456139@virnect.com>
2021-09-04 17:57:55 +0900

    fix: email authorization code not match exception problem fix

--- src/main/java/com/virnect/uaa/domain/auth/account/application/signup/DefaultAccountSignUpServiceImpl.java
+++ src/main/java/com/virnect/uaa/domain/auth/account/application/signup/DefaultAccountSignUpServiceImpl.java
@@ -1,24 +1,5 @@
 package com.virnect.uaa.domain.auth.account.application.signup;
 
-import java.io.IOException;
-import java.time.Duration;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.Locale;
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang.RandomStringUtils;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-import org.thymeleaf.context.Context;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import com.virnect.uaa.domain.auth.account.application.UserAccessLogService;
 import com.virnect.uaa.domain.auth.account.application.signup.handler.SignUpSuccessHandler;
 import com.virnect.uaa.domain.auth.account.dao.EmailAuthorizationRepository;
@@ -39,218 +20,237 @@ import com.virnect.uaa.infra.email.EmailService;
 import com.virnect.uaa.infra.email.context.MailMessageContext;
 import com.virnect.uaa.infra.file.Default;
 import com.virnect.uaa.infra.file.FileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.context.Context;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Locale;
+import java.util.UUID;
 
 @Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class DefaultAccountSignUpServiceImpl implements AccountSignUpService {
-	private final FileService fileService;
-	private final PasswordEncoder passwordEncoder;
-	private final SecessionUserRepository secessionUserRepository;
-	private final UserRepository userRepository;
-	private final EmailAuthorizationRepository emailAuthorizationRepository;
-	private final SignUpSuccessHandler signUpSuccessHandler;
-	private final UserAccessLogService userAccessLogService;
-	private final JwtProvider jwtProvider;
-	private final EmailService emailService;
+    private final FileService fileService;
+    private final PasswordEncoder passwordEncoder;
+    private final SecessionUserRepository secessionUserRepository;
+    private final UserRepository userRepository;
+    private final EmailAuthorizationRepository emailAuthorizationRepository;
+    private final SignUpSuccessHandler signUpSuccessHandler;
+    private final UserAccessLogService userAccessLogService;
+    private final JwtProvider jwtProvider;
+    private final EmailService emailService;
 
-	@Override
-	public OAuthTokenResponse signUp(
-		RegisterRequest registerRequest,
-		HttpServletRequest request,
-		Locale locale
-	) {
-		// Signup Session Code Check
-		signupSessionCodeValidate(registerRequest.getEmail(), registerRequest.getSessionCode());
+    @Override
+    public OAuthTokenResponse signUp(
+            RegisterRequest registerRequest,
+            HttpServletRequest request,
+            Locale locale
+    ) {
+        // Signup Session Code Check
+        signupSessionCodeValidate(registerRequest.getEmail(), registerRequest.getSessionCode());
 
-		// Check SignUp User Email Unique
-		signupEmailDuplicateCheck(registerRequest.getEmail());
+        // Check SignUp User Email Unique
+        signupEmailDuplicateCheck(registerRequest.getEmail());
 
-		// create register user
-		User user = createNewUser(registerRequest);
+        // create register user
+        User user = createNewUser(registerRequest);
 
-		// save new user
-		userRepository.save(user);
+        // save new user
+        userRepository.save(user);
 
-		// parse client user agent
-		ClientGeoIPInfo clientGeoIPInfo = userAccessLogService.saveUserAccessLogInformation(user, request);
+        // parse client user agent
+        ClientGeoIPInfo clientGeoIPInfo = userAccessLogService.saveUserAccessLogInformation(user, request);
 
-		// generate oauth token
-		OAuthTokenResponse oauthTokenFromSignup = getOauthTokenFromSignup(user, clientGeoIPInfo);
+        // generate oauth token
+        OAuthTokenResponse oauthTokenFromSignup = getOauthTokenFromSignup(user, clientGeoIPInfo);
 
-		// Do After Signup Success
-		signUpSuccessHandler.signUpSuccess(user, request, locale);
+        // Do After Signup Success
+        signUpSuccessHandler.signUpSuccess(user, request, locale);
 
-		return oauthTokenFromSignup;
-	}
+        return oauthTokenFromSignup;
+    }
 
-	@Override
-	public EmailAuthResponse emailAuthentication(
-		EmailAuthRequest emailAuthRequest,
-		Locale locale
-	) {
-		// 탈퇴 회원의 이메일 정보인지 확인
-		checkSecessionUserEmail(emailAuthRequest.getEmail());
+    @Override
+    public EmailAuthResponse emailAuthentication(
+            EmailAuthRequest emailAuthRequest,
+            Locale locale
+    ) {
+        // 탈퇴 회원의 이메일 정보인지 확인
+        checkSecessionUserEmail(emailAuthRequest.getEmail());
 
-		// 이미 가입된 사용자가 있는지 확인
-		emailDuplicateCheck(emailAuthRequest);
+        // 이미 가입된 사용자가 있는지 확인
+        emailDuplicateCheck(emailAuthRequest);
 
-		// 기존 인증 요청이 있는지 확인
-		authorizationRequestDuplicateCheck(emailAuthRequest);
+        // 기존 인증 요청이 있는지 확인
+        authorizationRequestDuplicateCheck(emailAuthRequest);
 
-		// 인증 이메일 전송
-		sendAuthenticationEmail(emailAuthRequest, locale);
+        // 인증 이메일 전송
+        sendAuthenticationEmail(emailAuthRequest, locale);
 
-		return new EmailAuthResponse(true);
-	}
+        return new EmailAuthResponse(true);
+    }
 
-	@Transactional(readOnly = true)
-	@Override
-	public EmailVerificationResponse emailAuthCodeVerification(
-		String email,
-		String code
-	) {
-		EmailAuth emailAuth = emailAuthorizationRepository.findById(email)
-			.orElseThrow(
-				() -> new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_AUTHENTICATION));
-		if (!emailAuth.getCode().equals(code)) {
-			throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_AUTHENTICATION);
-		}
-		return new EmailVerificationResponse(true, emailAuth.getSessionCode());
-	}
+    @Transactional(readOnly = true)
+    @Override
+    public EmailVerificationResponse emailAuthCodeVerification(
+            String email,
+            String code
+    ) {
+        EmailAuth emailAuth = emailAuthorizationRepository.findById(email)
+                .orElseThrow(
+                        () -> new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_AUTHENTICATION));
 
-	private void checkSecessionUserEmail(String email) {
-		boolean isSecessionUserEmail = secessionUserRepository.existsByEmail(email);
-		if (isSecessionUserEmail) {
-			log.info("[EMAIL_AUTHENTICATION] [SECESSION_USER] : [{}]", email);
-			throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_SECESSION_USER_EMAIL);
-		}
-	}
+        log.debug("[CurrentRequest] Email:[{}] Code:[{}] [EmailAuth]:{}", email, code, emailAuth);
 
-	private void emailDuplicateCheck(EmailAuthRequest emailAuthRequest) {
-		boolean hasDuplicatedEmail = userRepository.existsByEmail(emailAuthRequest.getEmail());
+        if (!emailAuth.getCode().equals(code)) {
+            throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_AUTHENTICATION);
+        }
+        return new EmailVerificationResponse(true, emailAuth.getSessionCode());
+    }
 
-		// Check email is Duplicate
-		if (hasDuplicatedEmail) {
-			throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_DUPLICATED_EMAIL);
-		}
-	}
+    private void checkSecessionUserEmail(String email) {
+        boolean isSecessionUserEmail = secessionUserRepository.existsByEmail(email);
+        if (isSecessionUserEmail) {
+            log.info("[EMAIL_AUTHENTICATION] [SECESSION_USER] : [{}]", email);
+            throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_SECESSION_USER_EMAIL);
+        }
+    }
 
-	/**
-	 * 이메일 인증 요청 중복 여부 검사
-	 *
-	 * @param emailAuthRequest - 이메일 인증 요청 정보
-	 */
-	private void authorizationRequestDuplicateCheck(EmailAuthRequest emailAuthRequest) {
-		boolean hasPreviousRequest = emailAuthorizationRepository
-			.existsById(emailAuthRequest.getEmail());
+    private void emailDuplicateCheck(EmailAuthRequest emailAuthRequest) {
+        boolean hasDuplicatedEmail = userRepository.existsByEmail(emailAuthRequest.getEmail());
 
-		// if have previous request
-		if (hasPreviousRequest) {
-			// delete previous request
-			emailAuthorizationRepository.deleteById(emailAuthRequest.getEmail());
-		}
-	}
+        // Check email is Duplicate
+        if (hasDuplicatedEmail) {
+            throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_DUPLICATED_EMAIL);
+        }
+    }
 
-	/**
-	 * 이메일 인증 메일 전송 처리
-	 *
-	 * @param emailAuthRequest - 이메일 인증 요청 정보
-	 * @param locale
-	 */
-	private void sendAuthenticationEmail(EmailAuthRequest emailAuthRequest, Locale locale) {
-		String code = RandomStringUtils.randomNumeric(6);
-		String sessionCode = UUID.randomUUID().toString().replace("-", "");
-		long duration = Duration.ofMinutes(30).getSeconds();
+    /**
+     * 이메일 인증 요청 중복 여부 검사
+     *
+     * @param emailAuthRequest - 이메일 인증 요청 정보
+     */
+    private void authorizationRequestDuplicateCheck(EmailAuthRequest emailAuthRequest) {
+        boolean hasPreviousRequest = emailAuthorizationRepository
+                .existsById(emailAuthRequest.getEmail());
 
-		ZoneOffset seoulZoneOffset = ZoneOffset.of("+09:00");
-		ZonedDateTime zonedDateTime = ZonedDateTime.now(seoulZoneOffset).plusSeconds(duration);
+        // if have previous request
+        if (hasPreviousRequest) {
+            // delete previous request
+            emailAuthorizationRepository.deleteById(emailAuthRequest.getEmail());
+        }
+    }
 
-		EmailAuth emailAuth = EmailAuth.builder()
-			.email(emailAuthRequest.getEmail())
-			.code(code)
-			.sessionCode(sessionCode)
-			.expireDate(duration)
-			.build();
+    /**
+     * 이메일 인증 메일 전송 처리
+     *
+     * @param emailAuthRequest - 이메일 인증 요청 정보
+     * @param locale - 사용자 언어 정보
+     */
+    private void sendAuthenticationEmail(EmailAuthRequest emailAuthRequest, Locale locale) {
+        String code = RandomStringUtils.randomNumeric(6);
+        String sessionCode = UUID.randomUUID().toString().replace("-", "");
+        long duration = Duration.ofMinutes(30).getSeconds();
 
-		// save new authentication request on redis
-		emailAuthorizationRepository.save(emailAuth);
+        ZoneOffset seoulZoneOffset = ZoneOffset.of("+09:00");
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(seoulZoneOffset).plusSeconds(duration);
 
-		// send email authentication email
-		Context context = new Context();
-		context.setVariable("email", emailAuthRequest.getEmail());
-		context.setVariable("code", code);
-		context.setVariable("expiredDate", zonedDateTime);
+        EmailAuth emailAuth = EmailAuth.builder()
+                .email(emailAuthRequest.getEmail())
+                .code(code)
+                .sessionCode(sessionCode)
+                .expireDate(duration)
+                .build();
 
-		MailMessageContext mailMessageContext = MailMessageContext.builder()
-			.templateContext(context)
-			.templateName("email_check")
-			.titleMessageSourceName("MAIL_TITLE_OF_REGISTER_EMAIL_CHECK")
-			.to(emailAuthRequest.getEmail())
-			.locale(locale)
-			.build();
-		emailService.send(mailMessageContext);
-	}
+        // save new authentication request on redis
+        emailAuthorizationRepository.save(emailAuth);
 
-	public void signupSessionCodeValidate(String email, String sessionCode) {
-		EmailAuth emailAuth = emailAuthorizationRepository.findById(email)
-			.orElseThrow(() -> {
-					log.error("EmailAuthentication Information Not Found. Search by [{}]", email);
-					return new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_SESSION_EXPIRE);
-				}
-			);
+        // send email authentication email
+        Context context = new Context();
+        context.setVariable("email", emailAuthRequest.getEmail());
+        context.setVariable("code", code);
+        context.setVariable("expiredDate", zonedDateTime);
 
-		if (!emailAuth.getSessionCode().equals(sessionCode) || !emailAuth.getEmail().equals(email)) {
-			log.error("Register Email or SessionCode is not matched.");
-			log.error("Register Request Email: [{}] , Register Request SessionCode: [{}]", email, sessionCode);
-			log.error("emailAuth: [{}]", emailAuth);
-			throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_AUTHENTICATION);
-		}
-	}
+        MailMessageContext mailMessageContext = MailMessageContext.builder()
+                .templateContext(context)
+                .templateName("email_check")
+                .titleMessageSourceName("MAIL_TITLE_OF_REGISTER_EMAIL_CHECK")
+                .to(emailAuthRequest.getEmail())
+                .locale(locale)
+                .build();
+        emailService.send(mailMessageContext);
+    }
 
-	public void signupEmailDuplicateCheck(String email) {
-		boolean isDuplicated = userRepository.existsByEmail(email);
-		if (isDuplicated) {
-			throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_DUPLICATED_EMAIL);
-		}
-	}
+    public void signupSessionCodeValidate(String email, String sessionCode) {
+        EmailAuth emailAuth = emailAuthorizationRepository.findById(email)
+                .orElseThrow(() -> {
+                            log.error("EmailAuthentication Information Not Found. Search by [{}]", email);
+                            return new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_SESSION_EXPIRE);
+                        }
+                );
 
-	public User createNewUser(RegisterRequest registerRequest) {
-		String profileUrl = getUserProfileUrlFromProfileImage(registerRequest.getProfile());
-		String encodedPassword = passwordEncoder.encode(registerRequest.getPassword());
-		return User.BySignUpUserBuilder()
-			.registerRequest(registerRequest)
-			.encodedPassword(encodedPassword)
-			.profileUrl(profileUrl)
-			.build();
-	}
+        if (!emailAuth.getSessionCode().equals(sessionCode) || !emailAuth.getEmail().equals(email)) {
+            log.error("Register Email or SessionCode is not matched.");
+            log.error("Register Request Email: [{}] , Register Request SessionCode: [{}]", email, sessionCode);
+            log.error("emailAuth: [{}]", emailAuth);
+            throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_AUTHENTICATION);
+        }
+    }
 
-	public String getUserProfileUrlFromProfileImage(MultipartFile profile) {
-		// return default image url
-		if (profile == null) {
-			return Default.USER_PROFILE.getValue();
-		}
-		try {
-			return fileService.upload(profile);
-		} catch (IOException e) {
-			log.error("profile image upload fail.");
-			log.error(e.getMessage(), e);
-			throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER);
-		}
-	}
+    public void signupEmailDuplicateCheck(String email) {
+        boolean isDuplicated = userRepository.existsByEmail(email);
+        if (isDuplicated) {
+            throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER_DUPLICATED_EMAIL);
+        }
+    }
 
-	public OAuthTokenResponse getOauthTokenFromSignup(User user, ClientGeoIPInfo clientGeoIPInfo) {
-		String accessTokenJwtId = UUID.randomUUID().toString();
-		String refreshTokenJwtId = UUID.randomUUID().toString();
-		String accessToken = jwtProvider.createAccessToken(user, accessTokenJwtId, clientGeoIPInfo);
-		String refreshToken = jwtProvider.createRefreshToken(
-			user, refreshTokenJwtId, accessTokenJwtId, clientGeoIPInfo);
-		OAuthTokenResponse oAuthTokenResponse = new OAuthTokenResponse();
-		oAuthTokenResponse.setAccessToken(accessToken);
-		oAuthTokenResponse.setRefreshToken(refreshToken);
-		oAuthTokenResponse.setExpireIn(jwtProvider.getAccessTokenExpire());
-		oAuthTokenResponse.setPasswordInitialized(true);
-		return oAuthTokenResponse;
-	}
+    public User createNewUser(RegisterRequest registerRequest) {
+        String profileUrl = getUserProfileUrlFromProfileImage(registerRequest.getProfile());
+        String encodedPassword = passwordEncoder.encode(registerRequest.getPassword());
+        return User.BySignUpUserBuilder()
+                .registerRequest(registerRequest)
+                .encodedPassword(encodedPassword)
+                .profileUrl(profileUrl)
+                .build();
+    }
+
+    public String getUserProfileUrlFromProfileImage(MultipartFile profile) {
+        // return default image url
+        if (profile == null) {
+            return Default.USER_PROFILE.getValue();
+        }
+        try {
+            return fileService.upload(profile);
+        } catch (IOException e) {
+            log.error("profile image upload fail.");
+            log.error(e.getMessage(), e);
+            throw new UserAuthenticationServiceException(AuthenticationErrorCode.ERR_REGISTER);
+        }
+    }
+
+    public OAuthTokenResponse getOauthTokenFromSignup(User user, ClientGeoIPInfo clientGeoIPInfo) {
+        String accessTokenJwtId = UUID.randomUUID().toString();
+        String refreshTokenJwtId = UUID.randomUUID().toString();
+        String accessToken = jwtProvider.createAccessToken(user, accessTokenJwtId, clientGeoIPInfo);
+        String refreshToken = jwtProvider.createRefreshToken(
+                user, refreshTokenJwtId, accessTokenJwtId, clientGeoIPInfo);
+        OAuthTokenResponse oAuthTokenResponse = new OAuthTokenResponse();
+        oAuthTokenResponse.setAccessToken(accessToken);
+        oAuthTokenResponse.setRefreshToken(refreshToken);
+        oAuthTokenResponse.setExpireIn(jwtProvider.getAccessTokenExpire());
+        oAuthTokenResponse.setPasswordInitialized(true);
+        return oAuthTokenResponse;
+    }
 }
