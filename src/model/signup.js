d4eb895dec678eca4679e3e9b7361aa950de0a41
Yongho-VIRNECT <yhkim@virnect.com>
2021-09-07 10:12:24 +0900

    Update signup.js
    - signup component에서 사용하면서 객체 변경

--- src/model/signup.js
+++ src/model/signup.js
@@ -1,35 +1,14 @@
 export default class Signup {
-  constructor(
-    email,
-    password,
-    firstName,
-    lastName,
-    birth,
-    marketInfoReceive,
-    joinInfo,
-    serviceInfo,
-    sessionCode,
-    description,
-    nickname,
-    mobile,
-    profile,
-    recoveryEmail,
-    inviteSession,
-  ) {
-    this.email = email
-    this.password = password
-    this.firstName = firstName
-    this.lastName = lastName
-    this.birth = birth
-    this.marketInfoReceive = marketInfoReceive
-    this.joinInfo = joinInfo
-    this.serviceInfo = serviceInfo
-    this.sessionCode = sessionCode
-    this.description = description
-    this.nickname = nickname
-    this.mobile = mobile
-    this.profile = profile
-    this.recoveryEmail = recoveryEmail
-    this.inviteSession = inviteSession
+  constructor() {
+    this.email = ''
+    this.password = ''
+    this.firstName = ''
+    this.lastName = ''
+    this.birth = ''
+    this.marketInfoReceive = false
+    this.joinInfo = ''
+    this.serviceInfo = ''
+    this.sessionCode = ''
+    this.inviteSession = ''
   }
 }
