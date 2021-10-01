aea740bc3c1c870d7924a40120d41714cd4f3666
Yunsuk-VIRNECT <66057430+Yunsuk-VIRNECT@users.noreply.github.com>
2021-10-01 17:31:25 +0900

    Fix: 제거됬던 스팟 라우터가드 수정

--- src/components/spot/SpotLayout.vue
+++ src/components/spot/SpotLayout.vue
@@ -67,9 +67,9 @@ export default {
     SpotConnectModal,
   },
   //spot 페이지 진입 라우터 가드
-  // beforeRouteEnter(to, from, next) {
-  //   spotControlRouterGuard(next)
-  // },
+  beforeRouteEnter(to, from, next) {
+    spotControlRouterGuard(next)
+  },
   mixins: [confirmMixin, spotMixin],
   data() {
     return {
