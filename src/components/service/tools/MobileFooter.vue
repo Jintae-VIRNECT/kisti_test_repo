51a9f0798af15ecf1a21fd208da5249bc89117a2
Yunsuk-VIRNECT <66057430+Yunsuk-VIRNECT@users.noreply.github.com>
2021-10-19 17:02:26 +0900

    Feat: 반응형 협업보드 종료 버튼 동작 추가(리더)

--- src/components/service/tools/MobileFooter.vue
+++ src/components/service/tools/MobileFooter.vue
@@ -26,8 +26,10 @@
         <mobile-flash-button></mobile-flash-button>
       </template>
       <template v-else-if="view === VIEW.DRAWING">
+        <!-- 협업보드 종료 버튼 리더에게만 표시 -->
         <mobile-drawing-exit-button
-          v-if="historyList.length > 0"
+          v-if="historyList.length > 0 && isLeader"
+          @exitDrawing="onExitDrawing"
         ></mobile-drawing-exit-button>
         <mobile-file-list-button
           :disabled="fileList.length === 0"
@@ -151,6 +153,11 @@ export default {
           })
         }
       }
+    },
+    onExitDrawing() {
+      this.toastDefault(this.$t('service.toast_drawing_end'))
+      this.showImage({}) //공유중 파일 초기화
+      this.setView(VIEW.STREAM) //탭 실시간 공유로 이동
     },
   },
   created() {
