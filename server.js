264e292c06afe8eb29a835adec3f8365412430b9
Sunghyun-VIRNECT <58674642+Sunghyun-VIRNECT@users.noreply.github.com>
2021-10-21 09:14:21 +0900

    fix: ssl file sert config rm

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(route)
 
 	if (envSet.SSL_ENV === 'private') {
 		const options = {
-			key: fs.readFileSync(`./cert/${config.sslConfig.name}.key`),
-			cert: fs.readFileSync(`./cert/${config.sslConfig.name}.crt`)
+			key: fs.readFileSync('./cert/virnect.key'),
+			cert: fs.readFileSync('./cert/virnect.crt')
       // passphrase: config.sslConfig.pass
 		}
 		https.createServer(options, app).listen(envSet.SERVER_PORT)
