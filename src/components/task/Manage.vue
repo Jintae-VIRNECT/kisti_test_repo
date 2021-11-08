664c4c17e4d3d4511ffeb5d5ce0ad5c12cd25308
Yongho-VIRNECT <58449745+Yongho-VIRNECT@users.noreply.github.com>
2021-11-08 12:14:23 +0900

    fix;DPLA-3843; 하위작업, 씬이 없을 때 처리 (#180)
    * fix;DPLA-3843; 하위작업, 씬이 없을 때 처리
    
    - 하위작업, 씬이 없는 경우도 있는데 이 경우를 생각하지 않고 초기화 및 벨리데이션 체크를 하지 않고 진행하던 부분을 처리
    - 하위작업, 씬이 없다면 this.$refs.subForm이 존재하지 않아 undefined가 떨어지게 되는데 이 부분을 처리하지 않고 forEach 나 el-form 함수를 실행하게 되면서 오류가 발생
    
    * update: remove console

--- src/components/task/Manage.vue
+++ src/components/task/Manage.vue
@@ -319,22 +319,24 @@ export default {
       // 공통
       this.activeSubForms = this.subForm.map(form => form.id)
       this.workerList = await workspaceService.allMembers()
-      // clearValidate: 검증 메시지 제거
-      this.$refs.mainForm.clearValidate()
-      this.$refs.subForm.forEach(form => form.clearValidate())
     },
     closed() {
+      // clearValidate: 검증 메시지 제거
       this.$refs.mainForm.resetFields()
-      this.$refs.subForm.forEach(form => form.resetFields())
       this.mainForm.worker = ''
       this.mainForm.position = ''
+      if (this.$refs.subForm !== undefined)
+        this.$refs.subForm.forEach(form => form.resetFields())
     },
     async validate() {
       try {
-        await Promise.all([
-          this.$refs.mainForm.validate(),
-          ...this.$refs.subForm.map(form => form.validate()),
-        ])
+        await this.$refs.mainForm.validate()
+        if (this.$refs.subForm !== undefined) {
+          await Promise.all([
+            ...this.$refs.subForm.map(form => form.validate()),
+          ])
+        }
+
         return true
       } catch (e) {
         return false
@@ -388,6 +390,8 @@ export default {
       }
     },
     changedSchedule(date) {
+      if (this.$refs.subForm === undefined) return false
+
       // 하위작업 필드 초기화
       this.$refs.subForm.forEach(form => {
         form.resetFields()
