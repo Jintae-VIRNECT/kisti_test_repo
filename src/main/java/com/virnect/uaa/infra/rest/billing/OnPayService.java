24ce14a5245cc156aac27a21b369c6eb7d96d0d2
jeonghyeon-VIRNECT <sky456139@virnect.com>
2021-12-20 14:52:23 +0900

    fix:DPLA-4020: 신규가입 시 무료 쿠폰 발급 불가 이슈 수정 및 accessToken 단위 수정

--- src/main/java/com/virnect/uaa/infra/rest/billing/OnPayService.java
+++ src/main/java/com/virnect/uaa/infra/rest/billing/OnPayService.java
@@ -1,31 +1,38 @@
 package com.virnect.uaa.infra.rest.billing;
 
+import java.util.Collections;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import com.virnect.uaa.infra.rest.billing.dto.BillingCouponRegisterRequest;
-import com.virnect.uaa.infra.rest.billing.dto.BillingCouponRegisterResponse;
 import com.virnect.uaa.infra.rest.billing.dto.BillingRestResponse;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Profile(value = {"staging", "production"})
+@Profile(value = {"staging", "production", "test"})
 public class OnPayService implements PayService {
 	private static final String COUPON_REGISTER_API_URL = "/billing/coupon/registerbyid";
 	private final RestTemplate restTemplate;
+	private final ObjectMapper objectMapper;
 
 	@Value("${payletter.coupon:0}")
 	private int couponId;
 	@Value("${payletter.api:none}")
-	private String BILLING_API_ENDPOINT;
+	private String billingApiEndpoint;
 	@Value("${spring.profiles.active:develop}")
 	private String serverMode;
 
@@ -34,7 +41,7 @@ public class OnPayService implements PayService {
 		log.info("============================================================================");
 		log.info("--> PAY_LETTER_REST_SERVICE_ENABLE..");
 		log.info("--> COUPON_ID: {}", couponId);
-		log.info("--> BILLING_API: {}", BILLING_API_ENDPOINT);
+		log.info("--> BILLING_API: {}", billingApiEndpoint);
 		log.info("--> SERVICE_MODE: {}", serverMode);
 		log.info("============================================================================");
 	}
@@ -53,20 +60,27 @@ public class OnPayService implements PayService {
 		billingCouponRegisterRequest.setUserName(name);
 		billingCouponRegisterRequest.setUserNumber(userId);
 
-		log.info("[USER_EVENT_COUPON_REGISTER_START] - {}", billingCouponRegisterRequest);
 		try {
-			BillingRestResponse<BillingCouponRegisterResponse> couponRegisterResponse = restTemplate.postForObject(
-				BILLING_API_ENDPOINT + COUPON_REGISTER_API_URL, billingCouponRegisterRequest, BillingRestResponse.class
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+			String couponRegisterJsonReqBody = objectMapper.writeValueAsString(billingCouponRegisterRequest);
+			HttpEntity<String> couponRegisterReqHttpEntity = new HttpEntity<>(couponRegisterJsonReqBody, headers);
+
+			log.info("[USER_EVENT_COUPON_REGISTER_REQUEST] - [{}]", couponRegisterReqHttpEntity);
+
+			BillingRestResponse<?> couponRegisterResponse = restTemplate.postForObject(
+				billingApiEndpoint + COUPON_REGISTER_API_URL, couponRegisterReqHttpEntity, BillingRestResponse.class
 			);
-			if (couponRegisterResponse.getResult().getCode() != 0) {
-				log.error("[USER_EVENT_COUPON_REGISTER_ERROR] - [{}]", couponRegisterResponse.getResult().toString());
+
+			if (couponRegisterResponse == null || couponRegisterResponse.getResult().getCode() != 0) {
+				log.error("[USER_EVENT_COUPON_REGISTER_ERROR] - [{}]", couponRegisterResponse);
 			} else {
-				log.info("[USER_EVENT_COUPON_REGISTER_SUCCESS] - [{}]", couponRegisterResponse.toString());
+				log.info("[USER_EVENT_COUPON_REGISTER_SUCCESS] - [{}]", couponRegisterResponse);
 			}
 		} catch (Exception e) {
-			log.error("[USER_EVENT_COUPON_REGISTER_ERROR] - {}", billingCouponRegisterRequest.toString());
 			log.error("[USER_EVENT_COUPON_REGISTER_ERROR]", e);
 		}
-		log.info("[USER_EVENT_COUPON_REGISTER_END]");
 	}
 }
