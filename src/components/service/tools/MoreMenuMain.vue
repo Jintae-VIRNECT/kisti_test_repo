4d2e645b94d541f61f3f0284fca7a2466f68be68
ykmo-VIRNECT <62318056+ykmo-VIRNECT@users.noreply.github.com>
2022-01-07 15:49:35 +0900

    fix:DPLA-4138:[반응형] 리더 권한이 아니나 서버 녹화 메뉴 노출 #606 (#620)

--- src/components/service/tools/MoreMenuMain.vue
+++ src/components/service/tools/MoreMenuMain.vue
@@ -29,6 +29,7 @@
 <script>
 import { SPOT_CONTROL_ACTIVE } from 'configs/env.config'
 import { mapGetters } from 'vuex'
+import { ROLE } from 'configs/remote.config'
 
 const MENU = {
   CAMERA_CONTROL: 'CAMERA_CONTROL',
@@ -69,6 +70,9 @@ export default {
     isPreparing() {
       return this.serverRecordStatus === 'PREPARE'
     },
+    isLeader() {
+      return this.account.roleType === ROLE.LEADER
+    },
     menus() {
       return [
         {
@@ -87,7 +91,7 @@ export default {
           title: this.isRecording
             ? this.$t('service.record_server_end')
             : this.$t('service.record_server'),
-          visible: this.useRecording,
+          visible: this.useRecording && this.isLeader,
         },
         {
           class: 'member',
