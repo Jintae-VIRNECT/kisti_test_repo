c271b53ae48bba3dd4e4fb43e036a12bd4b2c4ad
Jukyoung-VIRNECT <ljk@virnect.com>
2022-03-30 17:31:49 +0900

    fix:DCAR-683:#83:[PF-Download] 메모리 이슈 개선

--- src/main/java/com/virnect/download/application/download/ApkService.java
+++ src/main/java/com/virnect/download/application/download/ApkService.java
@@ -1,9 +1,14 @@
 package com.virnect.download.application.download;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import net.dongliu.apk.parser.ByteArrayApkFile;
+import net.dongliu.apk.parser.ApkFile;
 import net.dongliu.apk.parser.bean.ApkMeta;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,13 +19,15 @@ import com.virnect.download.global.error.ErrorCode;
 @Service
 @Slf4j
 public class ApkService {
+	private static final String FILE_DIR = "tmp";
+
 	public ApkMeta parsingAppInfo(MultipartFile app) {
 		if (app == null || app.isEmpty()) {
 			log.error("Upload Application file is empty or set as null");
 			throw new AppServiceException(ErrorCode.ERR_APP_UPLOAD_FAIL);
 		}
-		try (ByteArrayApkFile apkFile = new ByteArrayApkFile(app.getBytes())) {
-			ApkMeta apkMeta = apkFile.getApkMeta();
+		try {
+			ApkMeta apkMeta = getApkMeta(app);
 			log.info(apkMeta.toString());
 			return apkMeta;
 		} catch (Exception e) {
@@ -28,4 +35,36 @@ public class ApkService {
 			throw new AppServiceException(ErrorCode.ERR_APP_UPLOAD_FAIL);
 		}
 	}
+
+	private ApkMeta getApkMeta(MultipartFile multipartFile) {
+		File file = new File(FILE_DIR + "/" + multipartFile.getOriginalFilename());
+		log.info("[APK_SERVICE] FILE CONVERT TO APK FILE. NAME : {}", multipartFile.getOriginalFilename());
+		try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+			IOUtils.copy(multipartFile.getInputStream(), fileOutputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+			deleteFile(file);
+			throw new AppServiceException(ErrorCode.ERR_APP_UPLOAD_FAIL);
+		}
+
+		try (ApkFile apkFile = new ApkFile(file)) {
+			return apkFile.getApkMeta();
+		} catch (IOException e) {
+			e.printStackTrace();
+			deleteFile(file);
+			throw new AppServiceException(ErrorCode.ERR_APP_UPLOAD_FAIL);
+		} finally {
+			deleteFile(file);
+		}
+	}
+
+	private void deleteFile(File fIle) {
+		if (fIle.delete()) {
+			log.info("[APK_SERVICE] FILE DELETED.");
+		} else {
+			log.info("[APK_SERVICE] FILE NOT DELETED.");
+		}
+
+	}
+
 }
