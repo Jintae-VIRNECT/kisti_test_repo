3ace0981c42bdc50929098f53c634958a7221663
ykmo-VIRNECT <62318056+ykmo-VIRNECT@users.noreply.github.com>
2022-04-06 11:49:24 +0900

    fix:DCR-747:협업보드 동기화 이슈 (#760)
    * chore:불필요한 주석 제거
    
    * fix:참가자 업데이트 시그널 조건 변경
    
    * refactor:공유파일 재전송 로직 공통화처리
    
    * fix:backCanvas 크기 예외처리
    
    * fix:협업 보드 뷰 전환시 backCanvas 크기 재설정
    
    * refactor:remove unused comment

--- src/components/service/drawing/DrawingHistory.js
+++ src/components/service/drawing/DrawingHistory.js
@@ -16,6 +16,11 @@ export default {
         img: url,
         json: json,
       }
+      //backCanvas의 크기가 0인 상태에서 update 시도시
+      //empty image가 업데이트 되는것을 방지하기 위함.
+      const isEmptyBackCanvas =
+        this.backCanvas.width === 0 && this.backCanvas.height === 0
+      if (isEmptyBackCanvas) return
 
       this.$store.dispatch('updateHistory', params)
     },
