f61ac371074435abd1f675a51cd2ffbd5a212c09
Jintae-VIRNECT <93517034+Jintae-VIRNECT@users.noreply.github.com>
2022-07-06 14:46:19 +0900

    ci : PJTDEV-276 REMOTE 2.7 CI/CD 구성  - 프로젝트 개발팀  jenkinsFile 추가  - 개발 센터 jenkinsFile name 변경.  - influxdb ip 변경.

--- stats-collector/run.sh
+++ stats-collector/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "${INFLUXDB_URL}" ]]; then
-  export INFLUXDB_URL="http://10.0.0.40:8086/MEDIA"
+  export INFLUXDB_URL="http://192.168.0.9:8086/MEDIA"
 fi
 
 if [[ -z "${KMS_EXTERNAL_ADDRESS}" ]]; then
