919349d6b36171f07e6f259985052c6bbc166e4d
mypark@virnect.com <mypark@virnect.com>
2022-07-18 16:29:45 +0900

    feat:PJTDEV-306 로그아웃 시 주소입력창에 IP정보 비노출되도록 적용 1. 리다이렉트시 url 기반으로 이동하도록 변경

--- src/middleware/default.js
+++ src/middleware/default.js
@@ -17,9 +17,7 @@ export default async function({ req, store, redirect, error, $config }) {
     // 사용자가 로그인을 하지 않은 경우.
     if (!req.headers.cookie || !req.headers.cookie.match('accessToken=')) {
       return redirect(
-        `${url.console}?continue=${encodeURIComponent(
-          req.headers.referer || req.headers.host + req.url,
-        )}`,
+        `${url.console}?continue=${url.account}`,
       )
     }
 
@@ -30,9 +28,7 @@ export default async function({ req, store, redirect, error, $config }) {
       // 비정상 토큰
       if (/^Error: (8003|8005)/.test(e)) {
         return redirect(
-          `${url.console}?continue=${encodeURIComponent(
-            req.headers.referer || req.headers.host,
-          )}`,
+          `${url.console}?continue=${url.account}`,
         )
       } else if (e.code === 'ECONNABORTED') {
         e.statusCode = 504
