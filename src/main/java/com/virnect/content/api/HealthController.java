360652ae4b7ffdd35b8e7d497b7f94aef2d211c9
Jintae-VIRNECT <93517034+Jintae-VIRNECT@users.noreply.github.com>
2022-07-24 15:21:36 +0900

    chore : PJTDEV-317 서버 별 version 정보 확인 가능할 수 있도록 수정.
    - version update 1.5.0
    - healthcheck api  버전정보 추가.
    - 빌드 로그에 버전 정보 추가.

--- src/main/java/com/virnect/content/api/HealthController.java
+++ src/main/java/com/virnect/content/api/HealthController.java
@@ -1,11 +1,14 @@
 package com.virnect.content.api;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author jiyong.heo
@@ -17,9 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class HealthController {
-	@GetMapping("/healthCheck")
+	private final BuildProperties buildProperties;
+	@GetMapping("/healthcheck")
 	public ResponseEntity<String> healthCheck() {
-		return ResponseEntity.ok("Content Server Health Check " + LocalDateTime.now());
+		String message =
+			"\n\n"
+				+ "------------------------------------------------------------------------------\n" + "\n"
+				+ "   VIRNECT USER ACCOUNT AND AUTHENTICATION SERVER\n"
+				+ "   ---------------------------\n" + "\n"
+				+ "   * SERVER_VERSION: [ " + buildProperties.getVersion() + " ]\n" + "\n"
+				+ "   * SERVER_MODE: [ " + System.getenv("VIRNECT_ENV") + " ]\n" + "\n"
+				+ "   * HEALTH_CHECK_DATE: [ " + ZonedDateTime.now() + " ]\n" + "\n"
+				+ "------------------------------------------------------------------------------\n";
+
+		return ResponseEntity.ok(message);
 	}
 }
--- src/main/java/com/virnect/content/event/ApplicationEventHandler.java
+++ src/main/java/com/virnect/content/event/ApplicationEventHandler.java
@@ -1,20 +1,26 @@
 package com.virnect.content.event;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ApplicationEventHandler {
+
+	private final BuildProperties buildProperties;
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void applicationReady() {
 		String msg = "\n\n----------------------------------------------------\n" + "\n"
 			+ "   Platform ContentsManagement Server is ready!\n"
 			+ "   ---------------------------\n" + "\n"
+			+ "   * Server Version: [" + buildProperties.getVersion() + "]\n" + "\n"
 			+ "   * Server Host: [" + System.getenv("eureka.instance.ip-address") + "]\n" + "\n"
 			+ "   * VIRNECT_ENV: [" + System.getenv("VIRNECT_ENV") + "]\n" + "\n"
 			+ "   * Config Server Url: [" + System.getenv("CONFIG_SERVER") + "]\n" + "\n"
