eb936c01ccc708db129aac50fe253ced2ccc6461
Jintae-VIRNECT <93517034+Jintae-VIRNECT@users.noreply.github.com>
2022-07-24 17:33:12 +0900

    chore : PJTDEV-317 서버 별 version 정보 확인 가능할 수 있도록 수정.
    - version update 1.5.0
    - healthcheck api  버전정보 추가.
    - 빌드 로그에 버전 정보 추가.

--- src/main/java/com/virnect/download/api/HealthController.java
+++ src/main/java/com/virnect/download/api/HealthController.java
@@ -1,11 +1,15 @@
 package com.virnect.download.api;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Project: PF-Download
@@ -16,9 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class HealthController {
+
+	private final BuildProperties buildProperties;
+
 	@GetMapping("/healthCheck")
 	public ResponseEntity<String> healthCheck() {
-		return ResponseEntity.ok("Download Server Health Check " + LocalDateTime.now());
+
+		String message =
+			"\n\n"
+				+ "------------------------------------------------------------------------------\n" + "\n"
+				+ "   VIRNECT DOWNLOAD SERVER\n"
+				+ "   ---------------------------\n" + "\n"
+				+ "   * SERVER_VERSION: [ " + buildProperties.getVersion() + " ]\n" + "\n"
+				+ "   * SERVER_MODE: [ " + System.getenv("VIRNECT_ENV") + " ]\n" + "\n"
+				+ "   * HEALTH_CHECK_DATE: [ " + ZonedDateTime.now() + " ]\n" + "\n"
+				+ "------------------------------------------------------------------------------\n";
+		return ResponseEntity.ok(message);
 	}
 }
