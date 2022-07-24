861499fc855997b6dd81cd1c192992d02a485726
Jintae-VIRNECT <93517034+Jintae-VIRNECT@users.noreply.github.com>
2022-07-24 19:27:15 +0900

    chore : PJTDEV-317 서버 별 version 정보 확인 가능할 수 있도록 수정.
    - version update 1.3.3
    - healthcheck api  버전정보 추가.
    - 빌드 로그에 버전 정보 추가.

--- src/main/java/com/virnect/process/event/ApplicationEventHandler.java
+++ src/main/java/com/virnect/process/event/ApplicationEventHandler.java
@@ -1,20 +1,26 @@
 package com.virnect.process.event;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ApplicationEventHandler {
+
+	private final BuildProperties buildProperties;
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void applicationReady() {
 		String msg = "\n\n----------------------------------------------------\n" + "\n"
 			+ "   Platform ProcessManagement Server is ready!\n"
 			+ "   ---------------------------\n" + "\n"
+			+ "   * SERVER_VERSION: [ " + buildProperties.getVersion() + " ]\n" + "\n"
 			+ "   * Server Host: [" + System.getenv("eureka.instance.ip-address") + "]\n" + "\n"
 			+ "   * VIRNECT_ENV: [" + System.getenv("VIRNECT_ENV") + "]\n" + "\n"
 			+ "   * Config Server Url: [" + System.getenv("CONFIG_SERVER") + "]\n" + "\n"
