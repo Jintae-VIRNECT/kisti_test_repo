3a69f3d981cce8ce55ce9a069cdcaf19058966a0
Jintae-VIRNECT <93517034+Jintae-VIRNECT@users.noreply.github.com>
2022-07-24 17:52:58 +0900

    chore : PJTDEV-317 서버 별 version 정보 확인 가능할 수 있도록 수정.
    - version update 1.2.24.0
    - healthcheck api 추가 (서버, 버전 정보)
    - 빌드 로그에 버전 정보 추가.

--- src/main/java/com/virnect/eureka/EurekaApplication.java
+++ src/main/java/com/virnect/eureka/EurekaApplication.java
@@ -3,15 +3,20 @@ package com.virnect.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.event.EventListener;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
 @EnableEurekaServer
+@RequiredArgsConstructor
 public class EurekaApplication {
+
+	private final BuildProperties buildProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
@@ -23,6 +28,7 @@ public class EurekaApplication {
 		String msg = "\n\n----------------------------------------------------\n" + "\n"
 			+ "   Platform Eureka Server is ready!\n"
 			+ "   ---------------------------\n" + "\n"
+			+ "   * Server Version: [" + buildProperties.getVersion() + "]\n" + "\n"
 			+ "   * Server Host: [" + System.getenv("eureka.instance.ip-address") + "]\n" + "\n"
 			+ "   * VIRNECT_ENV: [" + System.getenv("VIRNECT_ENV") + "]\n" + "\n"
 			+ "   * Config Server Url: [" + System.getenv("CONFIG_SERVER") + "]\n" + "\n"
---
+++ src/main/java/com/virnect/eureka/api/HealthCheckController.java
@@ -0,0 +1,45 @@
+package com.virnect.eureka.api;
+
+import java.time.ZonedDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author delbert park
+ * @project PF-Config
+ * @email delbert@virnect.com
+ * @description
+ * @since 2020.09.21
+ */
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+    private final BuildProperties buildProperties;
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck() {
+
+
+        String message =
+            "\n\n"
+                + "------------------------------------------------------------------------------\n" + "\n"
+                + "   VIRNECT CONFIG SERVER\n"
+                + "   ---------------------------\n" + "\n"
+                + "   * SERVER_VERSION: [ " + buildProperties.getVersion() + " ]\n" + "\n"
+                + "   * SERVER_MODE: [ " + System.getenv("VIRNECT_ENV") + " ]\n" + "\n"
+                + "   * HEALTH_CHECK_DATE: [ " + ZonedDateTime.now() + " ]\n" + "\n"
+                + "------------------------------------------------------------------------------\n";
+
+        return ResponseEntity.ok(message);
+    }
+}
