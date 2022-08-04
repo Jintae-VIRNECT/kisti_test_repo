89235c31473b6960ab0547877daa0b8bb4e555b1
Jintae-VIRNECT <93517034+Jintae-VIRNECT@users.noreply.github.com>
2022-08-04 16:58:06 +0900

    fix :PJTDEV-343 pf-message 가 config 서버를 찾지 못할 경우 예외처리

--- src/main/java/com/virnect/message/global/config/rabbitmq/RabbitmqConfiguration.java
+++ src/main/java/com/virnect/message/global/config/rabbitmq/RabbitmqConfiguration.java
@@ -1,12 +1,13 @@
 package com.virnect.message.global.config.rabbitmq;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Project: PF-Message
@@ -18,38 +19,42 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class RabbitmqConfiguration {
-    private final RabbitmqProperty rabbitmqProperty;
+	private final RabbitmqProperty rabbitmqProperty;
 
-    @Bean
-    public CachingConnectionFactory cachingConnectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setPort(rabbitmqProperty.getPort());
-        connectionFactory.setUsername(rabbitmqProperty.getUsername());
-        connectionFactory.setPassword(rabbitmqProperty.getPassword());
-        connectionFactory.setHost(rabbitmqProperty.getHost());
-        connectionFactory.setPublisherReturns(true);
-        return connectionFactory;
-    }
+	@Bean
+	public CachingConnectionFactory cachingConnectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+		connectionFactory.setPort(rabbitmqProperty.getPort());
+		connectionFactory.setUsername(rabbitmqProperty.getUsername());
+		connectionFactory.setPassword(rabbitmqProperty.getPassword());
+		connectionFactory.setHost(rabbitmqProperty.getHost());
+		connectionFactory.setPublisherReturns(true);
+		return connectionFactory;
+	}
 
+	@Bean
+	public RabbitTemplate rabbitTemplate() {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(cachingConnectionFactory());
+		rabbitTemplate.setMessageConverter(messageConverter());
+		rabbitTemplate.setMandatory(true);
+		rabbitTemplate.setReturnCallback(new CustomReturnCallback());
+		return rabbitTemplate;
+	}
 
-    @Bean
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(cachingConnectionFactory());
-        rabbitTemplate.setMessageConverter(messageConverter());
-        rabbitTemplate.setMandatory(true);
-        rabbitTemplate.setReturnCallback(new CustomReturnCallback());
-        return rabbitTemplate;
-    }
+	private Jackson2JsonMessageConverter messageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
 
-    private Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+	@Bean
+	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() throws Exception {
 
-    @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
-        final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(cachingConnectionFactory());
-        factory.setMessageConverter(messageConverter());
-        return factory;
-    }
+		if (rabbitmqProperty.getPort() == 0) {
+			throw new Exception("The config server does not exist. Restart the message server.");
+		}
+
+		final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(cachingConnectionFactory());
+		factory.setMessageConverter(messageConverter());
+		return factory;
+	}
 }
